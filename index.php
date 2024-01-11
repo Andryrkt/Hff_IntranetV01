9c600bc53e54b35b21c791f88518a525e76370af
hasimanjaka <hratompo@gmail.com>
2024-01-11 17:33:30 +0300

    update

--- index.php
+++ index.php
@@ -1,20 +1,26 @@
 <?php
 include 'Model/LdapModel.php';
 include 'Controler/LdapControl.php';
-$Username = isset($_POST['Username']) ? $_POST['Username']: '';
-$Password = isset($_POST['Pswd']) ? $_POST['Pswd']: ''; 
+$Username = isset($_POST['Username']) ? $_POST['Username'] : '';
+$Password = isset($_POST['Pswd']) ? $_POST['Pswd'] : '';
 $Ldap = new LdapConnect();
-$Connexion_Ldap_User = $Ldap->userConnect($Username,$Password);
+$Connexion_Ldap_User = $Ldap->userConnect($Username, $Password);
 
 $action = isset($_GET['action']) ? $_GET['action'] : 'default';
 switch ($action) {
     case 'Authentification':
-            if(!$Connexion_Ldap_User){
-                echo "error";
-            }else{
-                echo 'you are connected';
-            }
+        if (!$Connexion_Ldap_User) {
+            echo '<script type="text/javascript">
+                alert("Merci de vérifier votre session LDAP");
+                document.location.href = "/Hff_IntranetV01";
+            </script>';
+        } else {
+            session_start();
+            $_SESSION['user']= $Username;
+            include'Views/Principal.html';
+        }
         break;
     default:
-        include 'Views/SignIn.php';
+        include 'Views/SignIn.html';
+        break;
 }
