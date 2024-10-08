f80c3dfd73411acce5215f8f48675e710247135f
Andryrkt <andryrkt08@gmail.com>
2024-10-08 17:16:28 +0300

    commencement de l'affichage du dossier intervention atelier

---
+++ src/Controller/dit/DossierInterventionAtelierController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller\dit;
+
+use App\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DossierInterventionAtelierController extends Controller
+{
+    /**
+     * @Route("/dit-dossier-intervention-atelier", name="dit_dossier_intervention_atelier")
+     *
+     * @return void
+     */
+    public function dossierInterventionAtelier()
+    {
+        self::$twig->display('dit/dossierInterventionAtelier.html.twig');
+    }
+}
