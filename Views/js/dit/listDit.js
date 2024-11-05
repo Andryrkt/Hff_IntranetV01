fa9c3ccb0d401ebdf09fc9f7013de8367d49a30a
Andryrkt <andryrkt08@gmail.com>
2024-11-05 11:59:59 +0300

    ajout confirmation de cloturer un dit

--- Views/js/dit/listDit.js
+++ Views/js/dit/listDit.js
@@ -263,3 +263,30 @@ document.addEventListener("DOMContentLoaded", (event) => {
     tableBody.innerHTML = ""; // Vider le tableau
   });
 });
+
+/**
+ * sweetalert pur le bouron cloturer dit
+ */
+// const cloturerDitInput = document.querySelector("#cloturerDit");
+// cloturerDitInput.addEventListener("click", () => {
+//   alert("Bonjour");
+// });
+// function cloturerDit() {
+//   Swal.fire({
+//     title: "Are you sure?",
+//     text: "You won't be able to revert this!",
+//     icon: "warning",
+//     showCancelButton: true,
+//     confirmButtonColor: "#3085d6",
+//     cancelButtonColor: "#d33",
+//     confirmButtonText: "Yes, delete it!",
+//   }).then((result) => {
+//     if (result.isConfirmed) {
+//       Swal.fire({
+//         title: "Deleted!",
+//         text: "Your file has been deleted.",
+//         icon: "success",
+//       });
+//     }
+//   });
+// }
