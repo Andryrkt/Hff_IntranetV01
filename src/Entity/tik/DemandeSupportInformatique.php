bdda98337dab16aa42a5df251cfebcd7b253e7ec
Andryrkt <andryrkt08@gmail.com>
2024-11-06 10:10:39 +0300

    rectification de l'entity demande support informatique

--- src/Entity/tik/DemandeSupportInformatique.php
+++ src/Entity/tik/DemandeSupportInformatique.php
@@ -6,8 +6,7 @@ use App\Repository\tik\DemandeSupportInformatiqueRepository;
 
 /**
  * @ORM\Entity(repositoryClass=DemandeSupportInformatiqueRepository::class)
- * @ORM\Table(name="Demande_Support_Informatique")
- * @ORM\HasLifecyleCallbacks
+ * @ORM\Table(name="Demande_Support_Informatique")Date_Fin
  */
 class DemandeSupportInformatique
 {
@@ -19,94 +18,118 @@ class DemandeSupportInformatique
     private int $id;
 
     /**
-     * @ORM\Column(type="string", length=1000, name="Mail_Demandeur")
+     * @ORM\Column(type="datetime", name="Date_Creation")
      */
-    private string $Mail_Demandeur;
+    private $dateCreation;
+
+    /**
+     * @ORM\Column(type="string", length=50, name="Utilisateur_Demandeur")
+     */
+    private string $utilisateurDemandeur;
+
+    /**
+     * @ORM\Column(type="string", length=50, name="Mail_Demandeur")
+     */
+    private string $mailDemandeur;
+
+    /**
+     * @ORM\Column(type="string", length=1000, name="Mail_En_Copie")
+     */
+    private string $mailEnCopie;
+
+    /**
+     * @ORM\Column(type="string", length=2, name="Code_Societe")
+     */
+    private string $codeSociete;
 
     /**
      * @ORM\Column(type="integer", name="ID_TKI_Categorie")
+     * TODO : RELATION A FAITE
      */
-    private int $Categorie;
+    private int $categorie;
 
     /**
      * @ORM\Column(type="integer", name="ID_TKI_Sous_Categorie")
+     * TODO : RELATION A FAITE
      */
-    private int $Sous_Categorie;
+    private int $sousCategorie;
 
     /**
      * @ORM\Column(type="integer", nullable=true, name="ID_TKI_Autres_Categorie")
+     * TODO : RELATION A FAITE
      */
-    private ?int $Autres_Categorie = null;
+    private ?int $autresCategorie = null;
 
     /**
      * @ORM\Column(type="string", length=100, name="AgenceService_Emetteur")
      */
-    private string $Agence_Emetteur;
+    private string $agenceServiceEmetteur;
 
     /**
      * @ORM\Column(type="string", length=100, name="AgenceService_Debiteur")
      */
-    private string $Agence_Debiteur;
+    private string $agenceServiceDebiteur;
 
     /**
-     * @ORM\Column(type="string", length=100, name="AgenceService_Debiteur")
+     * @ORM\Column(type="string", length=100, name="Mail_Intervenant")
      */
-    private string $Service_Emetteur;
+    private string $nomIntervenant;
 
     /**
-     * @ORM\Column(type="string", length=100, name="AgenceService_Debiteur")
+     * @ORM\Column(type="string", length=100, name="Nom_Intervenant")
      */
-    private string $Service_Debiteur;
+    private ?string $mailIntervenant = null;
 
     /**
-     * @ORM\Column(type="string", length=200, nullable=true, name="Mail_Intervenant")
+     * @ORM\Column(type="string", length=100, name="Objet_Demande")
      */
-    private ?string $Mail_Intervenant = null;
+    private string $objetDemande;
 
     /**
-     * @ORM\Column(type="string", length=200, name="Objet_Demande")
+     * @ORM\Column(type="string", length=5000, name="Detail_Demande")
      */
-    private string $Objet_Demande;
-
-    /**
-     * @ORM\Column(type="text", name="Detail_Demande")
-     */
-    private string $Detail_Demande;
+    private string $detailDemande;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true, name="Piece_Jointe1")
      */
-    private ?string $Piece_Jointe1 = null;
+    private ?string $pieceJointe1 = null;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true, name="Piece_Jointe2")
      */
-    private ?string $Piece_Jointe2 = null;
+    private ?string $pieceJointe2 = null;
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true, name="Piece_Jointe3")
      */
-    private ?string $Piece_Jointe3 = null;
+    private ?string $pieceJointe3 = null;
+
+     /**
+     * @ORM\Column(type="datetime", name="Date_Deb_Planning")
+     */
+    private $dateDebutPlanning;
 
     /**
      * @ORM\Column(type="datetime", name="Date_Fin_Planning")
      */
-    private DateTime $Date_Fin_Planning;
+    private $dateFinPlanning;
 
     /**
      * @ORM\Column(type="integer", name="ID_Niveau_Urgence")
+     * TODO : RELATIION A FAIRE
      */
-    private int $Niveau_Urgence;
+    private int $niveauUrgence;
 
     /**
      * @ORM\Column(type="string", length=50, name="Parc_Informatique")
      */
-    private string $Parc_Informatique;
+    private string $parcInformatique;
 
     /**
      * @ORM\Column(type="datetime", name="Date_Fin_Souhaitee")
      */
-    private DateTime $Date_Fin_Souhaitee;
+    private $dateFinSouhaitee;
 
 
     /**=====================================================================================
@@ -123,355 +146,5 @@ class DemandeSupportInformatique
         return $this->id;
     }
 
-    /**
-     * Set the value of id
-     *
-     * @return  self
-     */ 
-    public function setId($id)
-    {
-        $this->id = $id;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Mail_Demandeur
-     */ 
-    public function getMail_Demandeur()
-    {
-        return $this->Mail_Demandeur;
-    }
-
-    /**
-     * Set the value of Mail_Demandeur
-     *
-     * @return  self
-     */ 
-    public function setMail_Demandeur($Mail_Demandeur)
-    {
-        $this->Mail_Demandeur = $Mail_Demandeur;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Mail_Intervenant
-     */ 
-    public function getMail_Intervenant()
-    {
-        return $this->Mail_Intervenant;
-    }
-
-    /**
-     * Set the value of Mail_Intervenant
-     *
-     * @return  self
-     */ 
-    public function setMail_Intervenant($Mail_Intervenant)
-    {
-        $this->Mail_Intervenant = $Mail_Intervenant;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Objet_Demande
-     */ 
-    public function getObjet_Demande()
-    {
-        return $this->Objet_Demande;
-    }
-
-    /**
-     * Set the value of Objet_Demande
-     *
-     * @return  self
-     */ 
-    public function setObjet_Demande($Objet_Demande)
-    {
-        $this->Objet_Demande = $Objet_Demande;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Detail_Demande
-     */ 
-    public function getDetail_Demande()
-    {
-        return $this->Detail_Demande;
-    }
-
-    /**
-     * Set the value of Detail_Demande
-     *
-     * @return  self
-     */ 
-    public function setDetail_Demande($Detail_Demande)
-    {
-        $this->Detail_Demande = $Detail_Demande;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Piece_Jointe1
-     */ 
-    public function getPiece_Jointe1()
-    {
-        return $this->Piece_Jointe1;
-    }
-
-    /**
-     * Set the value of Piece_Jointe1
-     *
-     * @return  self
-     */ 
-    public function setPiece_Jointe1($Piece_Jointe1)
-    {
-        $this->Piece_Jointe1 = $Piece_Jointe1;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Piece_Jointe2
-     */ 
-    public function getPiece_Jointe2()
-    {
-        return $this->Piece_Jointe2;
-    }
-
-    /**
-     * Set the value of Piece_Jointe2
-     *
-     * @return  self
-     */ 
-    public function setPiece_Jointe2($Piece_Jointe2)
-    {
-        $this->Piece_Jointe2 = $Piece_Jointe2;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Piece_Jointe3
-     */ 
-    public function getPiece_Jointe3()
-    {
-        return $this->Piece_Jointe3;
-    }
-
-    /**
-     * Set the value of Piece_Jointe3
-     *
-     * @return  self
-     */ 
-    public function setPiece_Jointe3($Piece_Jointe3)
-    {
-        $this->Piece_Jointe3 = $Piece_Jointe3;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Date_Fin_Planning
-     */ 
-    public function getDate_Fin_Planning()
-    {
-        return $this->Date_Fin_Planning;
-    }
-
-    /**
-     * Set the value of Date_Fin_Planning
-     *
-     * @return  self
-     */ 
-    public function setDate_Fin_Planning($Date_Fin_Planning)
-    {
-        $this->Date_Fin_Planning = $Date_Fin_Planning;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Parc_Informatique
-     */ 
-    public function getParc_Informatique()
-    {
-        return $this->Parc_Informatique;
-    }
-
-    /**
-     * Set the value of Parc_Informatique
-     *
-     * @return  self
-     */ 
-    public function setParc_Informatique($Parc_Informatique)
-    {
-        $this->Parc_Informatique = $Parc_Informatique;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Date_Fin_Souhaitee
-     */ 
-    public function getDate_Fin_Souhaitee()
-    {
-        return $this->Date_Fin_Souhaitee;
-    }
-
-    /**
-     * Set the value of Date_Fin_Souhaitee
-     *
-     * @return  self
-     */ 
-    public function setDate_Fin_Souhaitee($Date_Fin_Souhaitee)
-    {
-        $this->Date_Fin_Souhaitee = $Date_Fin_Souhaitee;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Niveau_Urgence
-     */ 
-    public function getNiveau_Urgence()
-    {
-        return $this->Niveau_Urgence;
-    }
-
-    /**
-     * Set the value of Niveau_Urgence
-     *
-     * @return  self
-     */ 
-    public function setNiveau_Urgence($Niveau_Urgence)
-    {
-        $this->Niveau_Urgence = $Niveau_Urgence;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Categorie
-     */ 
-    public function getCategorie()
-    {
-        return $this->Categorie;
-    }
-
-    /**
-     * Set the value of Categorie
-     *
-     * @return  self
-     */ 
-    public function setCategorie($Categorie)
-    {
-        $this->Categorie = $Categorie;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Sous_Categorie
-     */ 
-    public function getSous_Categorie()
-    {
-        return $this->Sous_Categorie;
-    }
-
-    /**
-     * Set the value of Sous_Categorie
-     *
-     * @return  self
-     */ 
-    public function setSous_Categorie($Sous_Categorie)
-    {
-        $this->Sous_Categorie = $Sous_Categorie;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Autres_Categorie
-     */ 
-    public function getAutres_Categorie()
-    {
-        return $this->Autres_Categorie;
-    }
-
-    /**
-     * Set the value of Autres_Categorie
-     *
-     * @return  self
-     */ 
-    public function setAutres_Categorie($Autres_Categorie)
-    {
-        $this->Autres_Categorie = $Autres_Categorie;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Agence_Emetteur
-     */ 
-    public function getAgence_Emetteur()
-    {
-        return $this->Agence_Emetteur;
-    }
-
-    /**
-     * Set the value of Agence_Emetteur
-     *
-     * @return  self
-     */ 
-    public function setAgence_Emetteur($Agence_Emetteur)
-    {
-        $this->Agence_Emetteur = $Agence_Emetteur;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Agence_Debiteur
-     */ 
-    public function getAgence_Debiteur()
-    {
-        return $this->Agence_Debiteur;
-    }
-
-    /**
-     * Set the value of Agence_Debiteur
-     *
-     * @return  self
-     */ 
-    public function setAgence_Debiteur($Agence_Debiteur)
-    {
-        $this->Agence_Debiteur = $Agence_Debiteur;
-
-        return $this;
-    }
-
-    /**
-     * Get the value of Service_Debiteur
-     */ 
-    public function getService_Debiteur()
-    {
-        return $this->Service_Debiteur;
-    }
-
-    /**
-     * Set the value of Service_Debiteur
-     *
-     * @return  self
-     */ 
-    public function setService_Debiteur($Service_Debiteur)
-    {
-        $this->Service_Debiteur = $Service_Debiteur;
-
-        return $this;
-    }
+   
 }
