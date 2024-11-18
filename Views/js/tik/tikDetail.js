f2b98e9fdcb05df881449f23df60c5c3fc3cc590
Menja <ranofimenjajam@gmail.com>
2024-11-18 15:15:29 +0300

    commit bannal

--- Views/js/tik/tikDetail.js
+++ Views/js/tik/tikDetail.js
@@ -6,6 +6,25 @@ const categorieInput = document.querySelector(".categorie");
 const sousCategorieInput = document.querySelector(".sous-categorie");
 const autreCategorieInput = document.querySelector(".autre-categorie");
 
+document.addEventListener("DOMContentLoaded", function () {
+  const idServiceIntervenant = document.querySelector(
+    "#dit_validation_idServiceIntervenant"
+  );
+  const codeSection = document.querySelector("#dit_validation_codeSection");
+  const validerBtn = document.querySelector("#btn_valider");
+  const refuserBtn = document.querySelector("#btn_refuser");
+
+  refuserBtn.addEventListener("click", function () {
+    idServiceIntervenant.removeAttribute("required");
+    codeSection.removeAttribute("required");
+  });
+
+  validerBtn.addEventListener("click", function () {
+    idServiceIntervenant.setAttribute("required", "required");
+    codeSection.setAttribute("required", "required");
+  });
+});
+
 //AFFICHAGE SOUS CATEGORIES
 categorieInput.addEventListener("change", selectCategorieSousCategorie);
 
