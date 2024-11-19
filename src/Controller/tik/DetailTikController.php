933b223696b73cc07da0ae4602e9001cd5bf746e
Menja <ranofimenjajam@gmail.com>
2024-11-19 16:00:28 +0300

    modification du controleur de detailTik

--- src/Controller/tik/DetailTikController.php
+++ src/Controller/tik/DetailTikController.php
@@ -4,10 +4,12 @@ namespace App\Controller\tik;
 
 use App\Controller\Controller;
 use App\Entity\admin\StatutDemande;
+use App\Entity\admin\tik\TkiCommentaires;
+use App\Entity\admin\tik\TkiStatutTicketInformatique;
 use App\Entity\admin\utilisateur\User;
 use App\Entity\tik\DemandeSupportInformatique;
 use App\Form\tik\DetailTikType;
-use App\Repository\admin\utilisateur\UserRepository;
+use App\Repository\admin\StatutDemandeRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -24,10 +26,9 @@ class DetailTikController extends Controller
         $supportInfo = self::$em->getRepository(DemandeSupportInformatique::class)->find($id);
 
         /** 
-         * @var User $user l'utilisateur connecté
+         * @var User $connectedUser l'utilisateur connecté
          */
-        $user = self::$em->getRepository(User::class)->find($this->sessionService->get('user_id'));
-        $statutDemande = self::$em->getRepository(StatutDemande::class);
+        $connectedUser = self::$em->getRepository(User::class)->find($this->sessionService->get('user_id'));
 
         if (!$supportInfo) {
             self::$twig->display('404.html.twig');
@@ -41,28 +42,109 @@ class DetailTikController extends Controller
                  * @var DemandeSupportInformatique $dataForm l'entité du DemandeSupportInformatique envoyé par le formualire de validation
                  */
                 $dataForm = $form->getData();
-                
-                $supportInfo
-                    ->setNomIntervenant($dataForm->getIntervenant()->getNomUtilisateur())
-                    ->setMailIntervenant($dataForm->getIntervenant()->getMail())
-                ;
-                
-                //envoi les donnée dans la base de donnée
-                self::$em->persist($supportInfo);
-                self::$em->flush(); 
 
-                $this->sessionService->set('notification',['type' => 'success', 'message' => 'La validation a été enregistrée']);
+                $button = $this->getButton($request);
+
+                switch ($button['action']) {
+                    case 'refuser':
+                        $commentaires = new TkiCommentaires;
+                        $commentaires
+                            ->setNumeroTicket($dataForm->getNumeroTicket())
+                            ->setNomUtilisateur($connectedUser->getNomUtilisateur())
+                            ->setCommentaires($form->get('commentaires')->getData())
+                            ->setUtilisateur($connectedUser)
+                            ->setDemandeSupportInformatique($supportInfo)
+                        ;
+
+                        $supportInfo
+                            ->setIdStatutDemande($button['statut'])    // statut refusé
+                        ;
+
+                        self::$em->persist($commentaires);
+                        self::$em->persist($supportInfo);
+
+                        self::$em->flush();
+
+                        break;
+
+                    case 'valider':
+                        $supportInfo
+                            ->setNomIntervenant($dataForm->getIntervenant()->getNomUtilisateur())
+                            ->setMailIntervenant($dataForm->getIntervenant()->getMail())
+                            ->setIdStatutDemande($button['statut'])    // statut en cours
+                        ;
+                        
+                        //envoi les donnée dans la base de donnée
+                        self::$em->persist($supportInfo);
+                        self::$em->flush(); 
+
+                        $this->historiqueStatut($supportInfo, $button['statut']);
+        
+                        $this->sessionService->set('notification',['type' => 'success', 'message' => 'La validation a été enregistrée']);
+                        break;
+
+                    case 'transferer':
+                        # code...
+                        break;
+
+                    case 'planifier':
+                        # code...
+                        break;
+                }
+                
+                
                 $this->redirectToRoute("liste_tik_index");
             }
 
             self::$twig->display('tik/demandeSupportInformatique/detail.html.twig', [
                 'tik'        => $supportInfo,
                 'form'       => $form->createView(),
-                'autoriser'  => !empty(array_intersect(["INTERVENANT", "VALIDATEUR"], $user->getRoleNames())),  // vérfifie si parmi les roles de l'utilisateur on trouve "INTERVENANT" ou "VALIDATEUR"
-                'validateur' => in_array("VALIDATEUR", $user->getRoleNames())                                   // vérfifie si parmi les roles de l'utilisateur on trouve "VALIDATEUR"
+                'autoriser'  => !empty(array_intersect(["INTERVENANT", "VALIDATEUR"], $connectedUser->getRoleNames())),  // vérfifie si parmi les roles de l'utilisateur on trouve "INTERVENANT" ou "VALIDATEUR"
+                'validateur' => in_array("VALIDATEUR", $connectedUser->getRoleNames())                                   // vérfifie si parmi les roles de l'utilisateur on trouve "VALIDATEUR"
             ]);
+        } 
+    }
+
+    /** 
+     * fonction qui retourne l'action du bouton cliqué dans le formulaire
+     */
+    private function getButton(Request $request)
+    {
+        $actions = [
+            '80' => 'refuser',      // statut Refusé
+            '81' => 'valider',      // statut en cours
+            '82' => 'planifier',    // statut planifié
+            '00' => 'transferer',   
+        ];
+
+        /** 
+         * @var StatutDemandeRepository $statutDemande repository pour StatutDemande
+         */
+        $statutDemande = self::$em->getRepository(StatutDemande::class);
+
+        // Trouver la clé correspondante
+        foreach ($actions as $code => $action) {
+            if ($request->request->has($action)) {
+                return [
+                    'statut' => $statutDemande->find($code),
+                    'action' => $action
+                ];
+            }
         }
-        
+    }
+
+    /** 
+     * fonction pour historiser le statut du ticket
+     */
+    private function historiqueStatut($supportInfo, $statut)
+    {
+        $tikStatut = new TkiStatutTicketInformatique();
+        $tikStatut
+            ->setNumeroTicket($supportInfo->getNumeroTicket())
+            ->setCodeStatut($statut->getCodeStatut())
+            ->setIdStatutDemande($statut)
+        ;
+        self::$em->persist($tikStatut);
+        self::$em->flush();
     }
 }
-
