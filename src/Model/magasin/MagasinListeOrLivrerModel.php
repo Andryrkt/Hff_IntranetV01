6b1831eb683963e9a8a8156aff09dba777ef37ac
hasimanjaka <108670520+ratompoarinandro31@users.noreply.github.com>
2024-11-22 08:28:06 +0300

    rectification sevauchement

--- src/Model/magasin/MagasinListeOrLivrerModel.php
+++ src/Model/magasin/MagasinListeOrLivrerModel.php
@@ -146,7 +146,7 @@ class MagasinListeOrLivrerModel extends Model
                         and slor_soc = 'HF'
                             AND slor_constp NOT LIKE 'Z%'
                             AND slor_constp NOT IN ('LUB')
-                            AND slor_succ = '01'
+                           -- AND slor_succ = '01'
                             AND seor_serv ='SAV'
                         and slor_numor in ('".$numOrValide."')
                         and seor_numor||'-'||TRUNC(slor_nogrp/100) in ('".$numOrValideItv."')
@@ -224,7 +224,7 @@ class MagasinListeOrLivrerModel extends Model
 
                         where slor_soc = 'HF'
                         and slor_typlig = 'P'
-                        and slor_succ = '01'
+                        --and slor_succ = '01'
                         and seor_serv ='SAV'
                         and seor_typeor not in('950', '501')
                         and seor_numor||'-'||TRUNC(slor_nogrp/100) in ('".$lesOrSelonCondition['numOrValideString']."')
@@ -266,7 +266,7 @@ class MagasinListeOrLivrerModel extends Model
             and seor_numor = slor_numor
             where 
             slor_soc = 'HF'
-            and slor_succ = '01'
+           -- and slor_succ = '01'
             and slor_typlig = 'P'
     	    and slor_constp <> '---'
             and slor_constp not like 'Z%'
@@ -335,7 +335,7 @@ class MagasinListeOrLivrerModel extends Model
             where 
             slor_soc = 'HF'
 
-            and slor_succ = '01'
+           -- and slor_succ = '01'
             and slor_desi like '%" . $designations . "%'
             and slor_typlig = 'P'
             and slor_pos = 'EC'
@@ -362,7 +362,7 @@ class MagasinListeOrLivrerModel extends Model
             
             where 
             slor_soc = 'HF'
-            and slor_succ = '01'
+           -- and slor_succ = '01'
             and slor_refp like '%" . $refPiece . "%'
             and slor_typlig = 'P'
             and slor_pos = 'EC'
