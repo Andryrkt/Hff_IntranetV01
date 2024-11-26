9f2de2c99e839b4e90bc96fc8b122f54133e58b6
Andryrkt <andryrkt08@gmail.com>
2024-11-26 06:36:35 +0300

    creation d'entité et repository

--- src/Controller/dit/DitCdeSoumisAValidationController.php
+++ src/Controller/dit/DitCdeSoumisAValidationController.php
@@ -3,6 +3,20 @@
 namespace App\Controller\dit;
 
 use App\Controller\Controller;
+use App\Entity\dit\DitCdeSoumisAValidation;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DitCdeSoumisAValidationController extends Controller
-{}
+{
+    /**
+     * @Route("/insertion-cde/{numDit}", name="dit_insertion_cde")
+     */
+    public function cdeSoumisAValidation(Request $request, $numDit)
+    {
+        //verification si user connecter
+        $this->verifierSessionUtilisateur();
+
+        $ditCdeSoumisAValidation = new DitCdeSoumisAValidation();
+    }
+}
