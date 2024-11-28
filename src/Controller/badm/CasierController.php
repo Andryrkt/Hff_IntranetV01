8e79640a9d702f1b078561502584837ed65f57fb
Andryrkt <andryrkt08@gmail.com>
2024-11-28 11:55:36 +0300

    reorganisation ducode dom , badm

--- src/Controller/badm/CasierController.php
+++ src/Controller/badm/CasierController.php
@@ -45,8 +45,6 @@ class CasierController extends Controller
             $form = self::$validator->createBuilder(CasierForm1Type::class, $casier)->getForm();
             
             $form->handleRequest($request);
-            
-        
             if($form->isSubmitted() && $form->isValid())
             {
                 $casierModel = new CasierModel();
@@ -183,50 +181,7 @@ class CasierController extends Controller
         ];
     }
     
-    /**
-     * @Route("/casierDestinataire", name="badm_casierDestinataire")
-     */
-    public function casierDestinataire()
-    {
-        //verification si user connecter
-        $this->verifierSessionUtilisateur();
-        
-        $casierDestinataireInformix = $this->badm->recupeCasierDestinataireInformix();
-         //$casierDestinataireSqlServer = $this->badm->recupeCasierDestinataireSqlServer();
-        $casierDestinataire = self::$em->getRepository(CasierValider::class)->findAll();
-
-        $casierDestinataireSqlServer = [];
-        foreach ($casierDestinataire as $value) {
-            $casierDestinataireSqlServer[] = [
-                'Agence_Rattacher' => $value->getAgenceRattacher()->getCodeAgence(),
-                'Casier' => $value->getCasier()
-            ];
-        }
-
-        // Combinaison des deux tableaux
-        $resultat = [];
-
-        foreach ($casierDestinataireInformix as $agence) {
-            foreach ($casierDestinataireSqlServer as $casier) {
-
-                if ($casier['Agence_Rattacher'] == $agence['code_agence']) {
-
-                    $resultat[$agence['agence']][] = $casier['Casier'];
-                }
-            }
-
-            //Assurez-vous que chaque agence est présente même si elle n'a pas de casiers
-            if (!array_key_exists($agence['agence'], $resultat)) {
-                $resultat[$agence['agence']] = [];
-            }
-        }
-
-        header("Content-type:application/json");
-
-        $jsonData = json_encode($resultat);
-
-        $this->testJson($jsonData);
-    }
+    
 
 
     private function alertRedirection(string $message, string $chemin = "/Hffintranet/nouveauCasier")
