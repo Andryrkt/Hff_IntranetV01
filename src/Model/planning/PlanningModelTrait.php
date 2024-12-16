af0c2923b30eda07fc8c39fbdb0c8f4b203a56a9
lanto_hasina <andryrkt08@gmail.com>
2024-12-16 16:03:18 +0300

    changement de la fonction typeLIgne et sumPiece dans planningModeltrait

--- src/Model/planning/PlanningModelTrait.php
+++ src/Model/planning/PlanningModelTrait.php
@@ -32,6 +32,7 @@ trait PlanningModelTrait
 
       return $vStatutFacture; 
     }
+
     private function section($criteria){
       if(!empty($criteria->getSection())){
         $section = " AND sitv_typitv = '".$criteria->getSection()."' ";
@@ -40,20 +41,21 @@ trait PlanningModelTrait
       }
       return $section;
     }
-    private function typeLigne($criteria){
+
+   private function typeLigne($criteria){
       
       switch ($criteria->getTypeLigne()) {
         case "TOUTES": 
             $vtypeligne = " ";
             break;
         case "PIECES_MAGASIN":
-            $vtypeligne = "  AND  slor_constp  not in ('LUB', 'SOM', 'FAT', 'PDV', 'JOV')  AND slor_constp not like 'Z%' AND slor_typlig = 'P' ";
+            $vtypeligne = " AND slor_constp in ('AGR','ATC','AUS','CAT','CGM','CMX','DNL','DYN','GRO','HYS','JDR','KIT','LUB','MAN','MNT','OLY','OOM','PAR','PDV','PER','PUB','REM','SHM','TBI','THO') AND slor_typlig = 'P' ";
             break;
         case "ACHAT_LOCAUX":
-            $vtypeligne = " AND (slor_constp in ('SOM', 'FAT', 'PDV') or slor_constp like 'Z%')" ;
+            $vtypeligne = " AND slor_constp in ('ALI','BOI','CAR','CEN','FAT','FBU','HAB','INF','MIN','OUT','ZST')" ;
             break;
         case "LUBRIFIANTS":
-            $vtypeligne = " AND slor_constp in ('LUB', 'JOV')  AND slor_typlig = 'P'";
+            $vtypeligne = " AND slor_constp in ('LUB', 'SOM', 'FAT', 'PDV')  AND slor_typlig = 'P'";
             break;
         default:
             $vtypeligne = " ";
@@ -62,19 +64,19 @@ trait PlanningModelTrait
     }
     
     private function sumPieces($criteria){
-
+      
       switch ($criteria->getTypeLigne()) {
         case "TOUTES": 
             $vPieces = " ";
             break;
         case "PIECES_MAGASIN":
-            $vPieces = "AND  slor_constp  not in ('LUB', 'SOM', 'FAT', 'PDV', 'JOV')  AND slor_constp not like 'Z%' AND slor_typlig = 'P'";
+            $vPieces = " AND slor_constp in ('AGR','ATC','AUS','CAT','CGM','CMX','DNL','DYN','GRO','HYS','JDR','KIT','LUB','MAN','MNT','OLY','OOM','PAR','PDV','PER','PUB','REM','SHM','TBI','THO') AND slor_typlig = 'P'";
             break;
         case "ACHAT_LOCAUX":
-            $vPieces = "AND (slor_constp in ('SOM', 'FAT', 'PDV') or slor_constp like 'Z%')" ;
+            $vPieces = " AND slor_constp in ('ALI','BOI','CAR','CEN','FAT','FBU','HAB','INF','MIN','OUT','ZST')" ;
             break;
         case "LUBRIFIANTS":
-            $vPieces = "AND slor_constp in ('LUB', 'JOV')  AND slor_typlig = 'P'";
+            $vPieces = " AND slor_constp in ('LUB', 'SOM', 'FAT', 'PDV')  AND slor_typlig = 'P'";
             break;
         default:
             $vPieces = " ";
