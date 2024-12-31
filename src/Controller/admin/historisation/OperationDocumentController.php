c56f3149e3fa682e0f86c15a18d9be4d3bc0a5a3
Menja <ranofimenjajam@gmail.com>
2024-12-31 11:50:24 +0300

    ajout de route dans le controleur

--- src/Controller/admin/historisation/OperationDocumentController.php
+++ src/Controller/admin/historisation/OperationDocumentController.php
@@ -16,4 +16,24 @@ class OperationDocumentController extends Controller
             'admin/historisation/operation-document/index.html.twig'
         );
     }
+
+    /**
+     * @Route("/admin/operation-document/dashboard", name="operation_document_dashboard")
+     */
+    public function dashboard()
+    {
+        self::$twig->display(
+            'admin/historisation/operation-document/dashboard.html.twig'
+        );
+    }
+
+    /**
+     * @Route("/admin/operation-document/detail", name="operation_document_detail")
+     */
+    public function detail()
+    {
+        self::$twig->display(
+            'admin/historisation/operation-document/detail.html.twig'
+        );
+    }
 }
--- src/Controller/admin/historisation/PageConsultationController.php
+++ src/Controller/admin/historisation/PageConsultationController.php
@@ -16,4 +16,24 @@ class PageConsultationController extends Controller
             'admin/historisation/consultation-page/index.html.twig'
         );
     }
+
+    /**
+     * @Route("/admin/consultation-page/dashboard", name="consultation_page_dashboard")
+     */
+    public function dashboard()
+    {
+        self::$twig->display(
+            'admin/historisation/consultation-page/dashboard.html.twig'
+        );
+    }
+
+    /**
+     * @Route("/admin/consultation-page/detail", name="consultation_page_detail")
+     */
+    public function detail()
+    {
+        self::$twig->display(
+            'admin/historisation/consultation-page/detail.html.twig'
+        );
+    }
 }
