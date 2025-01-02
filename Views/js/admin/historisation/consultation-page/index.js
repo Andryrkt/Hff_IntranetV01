502fc95dafee443c321531019fa08dd10c6aae12
Menja <ranofimenjajam@gmail.com>
2025-01-02 10:53:59 +0300

    Ajout des données dans l' index (consultation de page)

--- Views/js/admin/historisation/consultation-page/index.js
+++ Views/js/admin/historisation/consultation-page/index.js
@@ -1,11 +1,20 @@
-const consultations = []; // Données JSON injectées par le back-end
+import { fetchData } from '../../../tik/utils/fetchUtils';
 
-function applyFilters() {
+const consultations = await fetchData(
+  '/Hffintranet/api/consultation-page-fetch-all'
+); // Données JSON injectées par le back-end
+
+const buttonFilter = document.getElementById('filter');
+
+buttonFilter.addEventListener('click', () => {
   const userFilter = document.getElementById('filterUser').value.toLowerCase();
   const pageFilter = document.getElementById('filterPage').value.toLowerCase();
-  const dateFilter = document.getElementById('filterDate').value;
+  const dateFilter = format(document.getElementById('filterDate').value);
 
   const filtered = consultations.filter((item) => {
+    console.log(item.date);
+    console.log(dateFilter);
+    console.log(item.date.startsWith(dateFilter));
     return (
       (!userFilter || item.user.toLowerCase().includes(userFilter)) &&
       (!pageFilter || item.page.toLowerCase().includes(pageFilter)) &&
@@ -14,24 +23,43 @@ function applyFilters() {
   });
 
   renderTable(filtered);
-}
+});
 
 function renderTable(data) {
   const tableBody = document.getElementById('consultationTable');
   tableBody.innerHTML = '';
 
-  data.forEach((item) => {
-    const row = `
-                <tr>
-                    <td>${item.user}</td>
-                    <td>${item.page}</td>
-                    <td>${item.date}</td>
-                    <td>${item.params}</td>
-                    <td>${item.machine}</td>
-                </tr>
-            `;
-    tableBody.innerHTML += row;
-  });
+  if (data.length == 0) {
+    console.log('if');
+    console.log(data.length);
+    tableBody.innerHTML += `<tr><td colspan="5" class="text-center">Aucun résultat</td></tr>`;
+  } else {
+    console.log('else');
+    console.log(data.length);
+    data.forEach((item) => {
+      const row = `
+                  <tr>
+                      <td>${item.user}</td>
+                      <td>${item.page}</td>
+                      <td>${item.date}</td>
+                      <td>${item.params}</td>
+                      <td>${item.machine}</td>
+                  </tr>
+              `;
+      tableBody.innerHTML += row;
+    });
+  }
+}
+
+function format(date) {
+  const dateObj = new Date(date);
+
+  // Obtenir les composants de la date
+  const jour = String(dateObj.getDate()).padStart(2, '0'); // Jour avec 2 chiffres (ex. 02)
+  const mois = String(dateObj.getMonth() + 1).padStart(2, '0'); // Mois avec 2 chiffres (01-12)
+  const annee = dateObj.getFullYear(); // Année
+
+  return `${jour}-${mois}-${annee}`;
 }
 
 // Initial rendering
