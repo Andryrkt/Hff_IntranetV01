5c36fe0eb6dc0469b09f0aeafc1cab9fb5ebd156
Menja <ranofimenjajam@gmail.com>
2025-01-02 16:27:41 +0300

    Mise à jour du spinner (operation document)

--- Views/js/admin/historisation/operation-document/index.js
+++ Views/js/admin/historisation/operation-document/index.js
@@ -1,28 +1,47 @@
 import { fetchData } from '../../../tik/utils/fetchUtils';
 
-const operations = await fetchData(
-  '/Hffintranet/api/operation-document-fetch-all'
-); // Données JSON injectées par le back-end
+const spinner = document.getElementById('spinner');
 
-function renderOperationsTable(data) {
-  const tableBody = document.getElementById('operationTable');
-  tableBody.innerHTML = '';
-
-  data.forEach((op) => {
-    const row = `
-                <tr>
-                    <td>${op.numeroDocument}</td>
-                    <td>${op.date}</td>
-                    <td>${op.username}</td>
-                    <td>${op.operationType}</td>
-                    <td>${op.documentType}</td>
-                    <td>${op.statut}</td>
-                    <td>${op.libelle}</td>
-                </tr>
-            `;
-    tableBody.innerHTML += row;
-  });
+// Fonction pour afficher et masquer le spinner
+function toggleSpinner(show) {
+  spinner.style.display = show ? 'block' : 'none';
 }
 
-// Initial rendering
-renderOperationsTable(operations);
+async function loadOperations() {
+  toggleSpinner(true); // Afficher le spinner pendant la récupération des données
+  const operations = await fetchData(
+    '/Hffintranet/api/operation-document-fetch-all'
+  ); // Données JSON injectées par le back-end
+  toggleSpinner(false); // Masquer le spinner après la récupération des données
+  return operations;
+}
+
+// Initialisation des opérations
+let operations = [];
+loadOperations().then((data) => {
+  operations = data;
+  renderTable(operations);
+});
+
+function renderTable(data) {
+  const tableBody = document.getElementById('operationTable');
+  tableBody.innerHTML =
+    data.length === 0
+      ? '<tr><td colspan="7" class="text-center">Aucun résultat</td></tr>'
+      : data.map((item) => createTableRow(item)).join('');
+}
+
+// Création d'une ligne de tableau
+function createTableRow(item) {
+  return `
+    <tr>
+      <td>${item.numeroDocument}</td>
+      <td>${item.date}</td>
+      <td>${item.username}</td>
+      <td>${item.operationType}</td>
+      <td>${item.documentType}</td>
+      <td>${item.statut}</td>
+      <td>${item.libelle}</td>
+    </tr>
+  `;
+}
