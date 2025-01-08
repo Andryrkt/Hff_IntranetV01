ce223abebe4fa6ba9a54cc746debc406c5e748c1
Menja <ranofimenjajam@gmail.com>
2025-01-08 13:48:34 +0300

    MAJ requête SQL

--- sql/tik/statut.sql
+++ sql/tik/statut.sql
@@ -13,7 +13,7 @@ CREATE TABLE STATUT_DEMANDE(
 INSERT INTO STATUT_DEMANDE (Code_Application, Code_Statut, Description, Date_creation, date_modification)
     VALUES
         ('TKI', 'OUV', 'OUVERT',  GETDATE (),  GETDATE ()),
-        ('TKI', 'REF', 'REFUDE',  GETDATE (),  GETDATE ()),
+        ('TKI', 'REF', 'REFUSE',  GETDATE (),  GETDATE ()),
         ('TKI', 'ENC', 'ENCOURS',  GETDATE (),  GETDATE ()),
         ('TKI', 'PLA', 'PLANIFIE',  GETDATE (),  GETDATE ()),
         ('TKI', 'RES', 'RESOLU',  GETDATE (),  GETDATE ()),
