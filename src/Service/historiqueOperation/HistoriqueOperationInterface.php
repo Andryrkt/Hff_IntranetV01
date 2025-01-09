706c557d9d0548d267c1c71955e651342ed3a175
Menja <ranofimenjajam@gmail.com>
2025-01-09 10:35:08 +0300

    MAj de l'interface HistoriqueOperationInterface

--- src/Service/historiqueOperation/HistoriqueOperationInterface.php
+++ src/Service/historiqueOperation/HistoriqueOperationInterface.php
@@ -7,9 +7,8 @@ interface HistoriqueOperationInterface
     /** 
      * @param string $numeroDocument numéro du document
      * @param int $typeOperationId id de l'opération effectué
-     * @param int $typeDocumentId id du type de document 
-     * @param string $statutOperation statut de l'opération 
+     * @param bool $succes statut de l'opération 
      * @param string $libelleOperation libellé de l'opération
      */
-    public function enregistrer(string $numeroDocument, int $typeOperationId, int $typeDocumentId, string $statutOperation, ?string $libelleOperation = null): void;
+    public function enregistrer(string $numeroDocument, int $typeOperationId, bool $succes, ?string $libelleOperation = null): void;
 }
