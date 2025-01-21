af87265b201ecf3192ff6308fa4e13e05763028d
Menja <ranofimenjajam@gmail.com>
2025-01-21 15:43:10 +0300

    Reformularisation de la logique

--- src/Controller/tik/DetailTikController.php
+++ src/Controller/tik/DetailTikController.php
@@ -4,6 +4,7 @@ namespace App\Controller\tik;
 
 use App\Controller\Controller;
 use App\Controller\Traits\lienGenerique;
+use App\Controller\Traits\tik\EnvoiFichier;
 use App\Entity\admin\StatutDemande;
 use App\Entity\admin\tik\TkiCommentaires;
 use App\Entity\admin\tik\TkiStatutTicketInformatique;
@@ -13,14 +14,22 @@ use App\Entity\tik\TkiPlanning;
 use App\Form\admin\tik\TkiCommentairesType;
 use App\Form\tik\DetailTikType;
 use App\Repository\admin\StatutDemandeRepository;
-use App\Service\EmailService;
-use App\Service\fichier\FileUploaderService;
+use App\Service\tik\EmailTikService;
+use App\Service\tik\HandleRequestService;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DetailTikController extends Controller
 {
     use lienGenerique;
+    use EnvoiFichier;
+    private $emailTikService;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->emailTikService = new EmailTikService;
+    }
 
     /**  
      * @Route("/tik-detail/{id<\d+>}", name="detail_tik")
@@ -40,39 +49,7 @@ class DetailTikController extends Controller
          */
         $connectedUser = self::$em->getRepository(User::class)->find($this->sessionService->get('user_id'));
 
-        /** 
-         * @var User $demandeur l'utilisateur qui a fait la demande de support info
-         */
-        $demandeur   = $supportInfo->getUserId();
-
-        /** 
-         * @var User $validateur l'utilisateur qui a validé ou refusé la demande
-         */
-        $validateur  = $supportInfo->getValidateur();
-
-        /** 
-         * @var User $intervenant l'utilisateur qui a été assigné à la demande
-         */
-        $intervenant = $supportInfo->getIntervenant();
-
-        /** 
-         * @var array $authorizedUsers les utilisateurs autorisés à commenter
-         */
-        $authorizedUsers = [$demandeur->getId(),];
-
-        if ($validateur !== null) {
-            $authorizedUsers[] = $validateur->getId();
-        }
-        if ($intervenant !== null) {
-            $authorizedUsers[] = $intervenant->getId();
-        }
-
-        /** 
-         * Vérifie si l'utilisateur connecté peut commenter.
-         * 
-         * @var bool $canComment Indique si l'utilisateur connecté peut commenter ou non.
-         */
-        $canComment = in_array($connectedUser->getId(), $authorizedUsers);
+        $handleRequestService = new HandleRequestService($connectedUser, $supportInfo);
 
         if (!$supportInfo) {
             self::$twig->display('404.html.twig');
@@ -93,7 +70,13 @@ class DetailTikController extends Controller
                  */
                 $button = $this->getButton($request);
 
-                switch ($button['action']) {
+                $handleRequestService->handleTheRequest($button, $form);
+
+                if ($button['action'] === 'planifier') {
+                    $this->redirectToRoute("tik_calendar_planning");
+                }
+
+                /* switch ($button['action']) {
                     case 'refuser':
                         $commentaires = new TkiCommentaires;
                         $commentaires
@@ -117,9 +100,9 @@ class DetailTikController extends Controller
                         $this->historiqueStatut($supportInfo, $button['statut']); // historisation du statut
 
                         // Envoi email refus
-                        $variableEmail = $this->donneeEmail($supportInfo, $connectedUser, $form->get('commentaires')->getData());
+                        $variableEmail = $this->emailTikService->prepareDonneeEmail($supportInfo, $connectedUser, $form->get('commentaires')->getData());
 
-                        $this->envoyerEmail($this->emailTikRefuse($variableEmail));
+                        $this->emailTikService->envoyerEmail($this->emailTikService->prepareEmail('refuse', $variableEmail));
 
                         $this->sessionService->set('notification', [
                             'type'    => 'success',
@@ -150,10 +133,10 @@ class DetailTikController extends Controller
 
                         $this->historiqueStatut($supportInfo, $button['statut']); // historisation du statut
 
-                        // Envoi email refus
-                        $variableEmail = $this->donneeEmail($supportInfo, $connectedUser, $form->get('commentaires')->getData());
+                        // Envoi email mise en attente
+                        $variableEmail = $this->emailTikService->prepareDonneeEmail($supportInfo, $connectedUser, $form->get('commentaires')->getData());
 
-                        $this->envoyerEmail($this->emailTikSuspendu($variableEmail));
+                        $this->emailTikService->envoyerEmail($this->emailTikService->prepareEmail('suspendu', $variableEmail));
 
                         $this->sessionService->set('notification', [
                             'type'    => 'success',
@@ -180,9 +163,9 @@ class DetailTikController extends Controller
                         $nomPrenomIntervenant = $dataForm->getIntervenant()->getPersonnels()->getNom() . ' ' . $dataForm->getIntervenant()->getPersonnels()->getPrenoms();
 
                         // Envoi email validation
-                        $variableEmail = $this->donneeEmail($supportInfo, $connectedUser, $nomPrenomIntervenant);
+                        $variableEmail = $this->emailTikService->prepareDonneeEmail($supportInfo, $connectedUser, $nomPrenomIntervenant);
 
-                        $this->envoyerEmail($this->emailTikValide($variableEmail));
+                        $this->emailTikService->envoyerEmail($this->emailTikService->prepareEmail('valide', $variableEmail));
 
                         $this->sessionService->set('notification', [
                             'type'    => 'success',
@@ -219,9 +202,9 @@ class DetailTikController extends Controller
                         $this->historiqueStatut($supportInfo, $button['statut']);
 
                         // Envoi email de planification
-                        $variableEmail = $this->donneeEmail($supportInfo, $connectedUser, $dataForm->getDateDebutPlanning());
+                        $variableEmail = $this->emailTikService->prepareDonneeEmail($supportInfo, $connectedUser, $dataForm->getDateDebutPlanning());
 
-                        $this->envoyerEmail($this->emailTikPlanifie($variableEmail));
+                        $this->emailTikService->envoyerEmail($this->emailTikService->prepareEmail('planifie', $variableEmail));
 
                         $this->redirectToRoute("tik_calendar_planning");
 
@@ -244,9 +227,9 @@ class DetailTikController extends Controller
                         $nomPrenomNouveauIntervenant = $dataForm->getIntervenant()->getPersonnels()->getNom() . ' ' . $dataForm->getIntervenant()->getPersonnels()->getPrenoms();
 
                         // Envoi email de transfert
-                        $variableEmail = $this->donneeEmail($supportInfo, $connectedUser, $nomPrenomNouveauIntervenant);
+                        $variableEmail = $this->emailTikService->prepareDonneeEmail($supportInfo, $connectedUser, $nomPrenomNouveauIntervenant);
 
-                        $this->envoyerEmail($this->emailTikTransfere($variableEmail));
+                        $this->emailTikService->envoyerEmail($this->emailTikService->prepareEmail('transfere', $variableEmail));
 
                         $this->sessionService->set('notification', [
                             'type'    => 'success',
@@ -278,9 +261,9 @@ class DetailTikController extends Controller
                         $this->historiqueStatut($supportInfo, $button['statut']); // historisation du statut
 
                         // Envoi email resolution
-                        $variableEmail = $this->donneeEmail($supportInfo, $connectedUser, $form->get('commentaires')->getData());
+                        $variableEmail = $this->emailTikService->prepareDonneeEmail($supportInfo, $connectedUser, $form->get('commentaires')->getData());
 
-                        $this->envoyerEmail($this->emailTikResolu($variableEmail));
+                        $this->emailTikService->envoyerEmail($this->emailTikService->prepareEmail('resolu', $variableEmail));
 
                         $this->sessionService->set('notification', [
                             'type'    => 'success',
@@ -288,7 +271,7 @@ class DetailTikController extends Controller
                         ]);
 
                         break;
-                }
+                } */
 
                 $this->redirectToRoute("liste_tik_index");
             }
@@ -313,9 +296,9 @@ class DetailTikController extends Controller
                 self::$em->persist($commentaire);
                 self::$em->flush();
 
-                $variableEmail = $this->donneeEmail($supportInfo, $connectedUser, $commentaire->getCommentaires());
+                $variableEmail = $this->emailTikService->prepareDonneeEmail($supportInfo, $connectedUser, $commentaire->getCommentaires());
 
-                $this->envoyerEmail($this->emailTikCommente($variableEmail, $connectedUser->getMail()));
+                $this->emailTikService->envoyerEmail($this->emailTikService->prepareEmail('comment', $variableEmail, $connectedUser->getMail()));
             }
 
             $statutOuvert  = $supportInfo->getIdStatutDemande()->getId() == 58;
@@ -329,7 +312,7 @@ class DetailTikController extends Controller
                 'tik'               => $supportInfo,
                 'form'              => $form->createView(),
                 'formCommentaire'   => $formCommentaire->createView(),
-                'canComment'        => $canComment,
+                'canComment'        => $this->canComment($connectedUser, $supportInfo),
                 'statutOuvert'      => $statutOuvert,
                 'autoriser'         => !empty(array_intersect(["INTERVENANT", "VALIDATEUR"], $connectedUser->getRoleNames())),  // vérfifie si parmi les roles de l'utilisateur on trouve "INTERVENANT" ou "VALIDATEUR"
                 'validateur'        => in_array("VALIDATEUR", $connectedUser->getRoleNames()),                                  // vérfifie si parmi les roles de l'utilisateur on trouve "VALIDATEUR"
@@ -394,206 +377,43 @@ class DetailTikController extends Controller
         self::$em->flush();
     }
 
-    private function donneeEmail(DemandeSupportInformatique $tik, User $userConnecter, $variable = ''): array
-    {
-        return [
-            'id'                 => $tik->getId(),
-            'numTik'             => $tik->getNumeroTicket(),
-            'emailValidateur'    => $tik->getValidateur() ? $tik->getValidateur()->getMail() : null,
-            'emailUserDemandeur' => $tik->getMailDemandeur(),
-            'emailIntervenant'   => $tik->getMailIntervenant(),
-            'variable'           => $variable,
-            'userConnecter'      => $userConnecter->getPersonnels()->getNom() . ' ' . $userConnecter->getPersonnels()->getPrenoms(),
-            'template'           => 'tik/email/emailTik.html.twig',
-        ];
-    }
-
     /** 
-     * email pour un ticket refusé
+     * Vérifie si l'utilisateur connecté peut commenter sur le ticket
+     * 
+     * @param User $connectedUser l'utilisateur connecté
+     * @param DemandeSupportInformatique $tik le ticket en question
+     * 
+     * @return bool
      */
-    private function emailTikRefuse($tab): array
+    private function canComment(User $connectedUser, DemandeSupportInformatique $tik): bool
     {
-        return [
-            'to'        => $tab['emailUserDemandeur'],
-            'template'  => $tab['template'],
-            'variables' => [
-                'statut'      => "refuse",
-                'subject'     => "{$tab['numTik']} - Ticket refusé",
-                'tab'         => $tab,
-                'action_url'  => $this->urlGenerique("Hffintranet/tik-detail/{$tab['id']}")
-            ]
-        ];
-    }
+        /** 
+         * @var User $demandeur l'utilisateur qui a fait la demande de support info
+         */
+        $demandeur   = $tik->getUserId();
 
-    /** 
-     * email pour un ticket suspendu
-     */
-    private function emailTikSuspendu($tab): array
-    {
-        return [
-            'to'        => $tab['emailUserDemandeur'],
-            'template'  => $tab['template'],
-            'variables' => [
-                'statut'      => "suspendu",
-                'subject'     => "{$tab['numTik']} - Ticket suspendu",
-                'tab'         => $tab,
-                'action_url'  => $this->urlGenerique("Hffintranet/tik-detail/{$tab['id']}")
-            ]
-        ];
-    }
+        /** 
+         * @var User $validateur l'utilisateur qui a validé ou refusé la demande
+         */
+        $validateur  = $tik->getValidateur();
 
-    /** 
-     * email pour un ticket validé
-     */
-    private function emailTikValide($tab): array
-    {
-        return [
-            'to'        => $tab['emailUserDemandeur'],
-            'cc'        => [$tab['emailIntervenant']],
-            'template'  => $tab['template'],
-            'variables' => [
-                'statut'      => "valide",
-                'subject'     => "{$tab['numTik']} - Ticket validé",
-                'tab'         => $tab,
-                'action_url'  => $this->urlGenerique("Hffintranet/tik-detail/{$tab['id']}")
-            ]
-        ];
-    }
+        /** 
+         * @var User $intervenant l'utilisateur qui a été assigné à la demande
+         */
+        $intervenant = $tik->getIntervenant();
 
-    /** 
-     * email pour un ticket commenté
-     */
-    private function emailTikCommente($tab, $emailUserConnected): array
-    {
-        if (isset($tab['emailValidateur'])) {
-            $tabEmail = array_filter([$tab['emailValidateur'], $tab['emailUserDemandeur'], $tab['emailIntervenant']]);
-            $cc = array_values(array_diff($tabEmail, [$emailUserConnected]));
-            $to = $cc[0];
-            $cc = !empty($cc[1]) ? [$cc[1]] : [];
-        } else {
-            $emailValidateurs = array_map(function ($validateur) {
-                return $validateur->getMail();
-            }, self::$em->getRepository(User::class)->findByRole('VALIDATEUR')); // tous les validateurs
-            $to = $emailValidateurs[0];
-            $cc = array_slice($emailValidateurs, 1);
+        /** 
+         * @var array $authorizedUsers les utilisateurs autorisés à commenter
+         */
+        $authorizedUsers = [$demandeur->getId(),];
+
+        if ($validateur !== null) {
+            $authorizedUsers[] = $validateur->getId();
         }
-        return [
-            'to'        => $to,
-            'cc'        => $cc,
-            'template'  => $tab['template'],
-            'variables' => [
-                'statut'      => "comment",
-                'subject'     => "{$tab['numTik']} - Commentaire émis",
-                'tab'         => $tab,
-                'action_url'  => $this->urlGenerique("Hffintranet/tik-detail/{$tab['id']}")
-            ]
-        ];
-    }
-
-    /** 
-     * email pour un ticket résolu
-     */
-    private function emailTikResolu($tab): array
-    {
-        $tabEmail = array_values(array_filter([$tab['emailUserDemandeur'], $tab['emailValidateur']]));
-        return [
-            'to'        => $tabEmail[0],
-            'cc'        => !empty($tabEmail[1]) ? [$tabEmail[1]] : [],
-            'template'  => $tab['template'],
-            'variables' => [
-                'statut'      => "resolu",
-                'subject'     => "{$tab['numTik']} - Ticket résolu",
-                'tab'         => $tab,
-                'action_url'  => $this->urlGenerique("Hffintranet/tik-detail/{$tab['id']}")
-            ]
-        ];
-    }
-
-    /** 
-     * email pour un ticket planifié
-     */
-    private function emailTikPlanifie($tab): array
-    {
-        $tabEmail = array_values(array_filter([$tab['emailUserDemandeur'], $tab['emailValidateur']]));
-        return [
-            'to'        => $tabEmail[0],
-            'cc'        => !empty($tabEmail[1]) ? [$tabEmail[1]] : [],
-            'template'  => $tab['template'],
-            'variables' => [
-                'statut'      => "planifie",
-                'subject'     => "{$tab['numTik']} - Ticket planifié",
-                'tab'         => $tab,
-                'action_url'  => $this->urlGenerique("Hffintranet/tik-detail/{$tab['id']}")
-            ]
-        ];
-    }
-
-    /** 
-     * email pour un ticket transferé
-     */
-    private function emailTikTransfere($tab): array
-    {
-        $tabEmail = array_values(array_filter([$tab['emailUserDemandeur'], $tab['emailValidateur'], $tab['emailIntervenant']]));
-        return [
-            'to'        => $tabEmail[0],
-            'cc'        => array_slice($tabEmail, 1),
-            'template'  => $tab['template'],
-            'variables' => [
-                'statut'      => "transfere",
-                'subject'     => "{$tab['numTik']} - Ticket transféré",
-                'tab'         => $tab,
-                'action_url'  => $this->urlGenerique("Hffintranet/tik-detail/{$tab['id']}")
-            ]
-        ];
-    }
-
-    /** 
-     * fonction pour vérifier l'envoi du mail ou non 
-     */
-    private function envoyerEmail(array $content)
-    {
-        $email = new EmailService;
-
-        $email->getMailer()->setFrom('noreply.email@hff.mg', 'noreply.ticketing');
-
-        $content['cc'] = $content['cc'] ?? [];
-
-        $email->sendEmail($content['to'], $content['cc'], $content['template'], $content['variables']);
-    }
-
-    /** 
-     * Fonction pour le traitement de fichier
-     */
-    private function traitementEtEnvoiDeFichier($form, TkiCommentaires $commentaire)
-    {
-        //TRAITEMENT FICHIER
-        $fileNames = [];
-        // Récupérez les fichiers uploadés depuis le formulaire
-        $files        = $form->get('fileNames')->getData();
-        $chemin       = $_SERVER['DOCUMENT_ROOT'] . '/Upload/tik/fichiers';
-        $fileUploader = new FileUploaderService($chemin);
-        if ($files) {
-            foreach ($files as $file) {
-                // Définissez le préfixe pour chaque fichier, par exemple "DS_" pour "Demande de Support"
-                $prefix   = $commentaire->getNumeroTicket() . '_commentaire_';
-                $fileName = $fileUploader->upload($file, $prefix);
-                // Obtenir la taille du fichier dans l'emplacement final
-                $filePath = $chemin . '/' . $fileName;
-                $fileSize = round(filesize($filePath) / 1024, 2); // Taille en Ko avec 2 décimales
-                if (file_exists($filePath)) {
-                    $fileSize = round(filesize($filePath) / 1024, 2);
-                } else {
-                    $fileSize = 0; // ou autre valeur par défaut ou message d'erreur
-                }
-
-                $fileNames[] = [
-                    'name' => $fileName,
-                    'size' => $fileSize
-                ];
-            }
+        if ($intervenant !== null) {
+            $authorizedUsers[] = $intervenant->getId();
         }
 
-        // Enregistrez les noms des fichiers dans votre entité
-        $commentaire->setFileNames($fileNames);
+        return in_array($connectedUser->getId(), $authorizedUsers);
     }
 }
