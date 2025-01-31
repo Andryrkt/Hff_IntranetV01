5d293384b321a86503011cbd665634d7d63e4e24
hasimanjaka <108670520+ratompoarinandro31@users.noreply.github.com>
2025-01-31 17:11:28 +0300

    add

--- src/Controller/planning/listeController.php
+++ src/Controller/planning/listeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\planning;
 
 use App\Controller\Controller;
+use App\Controller\Traits\PlanningTraits;
 use App\Model\planning\PlanningModel;
 use App\Controller\Traits\Transformation;
 use App\Entity\planning\ListePlanningSearch;
@@ -13,6 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class ListeController extends Controller
 {
     use Transformation;
+    use PlanningTraits;
     private ListePlanningSearch $listePlanningSearch;
     private PlanningModel $planningModel;
     public function __construct()
@@ -51,6 +53,13 @@ class ListeController extends Controller
         $form->handleRequest($request);
         //initialisation criteria
         $criteria = $this->listePlanningSearch;
+        if ($form->isSubmitted() && $form->isValid()) {
+            dd($form->getdata());
+            $criteria = $form->getdata();
+        }
+        if ($request->query->get('action') !== 'oui') {
+            $lesOrvalides = $this->recupNumOrBackValider($criteria,self::$em);
+        }
         self::$twig->display('planning/listePlanning.html.twig', [
             'form' => $form->createView(),
 
