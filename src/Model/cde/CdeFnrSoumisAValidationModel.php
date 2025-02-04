460625dd2bf32660158918cfb9fe20cc4fa29cc7
Andryrkt <andryrkt08@gmail.com>
2025-02-04 08:32:35 +0300

    interface commande fournisseur

---
+++ src/Model/cde/CdeFnrSoumisAValidationModel.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Model\cde;
+
+use App\Model\Model;
+use App\Model\Traits\ConversionModel;
+
+class CdefnrSoumisAValidationModel extends Model
+{
+    use ConversionModel;
+
+    public function recupListeFournissseur()
+    {
+        $statement=" SELECT  
+                        FBSE_NUMFOU AS num_fournisseur,
+                        UPPER(FBSE_NOMFOU) AS nom_fournisseur
+                    FROM 
+                        FRN_BSE
+                    JOIN 
+                        FRN_FOU ON FBSE_NUMFOU = FFOU_NUMFOU
+                    WHERE 
+                        FFOU_SOC = 'HF'
+                        --AND FBSE_NOMFOU LIKE CONCAT('%', nomFournisseur, '%') pour le recherche
+                    ORDER BY 
+                        FBSE_NOMFOU;
+        ";
+
+        $result = $this->connect->executeQuery($statement);
+
+
+        $data = $this->connect->fetchResults($result);
+
+        return $this->convertirEnUtf8($data);
+    }
+
+    public function recupCdeFnrNonReceptionner($numFournisseur)
+    {
+        $statement =" SELECT
+                FCDE_SUCC||FCDE_SERV  AS code_agence_service,
+                TRIM(ASUC_LIB)||' - '||trim(ATAB_LIB) AS libelle_agence_service,
+                FCDE_NUMCDE AS num_cde,
+                DATE(FCDE_DATE) AS date_cde,
+                TO_CHAR(FCDE_NUMFOU) AS num_fournisseur ,
+                FBSE_NOMFOU AS nom_fournisseur,
+                FCDE_LIB  AS libelle_cde,
+                FCDE_TTC AS prix_cde_ttc,
+                FCDE_TTC*FCDE_TXDEV AS prix_cde_ttc_devise,
+                FCDE_DEVISE AS devise_cde,
+                FCDE_TYPCDE AS type_cde
+                FROM FRN_CDE, FRN_CDL, AGR_SUCC, AGR_TAB, FRN_BSE
+                WHERE FCDE_SUCC = ASUC_NUM
+                AND FCDE_NUMFOU = FBSE_NUMFOU
+                AND ATAB_CODE = FCDE_SERV AND ATAB_NOM = 'SER'
+                AND (FCDE_NUMCDE = FCDL_NUMCDE AND FCDE_SOC = FCDL_SOC AND FCDE_SUCC = FCDL_SUCC)
+                AND FCDE_NUMCDE  IN (select FLLF_NUMCDE from FRN_LLF WHERE FLLF_SOC = FCDE_SOC AND FLLF_SUCC = FCDE_SUCC)
+                --and date(FCDE_DATE) >= dateDebut 
+                --and date(FCDE_DATE) <= dateFin
+                and FCDE_NUMFOU = '".$numFournisseur."'
+                AND FCDE_SOC = 'HF'
+                AND FCDE_SERV IN ('NEG')
+                AND (FCDE_TYPCDE <> 'CIS' OR (fcde_typcde = 'CIS' AND Length(to_char(fcde_numfou)) = 7))
+                AND FCDE_NUMCDE IN (select FCDL_NUMCDE from FRN_CDL WHERE FCDE_SOC = FCDL_SOC)
+                AND FCDE_TTC <> 0
+                GROUP by 1,2,3,4,5,6,7,8,9,10,11
+
+                UNION ALL
+
+                SELECT
+                FCDE_SUCC||FCDE_SERV  AS code_agence_service,
+                TRIM(ASUC_LIB)||' - '||TRIM(ATAB_LIB) AS libelle_agence_service,
+                FCDE_NUMCDE AS num_cde,
+                DATE(FCDE_DATE) AS date_cde,
+                TO_CHAR(FCDE_NUMFOU) AS num_fournisseur,
+                FBSE_NOMFOU AS nom_fournisseur,
+                FCDE_LIB AS libelle_cde,
+                Sum(FCDL_SOLDE*FCDL_PXACH) AS prix_cde_ttc,
+                Sum(FCDL_SOLDE*FCDL_PXACH*FCDE_TXDEV) AS prix_cde_ttc_devise,
+                FCDE_DEVISE AS devise_cde,
+                FCDE_TYPCDE AS type_cde
+                FROM FRN_CDE, FRN_CDL, AGR_SUCC, AGR_TAB, FRN_BSE
+                WHERE FCDE_SUCC = ASUC_NUM
+                AND FCDE_NUMFOU = FBSE_NUMFOU
+                AND ATAB_CODE = FCDE_SERV AND ATAB_NOM = 'SER'
+                AND (FCDE_NUMCDE = FCDL_NUMCDE AND FCDE_SOC = FCDL_SOC AND FCDE_SUCC = FCDL_SUCC)
+                AND FCDE_NUMCDE IN (select FLLF_NUMCDE from FRN_LLF WHERE FLLF_SOC = FCDE_SOC AND FLLF_SUCC = FCDE_SUCC)
+                --and date(FCDE_DATE) >= dateDebut
+                --and date(FCDE_DATE) <= dateFin
+                and FCDE_NUMFOU = '".$numFournisseur."' 
+                AND FCDE_SOC = 'HF'
+                AND FCDE_SERV IN ('NEG')
+                AND (FCDE_TYPCDE <> 'CIS' OR (fcde_typcde = 'CIS' AND Length(to_char(fcde_numfou)) = 7))
+                AND FCDE_NUMCDE IN (select FCDL_NUMCDE from FRN_CDL WHERE FCDE_SOC = FCDL_SOC)
+                AND (FCDL_QTE <> FCDL_QTELI AND FCDL_QTE <> 0 AND FCDL_SOLDE <> 0)
+                GROUP by 1,2,3,4,5,6,7,10,11
+                ORDER by 4, 3
+        ";
+
+        $result = $this->connect->executeQuery($statement);
+
+        $data = $this->connect->fetchResults($result);
+
+        return $this->convertirEnUtf8($data);
+    }
+}
