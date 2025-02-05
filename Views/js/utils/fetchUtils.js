806348cd7af136f9e95d40193fbe110b71bba1dd
Menja <ranofimenjajam@gmail.com>
2025-02-05 14:32:20 +0300

    ajout de postData dans fetchUtils

--- Views/js/utils/fetchUtils.js
+++ Views/js/utils/fetchUtils.js
@@ -8,3 +8,18 @@ export async function fetchData(url) {
     return [];
   }
 }
+
+export async function postData(url, data) {
+  try {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) throw new Error(`Erreur HTTP: ${response.status}`);
+    return await response.json();
+  } catch (error) {
+    console.error("Erreur lors de l'envoi des données:", error);
+    return null;
+  }
+}
