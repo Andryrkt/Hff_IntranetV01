1e745461900771803eb00dfb61026692cec79bdf
Menja <ranofimenjajam@gmail.com>
2025-02-19 10:00:21 +0300

    résolution bug: non envoi des données

--- src/Controller/mutation/MutationController.php
+++ src/Controller/mutation/MutationController.php
@@ -3,6 +3,11 @@
 namespace App\Controller\mutation;
 
 use App\Controller\Controller;
+use App\Entity\admin\dom\SousTypeDocument;
+use App\Entity\admin\utilisateur\User;
+use App\Entity\mutation\Mutation;
+use App\Form\mutation\MutationFormType;
+use DateTime;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,9 +16,98 @@ class MutationController extends Controller
     /**
      * @Route("/mutation/new", name="mutation_nouvelle_demande")
      */
-    public function firstForm(Request $request)
+    public function nouveau(Request $request)
     {
         //verification si user connecter
         $this->verifierSessionUtilisateur();
+
+        $mutation = new Mutation;
+        $this->initialisationMutation($mutation);
+
+        $form = self::$validator->createBuilder(MutationFormType::class, $mutation)->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            dd($form->getData());
+        }
+
+        self::$twig->display('mutation/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/mutation/list", name="mutation_liste")
+     */
+    public function listeDom(Request $request)
+    {
+        //verification si user connecter
+        $this->verifierSessionUtilisateur();
+
+        // $autoriser = $this->autorisationRole(self::$em);
+
+        // $domSearch = new DomSearch();
+
+        // $agenceServiceIps = $this->agenceServiceIpsObjet();
+        // /** INITIALIASATION et REMPLISSAGE de RECHERCHE pendant la nag=vigation pagiantion */
+        // $this->initialisation($domSearch, self::$em, $agenceServiceIps, $autoriser);
+
+        // $form = self::$validator->createBuilder(DomSearchType::class, $domSearch, [
+        //     'method' => 'GET',
+        //     'idAgenceEmetteur' => $agenceServiceIps['agenceIps']->getId()
+        // ])->getForm();
+
+        // $form->handleRequest($request);
+
+        // if ($form->isSubmitted() && $form->isValid()) {
+        //     $domSearch = $form->getData();
+        // }
+
+        // $criteria = [];
+        // //transformer l'objet ditSearch en tableau
+        // $criteria = $domSearch->toArray();
+
+        // $page = max(1, $request->query->getInt('page', 1));
+        // $limit = 10;
+
+        // $option = [
+        //     'boolean' => $autoriser,
+        //     'idAgence' => $this->agenceIdAutoriser(self::$em)
+        // ];
+
+        // $repository = self::$em->getRepository(Dom::class);
+        // $paginationData = $repository->findPaginatedAndFiltered($page, $limit, $domSearch, $option);
+
+        // //enregistre le critère dans la session
+        // $this->sessionService->set('dom_search_criteria', $criteria);
+        // $this->sessionService->set('dom_search_option', $option);
+
+        // $this->logUserVisit('doms_liste'); // historisation du page visité par l'utilisateur
+
+        // self::$twig->display(
+        //     'doms/list.html.twig',
+        //     [
+        //         'form' => $form->createView(),
+        //         'data' => $paginationData['data'],
+        //         'currentPage' => $paginationData['currentPage'],
+        //         'lastPage' => $paginationData['lastPage'],
+        //         'resultat' => $paginationData['totalItems'],
+        //         'criteria' => $criteria,
+        //     ]
+        // );
+    }
+
+    private function initialisationMutation(Mutation $mutation)
+    {
+        $agenceServiceIps = $this->agenceServiceIpsString();
+
+        $mutation
+            ->setDateDemande(new DateTime)
+            ->setAgenceEmetteur($agenceServiceIps['agenceIps'])
+            ->setServiceEmetteur($agenceServiceIps['serviceIps'])
+            ->setSousTypeDocument(self::$em->getRepository(SousTypeDocument::class)->find(5)) // Sous-type document MUTATION
+            ->setTypeDocument($mutation->getSousTypeDocument()->getCodeDocument())
+        ;
     }
 }
---
+++ src/Form/mutation/MutationFormType.php
@@ -0,0 +1,143 @@
+<?php
+
+namespace App\Form\mutation;
+
+use App\Entity\admin\Agence;
+
+use App\Entity\admin\dom\Rmq;
+use App\Controller\Controller;
+use App\Entity\admin\dom\Catg;
+use App\Entity\admin\Personnel;
+use Doctrine\ORM\EntityRepository;
+use App\Entity\admin\dom\Indemnite;
+use Symfony\Component\Form\FormEvent;
+use App\Entity\admin\utilisateur\User;
+use Symfony\Component\Form\FormEvents;
+use App\Entity\admin\AgenceServiceIrium;
+use Symfony\Component\Form\AbstractType;
+use App\Entity\admin\dom\SousTypeDocument;
+use App\Entity\mutation\Mutation;
+use App\Repository\admin\dom\CatgRepository;
+use App\Repository\admin\dom\SousTypeDocumentRepository;
+use App\Repository\admin\PersonnelRepository;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+
+class MutationFormType extends AbstractType
+{
+    private $em;
+
+    const SALARIE = [
+        'PERMANENT' => 'PERMANENT',
+        'TEMPORAIRE' => 'TEMPORAIRE',
+    ];
+
+    public function __construct()
+    {
+        $this->em = Controller::getEntity();
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+        $builder
+            ->add(
+                'agenceEmetteur',
+                TextType::class,
+                [
+                    'mapped'   => false,
+                    'label'    => 'Agence',
+                    'required' => false,
+                    'attr'     => [
+                        'readonly' => true
+                    ],
+                    'data'     => $options["data"]->getAgenceEmetteur() ?? null
+                ]
+            )
+            ->add(
+                'serviceEmetteur',
+                TextType::class,
+                [
+                    'mapped'   => false,
+                    'label'    => 'Service',
+                    'required' => false,
+                    'attr'     => [
+                        'readonly' => true,
+                    ],
+                    'data'     => $options["data"]->getServiceEmetteur() ?? null
+                ]
+            )
+            ->add(
+                'categorie',
+                EntityType::class,
+                [
+                    'label'         => 'Catégorie',
+                    'class'         => Catg::class,
+                    'choice_label'  => 'description',
+                    'query_builder' => function (CatgRepository $catg) {
+                        return $catg->createQueryBuilder('c')->orderBy('c.description', 'ASC');
+                    }
+                ]
+            )
+            ->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) use ($options) {
+                $form = $event->getForm();
+
+                $codeAgence = explode(" ", $options['data']->getAgenceEmetteur())[0];   // obtenir le code agence de l'utilisateur
+                $codeService = explode(" ", $options['data']->getServiceEmetteur())[0];  // obtenir le code service de l'utilisateur
+
+                // Récupération de l'ID du service agence irium
+                $agenceServiceIriumId = $this->em->getRepository(AgenceServiceIrium::class)
+                    ->findId($codeAgence, $codeService, $options['data']->getServiceEmetteur());
+
+                // Ajout du champ 'matriculeNom'
+                $form->add(
+                    'matriculeNomPrenom',
+                    EntityType::class,
+                    [
+                        'mapped'        => false,
+                        'label'         => 'Matricule, nom et prénoms',
+                        'class'         => Personnel::class,
+                        'placeholder'   => '-- choisir un personnel --',
+                        'choice_label'  => function (Personnel $personnel): string {
+                            return $personnel->getMatricule() . ' ' . $personnel->getNom() . ' ' . $personnel->getPrenoms();
+                        },
+                        'required'      => true,
+                        'query_builder' => function (PersonnelRepository $repository) use ($agenceServiceIriumId) {
+                            return $repository->createQueryBuilder('p')
+                                ->where('p.agenceServiceIriumId IN (:agenceIps)')
+                                ->setParameter('agenceIps', $agenceServiceIriumId)
+                                ->orderBy('p.Matricule', 'ASC');
+                        },
+                    ]
+                );
+            })
+            ->addEventListener(FormEvents::POST_SUBMIT, function (FormEvent $event) {
+                $mutation = $event->getData(); // Objet
+                $form = $event->getForm();
+
+                $personnelId = $form->get('matriculeNomPrenom')->getData(); // id du personnel sélectionné
+
+                /** 
+                 * @var Personnel $personnel
+                 */
+                $personnel = $this->em->getRepository(Personnel::class)->find($personnelId);
+
+                // On met à jour les données du formulaire
+                $mutation->setMatricule($personnel->getMatricule());
+                $mutation->setNom($personnel->getNom());
+                $mutation->setPrenom($personnel->getPrenoms());
+            })
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Mutation::class,
+        ]);
+    }
+}
