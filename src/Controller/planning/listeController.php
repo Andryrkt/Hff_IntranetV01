f272352200fc3234257178856066a808cd7fcad0
hasimanjaka <108670520+ratompoarinandro31@users.noreply.github.com>
2025-02-27 15:32:51 +0300

    modif

--- src/Controller/planning/listeController.php
+++ src/Controller/planning/listeController.php
@@ -85,7 +85,7 @@ class ListeController extends Controller
                 $backString = '';
             }
 
-            $res1 = $this->planningModel->recuperationMaterielplanifierListe($criteria, $lesOrvalides['orSansItv'], $backString, $page, $limit,true);
+            $res1 = $this->planningModel->recuperationMaterielplanifierListe($criteria, $lesOrvalides['orSansItv'], $backString, $page, $limit);
              $resultat = $this->planningModel->recuperationNombreMaterielplanifier($criteria, $lesOrvalides['orSansItv'], $backString);
             // Calcule le nombre total de pages
             $pagesCount = ceil($resultat / $limit);
