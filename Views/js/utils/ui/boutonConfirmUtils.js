8ffee574ec4bddbc663f8eb2910098ff91f5b3d4
Andryrkt <andryrkt08@gmail.com>
2025-03-03 08:28:29 +0300

    rectification de autoComplete.js , boutonConfirmUtils.js et principe

--- Views/js/utils/ui/boutonConfirmUtils.js
+++ Views/js/utils/ui/boutonConfirmUtils.js
@@ -1,6 +1,6 @@
 export function setupConfirmationButtons() {
   document.querySelectorAll("[data-confirmation]").forEach((button) => {
-    button.addEventListener("click", (e) => {
+    button.addEventListener("click", async (e) => {
       e.preventDefault();
 
       const overlay = document.getElementById("loading-overlays");
