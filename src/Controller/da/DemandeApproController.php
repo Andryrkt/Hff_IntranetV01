f54594dd4e6761eece208aea4bd04f01bd1f22fc
Menja <ranofimenjajam@gmail.com>
2025-03-13 17:31:20 +0300

    sélection de choix DA

---
+++ src/Controller/da/DemandeApproController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller\da;
+
+use App\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/demande-appro")
+ */
+class DemandeApproController extends Controller
+{
+    /**
+     * @Route("/first-form", name="da_first_form")
+     */
+    public function firstForm()
+    {
+        self::$twig->display('da/first-form.html.twig');
+    }
+
+    /**
+     * @Route("/list-dit", name="da_list_dit")
+     */
+    public function listeDIT()
+    {
+        self::$twig->display();
+    }
+
+    /**
+     * @Route("/new", name="da_new")
+     */
+    public function new()
+    {
+        self::$twig->display();
+    }
+}
