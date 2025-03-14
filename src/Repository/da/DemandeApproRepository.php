b03e162dcfc17d807d3ab247a6cc7f3f68115e5c
Menja <ranofimenjajam@gmail.com>
2025-03-14 10:22:52 +0300

    E/R de DemandeAppro

---
+++ src/Repository/da/DemandeApproRepository.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Repository\da;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Tools\Pagination\Paginator as DoctrinePaginator;
+
+class DemandeApproRepository extends EntityRepository {}
