316982b60c75b5e396eebe640d162d43606e5e29
Andryrkt <andryrkt08@gmail.com>
2025-03-14 06:50:11 +0300

    affichage font awesom

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,14 @@ import "bootstrap"; // Importer les scripts JavaScript de Bootstrap
 
 import $ from "jquery";
 
+// SELECT 2
 import "select2/dist/css/select2.min.css"; // Styles de base
 import "select2-bootstrap-theme/dist/select2-bootstrap.min.css"; // Thème Bootstrap
 import "select2"; // Bibliothèque Select2
 
-
+// Font Awesome
+import "@fortawesome/fontawesome-free/css/all.min.css"; // Pour les styles
+import "@fortawesome/fontawesome-free/js/all.min.js"; // Pour les icônes dynamiques (optionnel)
 
 // alert("Bonjour");
 console.log("Bonjour");
