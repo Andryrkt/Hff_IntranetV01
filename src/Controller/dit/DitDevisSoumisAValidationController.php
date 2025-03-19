73e317a23fde8011860d3f794fc5e8f6f1b72f18
lanto_hasina <andryrkt08@gmail.com>
2025-03-19 17:40:50 +0300

    renforcer le bloquage

--- src/Controller/dit/DitDevisSoumisAValidationController.php
+++ src/Controller/dit/DitDevisSoumisAValidationController.php
@@ -110,13 +110,14 @@ class DitDevisSoumisAValidationController extends Controller
         $estCepremierSoumission = $this->devisRepository->findVerificationPrimeSoumission($numDevis);
 
         if($type === 'VP') {
-            if(in_array('Prix refusé magasin', $devisStatut) && (int)$nbrPieceInformix == (int)$nbrPieceSqlServ) { // statut devi prix réfuseé magasin et pas de nouvelle ligne
+            
+            if ( $nbSotrieMagasin[0]['nbr_sortie_magasin'] !== "0" && (int)$nbrPieceInformix == (int)$nbrPieceSqlServ) {// il n'y a pas de pièce magasin et pas de nouvelle ligne
+                $message = " Merci de passer le devis à validation au magasin ";
+                $this->historiqueOperation->sendNotificationSoumission($message, $numDevis, 'dit_index');
+            } else if(in_array('Prix refusé magasin', $devisStatut) && (int)$nbrPieceInformix == (int)$nbrPieceSqlServ) { // statut devi prix réfuseé magasin et pas de nouvelle ligne
                 $message = " Le prix a été déjà vérifié ... Veuillez soumettre à validation à l'atelier";
                 $this->historiqueOperation->sendNotificationSoumission($message, $numDevis, 'dit_index');
-            } else if ( $nbSotrieMagasin[0]['nbr_sortie_magasin'] !== "0" && (int)$nbrPieceInformix == (int)$nbrPieceSqlServ) {// il n'y a pas de pièce magasin et pas de nouvelle ligne
-                $message = " Pas de vérification à faire par le magasin ";
-                $this->historiqueOperation->sendNotificationSoumission($message, $numDevis, 'dit_index');
-            }
+            } 
             else if ( $nbSotrieMagasin[0]['nbr_sortie_magasin'] === "0") {// il n'y a pas de pièce magasin
                 $message = " Pas de vérification à faire par le magasin ";
                 $this->historiqueOperation->sendNotificationSoumission($message, $numDevis, 'dit_index');
