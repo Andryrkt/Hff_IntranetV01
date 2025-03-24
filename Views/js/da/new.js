4da54526273f7eb423d9244f326752ea6ca8ab61
Menja <ranofimenjajam@gmail.com>
2025-03-24 10:06:14 +0300

    bloquer formulaire

--- Views/js/da/new.js
+++ Views/js/da/new.js
@@ -7,6 +7,13 @@ document.addEventListener('DOMContentLoaded', function () {
   document
     .getElementById('add-child')
     .addEventListener('click', ajouterUneLigne);
+  
+  document.getElementById('myForm').addEventListener('submit', function (e) {
+    if (!document.getElementById('children-container').hasChildNodes()) {
+      e.preventDefault();
+      alert('Vous devez au moins ajouter une ligne de DA!');
+    }
+  });
 });
 
 window.addEventListener('load', () => {
