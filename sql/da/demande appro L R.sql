9d31e93327a31136ed145882a6431de99c5ef773
Menja <ranofimenjajam@gmail.com>
2025-03-25 09:47:10 +0300

    modification requête SQL et création dans BDD

--- sql/da/demande appro L R.sql
+++ sql/da/demande appro L R.sql
@@ -2,9 +2,9 @@
 CREATE TABLE Demande_Appro_L_R(
     id int IDENTITY(1,1) NOT NULL,
     numero_demande_appro varchar(11) not null,
-    num_ligne_dem int(11) not null,
-    qte_dem int(11) not null,
-    qte_dispo int(11),
+    num_ligne_dem int not null,
+    qte_dem int not null,
+    qte_dispo int,
     art_constp varchar(3) not null,
     art_refp varchar(50) not null,
     art_desi varchar(100) not null,
