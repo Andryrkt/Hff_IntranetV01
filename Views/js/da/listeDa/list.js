8e4915fe167de7cef5131ed602bff1256c71e0a0
Menja <ranofimenjajam@gmail.com>
2025-03-26 12:05:22 +0300

    proposition étape 3: navigation sur les pages - 2 (JS)

--- Views/js/da/listeDa/list.js
+++ Views/js/da/listeDa/list.js
@@ -1,6 +1,14 @@
 import { displayOverlay } from '../../utils/spinnerUtils';
 
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+  const designations = document.querySelectorAll('.designation-btn');
+  designations.forEach((designation) => {
+    designation.addEventListener('click', function () {
+      let numeroLigne = this.getAttribute('data-numero-ligne');
+      localStorage.setItem('currentTab', numeroLigne);
+    });
+  });
+});
 
 window.addEventListener('load', () => {
   displayOverlay(false);
--- Views/js/da/proposition/proposition.js
+++ Views/js/da/proposition/proposition.js
@@ -1,6 +1,18 @@
 import { displayOverlay } from '../../utils/spinnerUtils';
+import { changeTab, showTab } from './pageNavigation';
 
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+  const prevBtns = document.querySelectorAll('.prevBtn'); // Bouton Précédent
+  const nextBtns = document.querySelectorAll('.nextBtn'); // Bouton Suivant
+
+  showTab();
+  prevBtns.forEach((prevBtn) => {
+    prevBtn.addEventListener('click', () => changeTab('prev'));
+  });
+  nextBtns.forEach((nextBtn) => {
+    nextBtn.addEventListener('click', () => changeTab('next'));
+  });
+});
 
 window.addEventListener('load', () => {
   displayOverlay(false);
