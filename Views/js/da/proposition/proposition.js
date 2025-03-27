edc8e1cf124e126d125699acbf0faa9a3b5e5704
Menja <ranofimenjajam@gmail.com>
2025-03-27 11:22:15 +0300

    évènement pour l'autocomplete

--- Views/js/da/proposition/proposition.js
+++ Views/js/da/proposition/proposition.js
@@ -1,13 +1,36 @@
 import { displayOverlay } from '../../utils/spinnerUtils';
 import { ajouterReference } from './article';
+import { autocompleteTheField } from './autocompletion';
 import { changeTab, showTab } from './pageNavigation';
 
 document.addEventListener('DOMContentLoaded', function () {
-  const prevBtns = document.querySelectorAll('.prevBtn'); // Bouton Précédent
-  const nextBtns = document.querySelectorAll('.nextBtn'); // Bouton Suivant
-  const addLines = document.querySelectorAll('[id*="add_line_"]');
+  const prevBtns = document.querySelectorAll('.prevBtn'); // Tous les boutons "Précédent"
+  const nextBtns = document.querySelectorAll('.nextBtn'); // Tous les boutons "Suivant"
+  const addLines = document.querySelectorAll('[id*="add_line_"]'); // Tous les boutons "Ajouter la référence"
+  const references = document.querySelectorAll('[id*="reference_"]'); // Tous les champs "Référence"
+  const fournisseurs = document.querySelectorAll('[id*="fournisseur_"]'); // Tous les champs "Fournisseur"
+  const designations = document.querySelectorAll('[id*="designation_"]'); // Tous les champs "Désignation"
+  const qtedispos = document.querySelectorAll('[id*="qte_dispo_"]'); // Tous les champs "Qté Dispo"
 
   showTab(); // afficher la page d'article sélectionné par l'utilisateur
+
+  /** Champs */
+  qtedispos.forEach((qtedispo) => {
+    qtedispo.addEventListener('input', function () {
+      qtedispo.value = qtedispo.value.replace(/[^\d]/g, '');
+    });
+  });
+  references.forEach((reference) => {
+    autocompleteTheField(reference, 'reference');
+  });
+  fournisseurs.forEach((fournisseur) => {
+    autocompleteTheField(fournisseur, 'fournisseur');
+  });
+  designations.forEach((designation) => {
+    autocompleteTheField(designation, 'designation');
+  });
+
+  /** Boutons */
   prevBtns.forEach((prevBtn) => {
     prevBtn.addEventListener('click', () => changeTab('prev'));
   });
