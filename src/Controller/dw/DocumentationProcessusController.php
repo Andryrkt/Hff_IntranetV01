2443909675723b3fca8119c88b4bb71900d6776c
Menja <ranofimenjajam@gmail.com>
2025-05-22 11:00:21 +0300

    formulaire de recherche pour la documentation interne

--- src/Controller/dw/DocumentationProcessusController.php
+++ src/Controller/dw/DocumentationProcessusController.php
@@ -4,6 +4,7 @@ namespace App\Controller\dw;
 
 use App\Controller\Controller;
 use App\Entity\dw\DwProcessusProcedure;
+use App\Form\dw\DocInterneSearchType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,7 +17,14 @@ class DocumentationProcessusController extends Controller
     {
         $data = self::$em->getRepository(DwProcessusProcedure::class)->findAll();
 
+        $form = self::$validator->createBuilder(DocInterneSearchType::class)->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {}
+
         self::$twig->display('dw/documentationInterne.html.twig', [
+            'form' => $form->createView(),
             'data' => $data
         ]);
     }
