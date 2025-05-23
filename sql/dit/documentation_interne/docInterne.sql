633bca47198a54048ce984f7375349e67ae7b83d
Menja <ranofimenjajam@gmail.com>
2025-05-23 09:09:10 +0300

    test phase 1

--- sql/dit/documentation_interne/docInterne.sql
+++ sql/dit/documentation_interne/docInterne.sql
@@ -1,6 +1,7 @@
 CREATE TABLE DW_Processus_procedure (
     id INT IDENTITY(1,1),
     id_document VARCHAR(11),
+    id_tiroir VARCHAR(100),
     nom_document VARCHAR(100),
     processus_lie VARCHAR(50),
     type_document VARCHAR(50),
@@ -8,7 +9,8 @@ CREATE TABLE DW_Processus_procedure (
     date_de_prochaine_revue DATE,
     nom_du_responsable VARCHAR(50),
     email_responsable_processus VARCHAR(50),
-    derniere_modification DATE,
+    date_derniere_modification DATE,
+    heure_derniere_modification TIME,
     numero_version INT,
     code_service VARCHAR(3),
     code_agence VARCHAR(3),
@@ -19,3 +21,30 @@ CREATE TABLE DW_Processus_procedure (
     path VARCHAR(100)
 PRIMARY KEY (id)
 );
+
+
+CREATE TABLE mise_a_jour_processus (
+    [id_update] [int] IDENTITY(1,1) NOT NULL,
+	[date_update] [date] NULL,
+	[heure_update] [time](7) NULL,
+PRIMARY KEY (id_update));
+
+
+
+CREATE TABLE document_a_telecharger_processus (
+	[id_update] [int] NULL,
+	[id_doc] [int] NULL,
+	[id_tiroir] [varchar](100) NULL,
+	[operation] [varchar](50) NULL
+) ON [PRIMARY]
+GO
+
+
+
+ALTER TABLE mise_a_jour_processus ADD  DEFAULT (getdate()) FOR [date_update]
+GO
+
+ALTER TABLE mise_a_jour_processus ADD  DEFAULT (getdate()) FOR [heure_update]
+GO
+
+
