4dcd59184d30fa0c0a3b1e9738b9d6e669a49108
Andryrkt <andryrkt08@gmail.com>
2025-05-30 13:39:41 +0300

    fusion recurssive et position sticki de titre

--- Views/js/da/listeCdeFrn/listCdefrn.js
+++ Views/js/da/listeCdeFrn/listCdefrn.js
@@ -1,20 +1,27 @@
-import { displayOverlay } from '../../utils/spinnerUtils';
-import { mergeCellsRecursiveTable } from './tableHandler';
-import { AutoComplete } from '../../utils/AutoComplete.js';
-import { FetchManager } from '../../api/FetchManager.js';
+import { displayOverlay } from "../../utils/spinnerUtils";
+import { mergeCellsRecursiveTable } from "./tableHandler";
+import { AutoComplete } from "../../utils/AutoComplete.js";
+import { FetchManager } from "../../api/FetchManager.js";
 const fetchManager = new FetchManager();
 
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   displayOverlay(false);
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-  mergeCellsRecursiveTable(2); // fusionne le tableau
+document.addEventListener("DOMContentLoaded", function () {
+  /*  1ᵉʳ appel : colonnes 0-3 selon le pivot que vous aviez déjà.
+   *  2ᵉ appel : colonnes 4-5 selon la colonne 4.
+   */
+  mergeCellsRecursiveTable([
+    { pivotIndex: 2, columns: [0, 1, 2, 3], insertSeparator: true },
+    { pivotIndex: 4, columns: [4, 5], insertSeparator: true },
+    { pivotIndex: 6, columns: [6], insertSeparator: true },
+  ]);
 });
 
 /** =========================================================*/
 async function fetchFournisseurs() {
-  return await fetchManager.get('api/numero-libelle-fournisseur');
+  return await fetchManager.get("api/numero-libelle-fournisseur");
 }
 
 function displayFournisseur(item) {
@@ -24,7 +31,7 @@ function displayFournisseur(item) {
 /**===================================================
  * Autocomplete champ numero FOURNISSEUR
  *====================================================*/
-const numFournisseurInput = document.querySelector('#cde_frn_list_numFrn');
+const numFournisseurInput = document.querySelector("#cde_frn_list_numFrn");
 
 function onSelectNumFournisseur(item) {
   numFournisseurInput.value = `${item.num_fournisseur}`;
@@ -32,8 +39,8 @@ function onSelectNumFournisseur(item) {
 
 new AutoComplete({
   inputElement: numFournisseurInput,
-  suggestionContainer: document.querySelector('#suggestion-num-fournisseur'),
-  loaderElement: document.querySelector('#loader-num-fournisseur'), // Ajout du loader
+  suggestionContainer: document.querySelector("#suggestion-num-fournisseur"),
+  loaderElement: document.querySelector("#loader-num-fournisseur"), // Ajout du loader
   debounceDelay: 300, // Délai en ms
   fetchDataCallback: fetchFournisseurs,
   displayItemCallback: displayFournisseur,
@@ -43,7 +50,7 @@ new AutoComplete({
 /**===================================================
  * Autocomplete champ nom FOURNISSEUR
  *====================================================*/
-const nomFournisseurInput = document.querySelector('#cde_frn_list_frn');
+const nomFournisseurInput = document.querySelector("#cde_frn_list_frn");
 
 function onSelectNomFournisseur(item) {
   nomFournisseurInput.value = `${item.nom_fournisseur}`;
@@ -51,8 +58,8 @@ function onSelectNomFournisseur(item) {
 
 new AutoComplete({
   inputElement: nomFournisseurInput,
-  suggestionContainer: document.querySelector('#suggestion-nom-fournisseur'),
-  loaderElement: document.querySelector('#loader-nom-fournisseur'), // Ajout du loader
+  suggestionContainer: document.querySelector("#suggestion-nom-fournisseur"),
+  loaderElement: document.querySelector("#loader-nom-fournisseur"), // Ajout du loader
   debounceDelay: 300, // Délai en ms
   fetchDataCallback: fetchFournisseurs,
   displayItemCallback: displayFournisseur,
