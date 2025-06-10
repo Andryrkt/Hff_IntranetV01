4783e5d599ed934783e970258c82baa2272bc90b
Menja <ranofimenjajam@gmail.com>
2025-06-10 14:59:40 +0300

    📦 NEW: ajout de fichiers étape 1

--- src/Form/da/DemandeApproLFormType.php
+++ src/Form/da/DemandeApproLFormType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -102,6 +103,17 @@ class DemandeApproLFormType extends AbstractType
                 'label' => false,
                 'required' => false,
             ])
+            ->add(
+                'fileNames',
+                FileType::class,
+                [
+                    'label'      => false,
+                    'required'   => false,
+                    'multiple'   => true,
+                    'data_class' => null,
+                    'mapped'     => false, // Indique que ce champ ne doit pas être lié à l'entité
+                ]
+            )
         ;
     }
 
