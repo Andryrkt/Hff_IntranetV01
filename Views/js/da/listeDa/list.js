f467199a1c63374e9e4144671ca76fec16b83b21
Menja <ranofimenjajam@gmail.com>
2025-06-16 09:56:12 +0300

    déverouillage modal côté ate

--- Views/js/da/listeDa/list.js
+++ Views/js/da/listeDa/list.js
@@ -13,30 +13,59 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   });
   mergeCellsTable(1); // fusionne le tableau en fonction de la colonne DA
+
+  /**===========================================================================
+   * Configuration des agences et services
+   *============================================================================*/
+
+  // Attachement des événements pour les agences
+  configAgenceService.emetteur.agenceInput.addEventListener('change', () =>
+    handleAgenceChange('emetteur')
+  );
+
+  configAgenceService.debiteur.agenceInput.addEventListener('change', () =>
+    handleAgenceChange('debiteur')
+  );
+
+  /**==================================================
+   * valider seulement les chiffres
+   *===================================================*/
+
+  const idMaterielInput = document.querySelector('#da_search_idMateriel');
+  idMaterielInput.addEventListener('input', () =>
+    allowOnlyNumbers(idMaterielInput)
+  );
+
+  /**==================================================
+   * Configuration sur le modal et le form dans le modal
+   *===================================================*/
+  const deverouillageModal = document.getElementById(
+    'demandeDeverouillageModal'
+  );
+  const modal = new bootstrap.Modal(deverouillageModal);
+
+  deverouillageModal.addEventListener('show.bs.modal', function (event) {
+    const form = deverouillageModal.querySelector('form');
+    form.querySelector('textarea').value = ''; // Réinitialiser le champ de texte
+  });
+
+  deverouillageModal
+    .querySelector('form')
+    .addEventListener('submit', function (event) {
+      event.preventDefault(); // Empêche l'envoi du formulaire par défaut
+      const motif = this.querySelector('textarea').value;
+
+      if (motif) {
+        // Logique pour traiter le formulaire
+        console.log(
+          `Demande de déverrouillage pour la DA avec le motif: ${motif}`
+        );
+        modal.hide(); // Ferme le modal après traitement
+        this.submit(); // Soumet le formulaire si nécessaire
+      }
+    });
 });
 
 window.addEventListener('load', () => {
   displayOverlay(false);
 });
-
-/**===========================================================================
- * Configuration des agences et services
- *============================================================================*/
-
-// Attachement des événements pour les agences
-configAgenceService.emetteur.agenceInput.addEventListener('change', () =>
-  handleAgenceChange('emetteur')
-);
-
-configAgenceService.debiteur.agenceInput.addEventListener('change', () =>
-  handleAgenceChange('debiteur')
-);
-
-/**==================================================
- * valider seulement les chiffres
- *===================================================*/
-
-const idMaterielInput = document.querySelector('#da_search_idMateriel');
-idMaterielInput.addEventListener('input', () =>
-  allowOnlyNumbers(idMaterielInput)
-);
