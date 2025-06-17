945797d7a457dbb0dfb931c4560f5ff2396560f8
Menja <ranofimenjajam@gmail.com>
2025-06-17 10:55:10 +0300

    statut de la DA: OK, ajout de nouveau

--- src/Repository/da/DaHistoriqueDemandeModifDARepository.php
+++ src/Repository/da/DaHistoriqueDemandeModifDARepository.php
@@ -4,4 +4,17 @@ namespace App\Repository\da;
 
 use Doctrine\ORM\EntityRepository;
 
-class DaHistoriqueDemandeModifDARepository extends EntityRepository {}
+class DaHistoriqueDemandeModifDARepository extends EntityRepository
+{
+    public function findNumDaOfNonDeverrouillees(): array
+    {
+        $results = $this->createQueryBuilder('h')
+            ->select('h.numDa')
+            ->andWhere('h.estDeverouillee = :val')
+            ->setParameter('val', false)
+            ->getQuery()
+            ->getResult();
+
+        return array_column($results, 'numDa');
+    }
+}
