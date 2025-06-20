c89d8148c8f0c1ea67e59e7028881974b043e581
Andryrkt <andryrkt08@gmail.com>
2025-06-20 11:32:59 +0300

    rectification du blocage dateplanning < date du jours

--- src/Controller/Traits/dit/DitOrSoumisAValidationTrait.php
+++ src/Controller/Traits/dit/DitOrSoumisAValidationTrait.php
@@ -236,7 +236,7 @@ trait DitOrSoumisAValidationTrait
             $recapAvantApres[] = [
                 'itv' => $itv,
                 'libelleItv' => $libelleItv,
-                'datePlanning' => $this->datePlanning($OrSoumisAvant[$i]->getNumeroOR()),
+                'datePlanning' => $this->datePlanning($OrSoumisAvant[$i]->getNumeroOR(), $itv),
                 'nbLigAv' => $nbLigAv,
                 'nbLigAp' => $nbLigAp,
                 'mttTotalAv' => $mttTotalAv,
@@ -417,21 +417,30 @@ trait DitOrSoumisAValidationTrait
 
     private function datePlanningInferieurDateDuJour($numOr): bool
     {
-        $datePlannig1 = $this->magasinListOrLivrerModel->recupDatePlanning12($numOr);
-        $datePlannig2 = $this->magasinListOrLivrerModel->recupDatePlanning22($numOr);
-        // dump($datePlannig1, $datePlannig2);
-        $datePlanning = empty($datePlannig1) ? new DateTime($datePlannig2[0]['dateplanning2']) : new DateTime($datePlannig1[0]['dateplanning1']);
-        $dateDuJour = new DateTime('now');
-        // dump($datePlanning, $dateDuJour);
+        //première soumission
         $nbrOrSoumis = $this->orRepository->getNbrOrSoumis($numOr);
-        // dd($datePlanning->format('Y-m-d') < $dateDuJour->format('Y-m-d') && (int)$nbrOrSoumis <= 0);
-        return $datePlanning->format('Y-m-d') < $dateDuJour->format('Y-m-d') && (int)$nbrOrSoumis <= 0;
+        $typeLigs = $this->ditOrsoumisAValidationModel->getTypeLigne($numOr);
+        if ((int)$nbrOrSoumis <= 0 && in_array('P', $typeLigs)) {
+            $numItvs = $this->ditOrsoumisAValidationModel->getNumItv($numOr);
+            $dateDuJour = new DateTime('now');
+            foreach ($numItvs as $numItv) {
+                $datePlannig1 = $this->magasinListOrLivrerModel->recupDatePlanningOR1($numOr, $numItv);
+                $datePlannig2 = $this->magasinListOrLivrerModel->recupDatePlanningOR2($numOr, $numItv);
+                $datePlanning = empty($datePlannig1) ? new DateTime($datePlannig2[0]['dateplanning2']) : new DateTime($datePlannig1[0]['dateplanning1']);
+                if ($datePlanning->format('Y-m-d') < $dateDuJour->format('Y-m-d')) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            return false;
+        }
     }
 
-    private function datePlanning($numOr)
+    private function datePlanning($numOr, $numItv)
     {
-        $datePlannig1 = $this->magasinListOrLivrerModel->recupDatePlanning1($numOr);
-        $datePlannig2 = $this->magasinListOrLivrerModel->recupDatePlanning2($numOr);
+        $datePlannig1 = $this->magasinListOrLivrerModel->recupDatePlanningOR1($numOr, $numItv);
+        $datePlannig2 = $this->magasinListOrLivrerModel->recupDatePlanningOR2($numOr, $numItv);
 
         return empty($datePlannig1) ? $datePlannig2[0]['dateplanning2'] : $datePlannig1[0]['dateplanning1'];
     }
