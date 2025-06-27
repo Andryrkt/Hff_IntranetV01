aa56e14de45bff17448965acb8cd033165562f51
Andryrkt <andryrkt08@gmail.com>
2025-06-27 16:27:39 +0300

    rectification d'ajout des donné utile dans le cdefrn Da

--- src/Repository/da/DaValiderRepository.php
+++ src/Repository/da/DaValiderRepository.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\EntityRepository;
 class DaValiderRepository extends EntityRepository
 {
     /**
-     *  Récupère le numéro de version maximum pour une demande d'approvisionnement donnée.
+     *  Récupère le numéro de version maximum pour une demande d'approvisionnement (DA) donnée.
      *
      * @param string $numeroDemandeAppro
      * @return void
@@ -25,7 +25,7 @@ class DaValiderRepository extends EntityRepository
     }
 
     /**
-     *  Récupère le numéro de version maximum pour une numero commande donnée.
+     *  Récupère le numéro de version maximum pour une numero commande (Cde) donnée.
      *
      * @param string $numeroCde
      * @return int
@@ -36,6 +36,27 @@ class DaValiderRepository extends EntityRepository
             ->select('DISTINCT MAX(dav.numeroVersion)')
             ->where('dav.numeroCde = :numCde')
             ->setParameter('numCde', $numeroCde)
+            ->getQuery()
+            ->getSingleScalarResult();
+
+        if ($numeroVersionMax === null) {
+            return 0;
+        }
+        return $numeroVersionMax;
+    }
+
+    /**
+     *  Récupère le numéro de version maximum pour une numero demande d'intervention (DIT) donnée.
+     *
+     * @param string $numeroDit
+     * @return int
+     */
+    public function getNumeroVersionMaxDit(string $numeroDit): int
+    {
+        $numeroVersionMax = $this->createQueryBuilder('dav')
+            ->select('DISTINCT MAX(dav.numeroVersion)')
+            ->where('dav.numeroDemandeDit = :numDit')
+            ->setParameter('numDit', $numeroDit)
             ->getQuery()
             ->getSingleScalarResult();
 
