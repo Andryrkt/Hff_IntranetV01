e58a59199ce1a3e9141ef22eac3bd2f59aeac806
Menja <ranofimenjajam@gmail.com>
2025-07-01 09:08:37 +0300

    dal et dalr

--- Views/js/da/detail/detail.js
+++ Views/js/da/detail/detail.js
@@ -6,6 +6,24 @@ document.addEventListener("DOMContentLoaded", function () {
   allMontantTd.forEach((mtt) => {
     mtt.innerText = formaterNombre(mtt.innerText);
   });
+
+  /** Toggle button pour le + et - */
+  document.querySelectorAll(".toggle-btn").forEach(function (button) {
+    button.addEventListener("click", function () {
+      const parentRow = button.closest("tr");
+      let nextRow = parentRow.nextElementSibling;
+
+      // Toggle les lignes enfants jusqu'à ce qu'on tombe sur une nouvelle ligne parente
+      while (nextRow && nextRow.classList.contains("child-row")) {
+        nextRow.style.display =
+          nextRow.style.display === "none" ? "table-row" : "none";
+        nextRow = nextRow.nextElementSibling;
+      }
+
+      // Change le bouton de + à - et inversement
+      button.textContent = button.textContent === "+" ? "-" : "+";
+    });
+  });
 });
 
 window.addEventListener("load", () => {
