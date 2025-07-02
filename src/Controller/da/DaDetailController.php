7b4fc5e598658235dbafc0620f6109638a9ef2d5
Menja <ranofimenjajam@gmail.com>
2025-07-02 11:14:00 +0300

    envoi d'observation dans le détail

--- src/Controller/da/DaDetailController.php
+++ src/Controller/da/DaDetailController.php
@@ -3,16 +3,20 @@
 namespace App\Controller\da;
 
 use App\Controller\Controller;
+use App\Controller\Traits\lienGenerique;
 use App\Entity\da\DemandeAppro;
 use App\Entity\da\DaObservation;
 use App\Entity\da\DemandeApproL;
 use App\Form\da\DemandeApproFormType;
 use App\Repository\dit\DitRepository;
 use App\Entity\dit\DemandeIntervention;
+use App\Form\da\DaObservationType;
 use App\Model\dit\DitModel;
 use App\Repository\da\DemandeApproRepository;
 use App\Repository\da\DaObservationRepository;
 use App\Repository\da\DemandeApproLRepository;
+use App\Service\EmailService;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -20,7 +24,7 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class DaDetailController extends Controller
 {
-	private const ID_ATELIER = 3;
+	use lienGenerique;
 
 	private DemandeApproRepository $daRepository;
 	private DitRepository $ditRepository;
@@ -39,7 +43,7 @@ class DaDetailController extends Controller
 	/**
 	 * @Route("/detail/{id}", name="da_detail")
 	 */
-	public function deatil(int $id)
+	public function deatil(int $id, Request $request)
 	{
 		//verification si user connecter
 		$this->verifierSessionUtilisateur();
@@ -51,9 +55,15 @@ class DaDetailController extends Controller
 		$numeroVersionMax = $this->daLRepository->getNumeroVersionMax($demandeAppro->getNumeroDemandeAppro());
 		$demandeAppro = $this->filtreDal($demandeAppro, $dit, (int)$numeroVersionMax); // on filtre les lignes de la DA selon le numero de version max
 
+		$daObservation = new DaObservation;
+		$form = self::$validator->createBuilder(DaObservationType::class, $daObservation)->getForm();
+
+		$this->traitementFormulaire($form, $request, $demandeAppro);
+
 		$observations = $this->daObservationRepository->findBy(['numDa' => $demandeAppro->getNumeroDemandeAppro()], ['dateCreation' => 'DESC']);
 
 		self::$twig->display('da/detail.html.twig', [
+			'form'				=> $form->createView(),
 			'demandeAppro'      => $demandeAppro,
 			'observations'      => $observations,
 			'numSerie'          => $dataModel[0]['num_serie'],
@@ -84,9 +94,73 @@ class DaDetailController extends Controller
 		return $demandeAppro;
 	}
 
-	private function estUserDansServiceAtelier()
+	/** 
+	 * Traitement du formulaire
+	 */
+	private function traitementFormulaire($form, Request $request, DemandeAppro $demandeAppro)
 	{
-		$serviceIds = $this->getUser()->getServiceAutoriserIds();
-		return in_array(self::ID_ATELIER, $serviceIds);
+		$form->handleRequest($request);
+
+		if ($form->isSubmitted() && $form->isValid()) {
+			/** @var DaObservation $daObservation daObservation correspondant au donnée du form */
+			$daObservation = $form->getData();
+			$this->insertionObservation($daObservation, $demandeAppro);
+
+			$notification = [
+				'type' => 'success',
+				'message' => 'Votre observation a été enregistré avec succès.',
+			];
+
+			/** ENVOIE D'EMAIL à l'APPRO pour l'observation */
+			$service = $this->estUserDansServiceAtelier() ? 'atelier' : ($this->estUserDansServiceAppro() ? 'appro' : '');
+			$this->envoyerMailObservation([
+				'numDa'         => $demandeAppro->getNumeroDemandeAppro(),
+				'observation'   => $daObservation->getObservation(),
+				'service'       => $service,
+				'userConnecter' => $this->getUser()->getPersonnels()->getNom() . ' ' . $this->getUser()->getPersonnels()->getPrenoms(),
+			]);
+
+			$this->sessionService->set('notification', ['type' => $notification['type'], 'message' => $notification['message']]);
+			$this->redirectToRoute("da_list");
+		}
+	}
+
+	/** 
+	 * Insertion de l'observation dans la Base de donnée
+	 */
+	private function insertionObservation(DaObservation $daObservation, DemandeAppro $demandeAppro)
+	{
+		$daObservation
+			->setNumDa($demandeAppro->getNumeroDemandeAppro())
+			->setUtilisateur($this->getUser()->getNomUtilisateur())
+		;
+
+		self::$em->persist($daObservation);
+		self::$em->flush();
+	}
+
+	/** 
+	 * Fonctions pour envoyer un mail sur l'observation à la service Appro 
+	 */
+	private function envoyerMailObservation(array $tab)
+	{
+		$email       = new EmailService;
+
+		$to = $tab['service'] == 'atelier' ? DemandeAppro::MAIL_APPRO : DemandeAppro::MAIL_ATELIER;
+
+		$content = [
+			'to'        => $to,
+			// 'cc'        => array_slice($emailValidateurs, 1),
+			'template'  => 'da/email/emailDa.html.twig',
+			'variables' => [
+				'statut'     => "commente",
+				'subject'    => "{$tab['numDa']} - Observation ajoutée par l'" . strtoupper($tab['service']),
+				'tab'        => $tab,
+				'action_url' => $this->urlGenerique(str_replace('/', '', $_ENV['BASE_PATH_COURT']) . "/demande-appro/list"),
+			]
+		];
+		$email->getMailer()->setFrom('noreply.email@hff.mg', 'noreply.da');
+		// $email->sendEmail($content['to'], $content['cc'], $content['template'], $content['variables']);
+		$email->sendEmail($content['to'], [], $content['template'], $content['variables']);
 	}
 }
