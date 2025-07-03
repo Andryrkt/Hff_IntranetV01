f52becad99f9cfde28857a7a050e509b6455db88
lanto hasina <andryrkt08@gmail.com>
2025-07-03 16:51:49 +0300

    simple modification

--- src/Controller/da/DaEditController.php
+++ src/Controller/da/DaEditController.php
@@ -203,7 +203,8 @@ class DaEditController extends Controller
 
     private function PeutModifier($demandeAppro)
     {
-        return ($this->estUserDansServiceAtelier() && ($demandeAppro->getStatutDal() == DemandeAppro::STATUT_SOUMIS_APPRO || $demandeAppro->getStatutDal() == DemandeAppro::STATUT_VALIDE || $demandeAppro->getStatutDal() == DemandeAppro::STATUT_AUTORISER_MODIF_ATE));
+        return ($this->estUserDansServiceAtelier() && ($demandeAppro->getStatutDal() == DemandeAppro::STATUT_SOUMIS_APPRO || $demandeAppro->getStatutDal() == DemandeAppro::STATUT_VALIDE ));
+        
     }
 
     private function traitementForm($form, Request $request, DemandeAppro $demandeAppro): void
