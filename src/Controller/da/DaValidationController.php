3fb764d2405c7696b1477a57ba13176a7c7daed4
Menja <ranofimenjajam@gmail.com>
2025-07-30 10:10:03 +0300

    résolution de bug sur la validation d'article

--- src/Controller/da/DaValidationController.php
+++ src/Controller/da/DaValidationController.php
@@ -11,9 +11,11 @@ use App\Entity\da\DemandeApproL;
 use App\Entity\da\DemandeApproLR;
 use App\Controller\Traits\lienGenerique;
 use App\Entity\dit\DemandeIntervention;
+use App\Entity\dit\DitOrsSoumisAValidation;
 use App\Repository\da\DemandeApproRepository;
 use App\Repository\da\DemandeApproLRepository;
 use App\Repository\da\DemandeApproLRRepository;
+use App\Repository\dit\DitOrsSoumisAValidationRepository;
 use App\Repository\dit\DitRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -29,6 +31,7 @@ class DaValidationController extends Controller
     private DemandeApproLRepository $demandeApproLRepository;
     private DemandeApproLRRepository $demandeApproLRRepository;
     private DemandeApproRepository $demandeApproRepository;
+    private DitOrsSoumisAValidationRepository $ditOrsSoumisAValidationRepository;
     private DitRepository $ditRepository;
 
     public function __construct()
@@ -38,6 +41,7 @@ class DaValidationController extends Controller
         $this->demandeApproLRRepository = self::$em->getRepository(DemandeApproLR::class);
         $this->demandeApproRepository = self::$em->getRepository(DemandeAppro::class);
         $this->ditRepository = self::$em->getRepository(DemandeIntervention::class);
+        $this->ditOrsSoumisAValidationRepository = self::$em->getRepository(DitOrsSoumisAValidation::class);
     }
 
     /**
