dc528929bb641d50b96e3bc4d17bff96ed6d9b7c
Andryrkt <andryrkt08@gmail.com>
2025-07-31 13:41:16 +0300

    resolution conflit dans new da

--- src/Controller/da/DaNewController.php
+++ src/Controller/da/DaNewController.php
@@ -196,6 +196,84 @@ class DaNewController extends Controller
         }
     }
 
+    private function traitementFormDirect($form, Request $request, DemandeAppro $demandeAppro): void
+    {
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** 
+             * @var DemandeAppro $demandeAppro
+             */
+            $demandeAppro = $form->getData();
+            dd($demandeAppro);
+            $demandeAppro
+                ->setDemandeur(Controller::getUser()->getNomUtilisateur())
+                ->setNumeroDemandeAppro($this->autoDecrement('DAP'))
+                ->setNiveauUrgence($this->ditRepository->getNiveauUrgence($demandeAppro->getNumeroDemandeDit()))
+            ;
+
+            $numDa = $demandeAppro->getNumeroDemandeAppro();
+            $numDit = $demandeAppro->getNumeroDemandeDit();
+            $numeroVersionMax = $this->demandeApproLRepository->getNumeroVersionMax($numDa);
+
+            $formDAL = $form->get('DAL');
+            /** ajout de ligne de demande appro dans la table Demande_Appro_L */
+            foreach ($demandeAppro->getDAL() as $ligne => $DAL) {
+                /** 
+                 * @var DemandeApproL $DAL
+                 */
+                $DAL
+                    ->setNumeroDemandeAppro($numDa)
+                    ->setNumeroLigne($ligne + 1)
+                    ->setStatutDal(DemandeAppro::STATUT_SOUMIS_APPRO)
+                    ->setNumeroVersion($this->autoIncrement($numeroVersionMax))
+                    ->setPrixUnitaire($this->daModel->getPrixUnitaire($DAL->getArtRefp())[0])
+                    ->setNumeroDit($numDit)
+                    ->setJoursDispo($this->getJoursRestants($DAL))
+                ;
+                $this->traitementFichiers($DAL, $formDAL[$ligne + 1]->get('fileNames')->getData()); // traitement des fichiers uploadés pour chaque ligne DAL
+                if (null === $DAL->getNumeroFournisseur()) {
+                    $this->sessionService->set('notification', ['type' => 'danger', 'message' => 'Erreur : Le nom du fournisseur doit correspondre à l’un des choix proposés.']);
+                    $this->redirectToRoute("da_list");
+                }
+                self::$em->persist($DAL);
+            }
+
+            /** Modifie la colonne dernière_id dans la table applications */
+            $application = self::$em->getRepository(Application::class)->findOneBy(['codeApp' => 'DAP']);
+            $application->setDerniereId($numDa);
+            self::$em->persist($application);
+
+            /** Ajout de demande appro dans la base de donnée (table: Demande_Appro) */
+            self::$em->persist($demandeAppro);
+
+            /** ajout de l'observation dans la table da_observation si ceci n'est pas null */
+            if ($demandeAppro->getObservation() !== null) {
+                $this->insertionObservation($demandeAppro);
+            }
+
+            self::$em->flush();
+
+            /** ENVOIE D'EMAIL */
+            $numeroVersionMax = $this->demandeApproLRepository->getNumeroVersionMax($numDa);
+            $dal = $this->demandeApproLRepository->findBy(['numeroDemandeAppro' => $numDa, 'numeroVersion' => $numeroVersionMax]);
+
+            $this->envoyerMailAuxAppros([
+                'id'            => $demandeAppro->getId(),
+                'numDa'         => $numDa,
+                'objet'         => $demandeAppro->getObjetDal(),
+                'detail'        => $demandeAppro->getDetailDal(),
+                'dal'           => $dal,
+                'service'       => 'atelier',
+                'observation'   => $demandeAppro->getObservation() !== null ? $demandeAppro->getObservation() : '-',
+                'userConnecter' => Controller::getUser()->getPersonnels()->getNom() . ' ' . Controller::getUser()->getPersonnels()->getPrenoms(),
+            ]);
+
+            $this->sessionService->set('notification', ['type' => 'success', 'message' => 'Votre demande a été enregistrée']);
+            $this->redirectToRoute("da_list");
+        }
+    }
+
     /** 
      * Fonctions pour envoyer un mail à la service Appro 
      */
