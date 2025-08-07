c9c7bd2b5e6961544ae8121abd704178689dfa51
Menja <ranofimenjajam@gmail.com>
2025-08-07 10:42:05 +0300

    validation d'une DA direct

--- src/Controller/Traits/da/DaValidationDirectTrait.php
+++ src/Controller/Traits/da/DaValidationDirectTrait.php
@@ -2,4 +2,75 @@
 
 namespace App\Controller\Traits\da;
 
-trait DaValidationTrait {}
+use App\Entity\da\DaValider;
+use App\Entity\da\DemandeApproL;
+use App\Entity\da\DemandeApproLR;
+use App\Repository\da\DaValiderRepository;
+use App\Service\autres\VersionService;
+
+trait DaValidationDirectTrait
+{
+    use DaTrait;
+    use DaValidationTrait;
+
+    //=====================================================================================
+    private DaValiderRepository $daValiderRepository;
+
+    /**
+     * Initialise les valeurs par défaut du trait
+     */
+    public function initDaValidationDirectTrait(): void
+    {
+        $em = $this->getEntityManager();
+        $this->daValiderRepository = $em->getRepository(DaValider::class);
+    }
+    //=====================================================================================
+
+    /** 
+     * Création du fichier Excel pour une DA directe
+     * 
+     * @param string $numDa
+     * @param int $numeroVersion
+     * @return array
+     */
+    private function creationFichierExcelDirect(string $numDa, int $numeroVersion): array
+    {
+        return $this->genererFichierExcelPourDa($numDa, $numeroVersion, function ($numDa, $donnees) {
+            $this->enregistrerDaDirectDansDaValider($numDa, $donnees); // Enregistrement des données dans DaValider
+        });
+    }
+
+    /** 
+     * Enregistre les données de la DA directe dans DaValider
+     * 
+     * @param string $numDa
+     * @param array $donnees
+     * @return void
+     */
+    private function enregistrerDaDirectDansDaValider(string $numDa, array $donnees): void
+    {
+        $em = $this->getEntityManager();
+        $da = $this->demandeApproRepository->findOneBy(['numeroDemandeAppro' => $numDa]);
+
+        foreach ($donnees as $donnee) {
+            $daValider = new DaValider;
+
+            $numeroVersion = $this->daValiderRepository->getNumeroVersionMax($numDa);
+            $daValider
+                ->setNumeroVersion(VersionService::autoIncrement($numeroVersion)) // numero de version de DaValider
+            ;
+
+            $daValider->enregistrerDa($da); // enregistrement pour DA
+
+            if ($donnee instanceof DemandeApproL) {
+                $daValider->enregistrerDal($donnee); // enregistrement pour DAL
+            } else if ($donnee instanceof DemandeApproLR) {
+                $daValider->enregistrerDalr($donnee); // enregistrement pour DALR
+            }
+
+            $em->persist($daValider);
+        }
+
+        $em->flush();
+    }
+}
--- src/Controller/Traits/da/DaValidationTrait.php
+++ src/Controller/Traits/da/DaValidationTrait.php
@@ -2,15 +2,14 @@
 
 namespace App\Controller\Traits\da;
 
-use App\Controller\Traits\EntityManagerAwareTrait;
 use App\Entity\da\DemandeAppro;
 use App\Entity\da\DemandeApproL;
 use App\Entity\da\DemandeApproLR;
+use DateTime;
 
 trait DaValidationTrait
 {
     use DaTrait;
-    use EntityManagerAwareTrait;
 
     /** 
      * Modification des tables DemandeAppro, DemandeApproL et DemandeApproLR
@@ -73,6 +72,13 @@ trait DaValidationTrait
         return $da;
     }
 
+    /** 
+     * Mettre à jour le choix d'une ligne DALR
+     * 
+     * @param DemandeApproLR $dalr
+     * @param array $refsValide
+     * @return void
+     */
     private function mettreAJourChoixDalr(DemandeApproLR $dalr, array $refsValide): void
     {
         if (empty($refsValide)) return;
@@ -85,5 +91,82 @@ trait DaValidationTrait
         if (isset($refsValide[$numeroLigne]) && $numeroLigneTableau === $refsValide[$numeroLigne]) {
             $dalr->setChoix(true);
         }
+    }
+
+    /** 
+     * Fonction pour générer un fichier Excel pour une DA
+     * 
+     * @param string $numDa
+     * @param int $numeroVersion
+     * @return array
+     */
+    private function genererFichierExcelPourDa(string $numDa, int $numeroVersion, callable $strategieEnregistrement): array
+    {
+        // 1. Récupération des lignes rectifiées de la DA
+        $donnees = $this->getLignesRectifieesDA($numDa, $numeroVersion);
+
+        // 2. Enregistrement dans la table DaValider selon la stratégie (direct ou avec dit)
+        $strategieEnregistrement($numDa, $donnees);
+
+        // 3. Transformation des entités en tableau pour Excel
+        $donneesExcel = $this->convertirEntitesPourExcel($donnees);
+
+        // 4. Génération du fichier Excel
+        $fileName = $this->genererNomFichierExcel($numDa);
+        $filePath = $this->genererCheminFichierExcel($numDa, $fileName);
+        $this->excelService->createSpreadsheetEnregistrer($donneesExcel, $filePath);
+
+        return [
+            'fileName' => $fileName,
+            'filePath' => $filePath
+        ];
+    }
+
+    /** 
+     * Convertit les entités en tableau pour Excel
+     * 
+     * @param array $entities
+     * @return array
+     */
+    private function convertirEntitesPourExcel(array $entities): array
+    {
+        $tableau = [];
+        $tableau[] = ['constructeur', 'reference', 'quantité', '', 'designation', 'PU'];
+
+        foreach ($entities as $entity) {
+            $tableau[] = [
+                $entity->getArtConstp(),
+                $entity->getArtRefp(),
+                $entity->getQteDem(),
+                '',
+                $entity->getArtConstp() === 'ZDI' || $entity->getArtRefp() === 'ST' ? $entity->getArtDesi() : '',
+                $entity->getArtConstp() === 'ZDI' || $entity->getArtRefp() === 'ST' ? $entity->getPrixUnitaire() : '',
+            ];
+        }
+
+        return $tableau;
+    }
+
+    /** 
+     * Génère le nom du fichier Excel
+     * 
+     * @param string $numDa
+     * @return string
+     */
+    private function genererNomFichierExcel(string $numDa): string
+    {
+        return $numDa . '_' . (new DateTime())->format('Ymd_His') . '.xlsx';
+    }
+
+    /** 
+     * Génère le chemin complet du fichier Excel
+     * 
+     * @param string $numDa
+     * @param string $fileName
+     * @return string
+     */
+    private function genererCheminFichierExcel(string $numDa, string $fileName): string
+    {
+        return $_ENV['BASE_PATH_FICHIER'] . "/da/$numDa/$fileName";
     }
 }
