2b7057e7224b2f5a564da78b07de88fd3750c355
Menja <ranofimenjajam@gmail.com>
2025-08-11 15:49:07 +0300

    résolution merge branch

--- src/Controller/Traits/da/DaListeTrait.php
+++ src/Controller/Traits/da/DaListeTrait.php
@@ -2,34 +2,12 @@
 
 namespace App\Controller\Traits\da;
 
-use DateTime;
 use App\Entity\da\DemandeAppro;
 use App\Entity\da\DemandeApproL;
 
 trait DaListeTrait
 {
-        /**
-     * Permet de calculer le nombre de jours disponibles avant la date de fin souhaitée
-     *
-     * @param DemandeApproL $dal
-     * @return int Nombre de jours disponibles (positif si la date n'est pas encore passée, négatif si elle l'est)
-     */
-    public function getJoursRestants(DemandeApproL $dal): int
-    {
-        // --- 1. Mettre les deux dates à minuit (00:00:00) ---
-        $dateFin     = clone $dal->getDateFinSouhaite(); // on clone pour ne pas modifier l'objet de l'entity
-        $dateFin->setTime(0, 0, 0);                      // Y-m-d 00:00:00
-
-        $aujourdhui  = new DateTime('today');            // 'today' crée déjà la date du jour à 00:00:00
-
-        // --- 2. Calculer la différence ---
-        $interval = $aujourdhui->diff($dateFin);         // toujours positif dans $interval->days
-        $days     = $interval->invert ? -$interval->days // invert = 1 si $dateFin est passée
-            :  $interval->days;
-
-        // --- 3. Retourner la valeur ---
-        return $days;
-    }
+    use DaTrait;
 
     /**
      * Met à jour le champ `joursDispo` pour chaque DAL sauf si elle est déjà validée.
--- src/Controller/Traits/da/DaTrait.php
+++ src/Controller/Traits/da/DaTrait.php
@@ -58,7 +58,28 @@ trait DaTrait
     }
     //=====================================================================================
 
+    /**
+     * Permet de calculer le nombre de jours disponibles avant la date de fin souhaitée
+     *
+     * @param DemandeApproL $dal
+     * @return int Nombre de jours disponibles (positif si la date n'est pas encore passée, négatif si elle l'est)
+     */
+    public function getJoursRestants(DemandeApproL $dal): int
+    {
+        // --- 1. Mettre les deux dates à minuit (00:00:00) ---
+        $dateFin     = clone $dal->getDateFinSouhaite(); // on clone pour ne pas modifier l'objet de l'entity
+        $dateFin->setTime(0, 0, 0);                      // Y-m-d 00:00:00
 
+        $aujourdhui  = new DateTime('today');            // 'today' crée déjà la date du jour à 00:00:00
+
+        // --- 2. Calculer la différence ---
+        $interval = $aujourdhui->diff($dateFin);         // toujours positif dans $interval->days
+        $days     = $interval->invert ? -$interval->days // invert = 1 si $dateFin est passée
+            :  $interval->days;
+
+        // --- 3. Retourner la valeur ---
+        return $days;
+    }
 
     /** 
      * Fonction pour l'insertion d'une observation
@@ -161,7 +182,7 @@ trait DaTrait
 
 
 
-    
+
 
     private function creationPdf(string $numDa, int $numeroVersionMax)
     {
