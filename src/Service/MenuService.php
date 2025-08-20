cfde82b6dcf6cbeb1def45dc0b2f5d45a0e02e28
Menja <ranofimenjajam@gmail.com>
2025-08-20 16:53:32 +0300

    ajout de target blank dans le lien

--- src/Service/MenuService.php
+++ src/Service/MenuService.php
@@ -33,7 +33,7 @@ class MenuService
             'folder',
             [
                 $this->createSimpleItem('Annuaire', 'address-book', '#'),
-                $this->createSimpleItem('Plan analytique HFF', 'ruler-vertical', '/Upload/documentation/Structure%20analytique%20HFF.pdf'),
+                $this->createSimpleItem('Plan analytique HFF', 'ruler-vertical', '/Upload/documentation/Structure%20analytique%20HFF.pdf', [], "_blank"),
                 $this->createSimpleItem('Documentation interne', 'folder-tree', 'documentation_interne')
             ]
         );
@@ -298,12 +298,13 @@ class MenuService
     /**
      * Crée un item simple sans sous-menu
      */
-    public function createSimpleItem(string $label, ?string $icon = null, string $link = '#', array $routeParams = []): array
+    public function createSimpleItem(string $label, ?string $icon = null, string $link = '#', array $routeParams = [], string $target = ""): array
     {
         return [
             'title' => $label,
             'link' => $link,
             'icon' => 'fas fa-' . ($icon ?? 'file'),
+            'target' => $target,
             'routeParams' => $routeParams
         ];
     }
