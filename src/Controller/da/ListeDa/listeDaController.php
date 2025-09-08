0d314beb6e0baa78f6a504ac6afd9f9cd60f4d4a
Menja <ranofimenjajam@gmail.com>
2025-09-08 11:01:10 +0300

    front pour le tri (classe et JS)

--- src/Controller/da/ListeDa/listeDaController.php
+++ src/Controller/da/ListeDa/listeDaController.php
@@ -56,17 +56,37 @@ class listeDaController extends Controller
         //nombre de ligne par page
         $limit = 20;
 
+        $allowedSortFields = ['joursDispo']; // Champs autorisés pour le tri
+        // Récupération des paramètres actuels pour le tri
+        $sortField = $request->query->get('sort');
+        $sortDirection = $request->query->get('direction');
+
+        // Valide uniquement si les deux paramètres existent et sont valides
+        if (!in_array($sortField, $allowedSortFields) || !in_array(strtolower($sortDirection), ['asc', 'desc'])) {
+            $sortField = null;
+            $sortDirection = null;
+        }
+
+        $currentSortClass = [
+            'asc'  => $sortDirection === 'asc' ? 'text-warning' : 'text-white',
+            'desc' => $sortDirection === 'desc' ? 'text-warning' : 'text-white',
+        ];
+
+        $criteria['sort'] = $sortField; // Champ de tri
+        $criteria['direction'] = $sortDirection; // Direction de tri
+
         // Donnée à envoyer à la vue
-        $paginationData = $this->getPaginationData($criteria, $page, $limit);
+        $paginationData = $this->getPaginationData($criteria, $page, $limit, $sortField, $sortDirection);
         $dataPrepared = $this->prepareDataForDisplay($paginationData['data']);
 
         return $this->render('da/list-da.html.twig', [
-            'data'        => $dataPrepared,
-            'form'        => $form->createView(),
-            'criteria'    => $criteria,
-            'currentPage' => $paginationData['currentPage'],
-            'totalPages'  => $paginationData['lastPage'],
-            'resultat'    => $paginationData['totalItems'],
+            'data'             => $dataPrepared,
+            'form'             => $form->createView(),
+            'criteria'         => $criteria,
+            'currentSortClass' => $currentSortClass,
+            'currentPage'      => $paginationData['currentPage'],
+            'totalPages'       => $paginationData['lastPage'],
+            'resultat'         => $paginationData['totalItems'],
         ]);
     }
 }
