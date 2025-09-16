f1c5525a273d20683ca2e1aa6b702c927e8ca965
Menja <ranofimenjajam@gmail.com>
2025-09-16 17:06:22 +0300

    chagnement de l'interface de congé et contrat

--- src/Controller/ddc/NewCongeController.php
+++ src/Controller/ddc/NewCongeController.php
@@ -26,25 +26,8 @@ class NewCongeController extends Controller
         $this->autorisationAcces($this->getUser(), Application::ID_DDC);
         /** FIN AUtorisation accès */
 
-        // ⚠️ URL distante (celle que tu veux masquer)
-        $url = "https://hffc.docuware.cloud/docuware/formsweb/demande-de-conges-new?orgID=5adf2517-2f77-4e19-8b42-9c3da43af7be";
-
-        // Récupération du contenu distant (sans HttpClient)
-        $context = stream_context_create([
-            "http" => [
-                "header" => "User-Agent: Symfony-Proxy\r\n"
-            ]
-        ]);
-
-        $content = @file_get_contents($url, false, $context);
-
-        if ($content === false) {
-            $content = "<p>Impossible de charger le contenu distant.</p>";
-        }
-
-        // Tu passes ça dans Twig (par ex. pour l'afficher dans un <div>)
         return $this->render('ddc/conge_new.html.twig', [
-            'content' => $content,
+            'url' => "https://hffc.docuware.cloud/docuware/formsweb/demande-de-conges-new?orgID=5adf2517-2f77-4e19-8b42-9c3da43af7be",
         ]);
     }
 }
