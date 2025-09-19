c780f181b8c4d15f5232386a7dd69602da5eae85
Menja <ranofimenjajam@gmail.com>
2025-09-19 16:07:20 +0300

    statut OR (DW) des DA

--- src/Entity/da/DemandeAppro.php
+++ src/Entity/da/DemandeAppro.php
@@ -27,17 +27,17 @@ class DemandeAppro
     public const ID_ATELIER                 = 3;
     private const MAIL_APPRO_PROD           = 'appro@hff.mg';
     private const MAIL_APPRO_TEST           = 'hoby.ralahy@hff.mg';
-    public const MAIL_APPRO                 = self::MAIL_APPRO_TEST;        // TODO: à changer selon environnement (PROD | TEST)
+    public const MAIL_APPRO                 = self::MAIL_APPRO_TEST;       // TODO: à changer selon environnement (PROD | TEST)
     public const STATUT_VALIDE              = 'Bon d’achats validé';       /*__ DA direct et DA via OR __*/ // cliquable par Admin et Appro
     public const STATUT_TERMINER            = 'TERMINER';                  /*__ DA direct et DA via OR __*/ // ! non cliquable par quiconque
     public const STATUT_EN_COURS_CREATION   = 'En cours de création';      /*_________ DA via OR ________*/ // cliquable par Admin et Atelier
     public const STATUT_SOUMIS_APPRO        = 'Demande d’achats';          /*__ DA direct et DA via OR __*/ // cliquable par Admin et Appro
     public const STATUT_AUTORISER_MODIF_ATE = 'Création demande initiale'; /*_________ DA via OR ________*/ // cliquable par Admin et Atelier
     public const STATUT_SOUMIS_ATE          = 'Proposition achats';        /*__ DA direct et DA via OR __*/ // cliquable par Admin et (Atelier ou service emetteur) et Appro
-    public const STATUT_DW_A_VALIDE         = 'Soumis à validation';       /*_________ DA direct ________*/ // ! non cliquable par quiconque
-    public const STATUT_DW_REFUSEE          = 'DA Refusée';                /*_________ DA direct ________*/ // ! non cliquable par quiconque
-    public const STATUT_DW_VALIDEE          = 'DA Validée';                /*_________ DA direct ________*/ // ! non cliquable par quiconque
-    public const STATUT_DW_A_MODIFIER       = 'DA à modifier';             /*_________ DA direct ________*/ // cliquable par Admin , service emetteur + chef de service
+    public const STATUT_DW_A_VALIDE         = 'DA soumis à validation';    /*_________ DA direct ________*/ // ! non cliquable par quiconque
+    public const STATUT_DW_REFUSEE          = 'DA refusée';                /*_________ DA direct ________*/ // ! non cliquable par quiconque
+    public const STATUT_DW_VALIDEE          = 'DA validée';                /*_________ DA direct ________*/ // cliquable par Admin et Appro
+    public const STATUT_DW_A_MODIFIER       = 'DA à modifier';             /*_________ DA direct ________*/ // cliquable par Admin et service emetteur et Appro
 
     /**
      * @ORM\Id
