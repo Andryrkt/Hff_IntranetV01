85af599fdfc3b0ec86e6e16c3931ebebc7ea19e9
lanto hasina <andryrkt08@gmail.com>
2025-09-22 08:16:05 +0300

    correction rapide dans main

--- src/Controller/dit/Facture/DitFactureSoumisAValidationController.php
+++ src/Controller/dit/Facture/DitFactureSoumisAValidationController.php
@@ -168,7 +168,8 @@ class DitFactureSoumisAValidationController extends Controller
 
                     if ($interneExterne === 'INTERNE') {
                         $ficherAfusioner = $this->fileUploaderService->insertFileAtPosition($pathFichiers, $pathPageDeGarde, 0);
-                        $this->fusionPdf->mergePdfs($ficherAfusioner, $pathPageDeGarde);
+                        $fichierConvertie = $this->ConvertirLesPdf($ficherAfusioner);
+                        $this->fusionPdf->mergePdfs($fichierConvertie, $pathPageDeGarde);
                         $this->genererPdfFacture->copyToDwFactureSoumis($this->ditFactureSoumiAValidation->getNumeroSoumission(), $numFac);
                     } else {
                         $this->genererPdfFacture->copyToDwFacture($this->ditFactureSoumiAValidation->getNumeroSoumission(), $numFac);
@@ -192,6 +193,51 @@ class DitFactureSoumisAValidationController extends Controller
             'form' => $form->createView(),
         ]);
     }
+
+    private function ConvertirLesPdf(array $tousLesFichersAvecChemin): array
+    {
+        $tousLesFichiers = [];
+        foreach ($tousLesFichersAvecChemin as $filePath) {
+            $tousLesFichiers[] = $this->convertPdfWithGhostscript($filePath);
+        }
+
+        return $tousLesFichiers;
+    }
+
+
+    private function convertPdfWithGhostscript($filePath)
+    {
+        $gsPath = 'C:\Program Files\gs\gs10.05.0\bin\gswin64c.exe'; // Modifier selon l'OS
+        $tempFile = $filePath . "_temp.pdf";
+
+        // Vérifier si le fichier existe et est accessible
+        if (!file_exists($filePath)) {
+            throw new Exception("Fichier introuvable : $filePath");
+        }
+
+        if (!is_readable($filePath)) {
+            throw new Exception("Le fichier PDF ne peut pas être lu : $filePath");
+        }
+
+        // Commande Ghostscript
+        $command = "\"$gsPath\" -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -o \"$tempFile\" \"$filePath\"";
+        // echo "Commande exécutée : $command<br>";
+
+        exec($command, $output, $returnVar);
+
+        if ($returnVar !== 0) {
+            echo "Sortie Ghostscript : " . implode("\n", $output);
+            throw new Exception("Erreur lors de la conversion du PDF avec Ghostscript");
+        }
+
+        // Remplacement du fichier
+        if (!rename($tempFile, $filePath)) {
+            throw new Exception("Impossible de remplacer l'ancien fichier PDF.");
+        }
+
+        return $filePath;
+    }
+
 
     public function enregistrerPdf($dataForm, string $numDit, $factureSoumisAValidation, string $interneExterne)
     {
