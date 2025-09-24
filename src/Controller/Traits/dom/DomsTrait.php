1c5681bb12fee6f02b92b56e77e760fb3ee4e522
Andryrkt <andryrkt08@gmail.com>
2025-09-24 14:48:48 +0300

    correction bug dom

--- src/Controller/Traits/dom/DomsTrait.php
+++ src/Controller/Traits/dom/DomsTrait.php
@@ -31,7 +31,7 @@ trait DomsTrait
         $dom->setMatricule($form1Data['matricule']);
         $dom->setSalarier($form1Data['salarier']);
         $dom->setSousTypeDocument($form1Data['sousTypeDocument']);
-        $dom->setCategorie($form1Data['categorie']);
+        $dom->setCategorie($form1Data['categoryId']);
         $dom->setDateDemande(new \DateTime());
         if ($form1Data['salarier'] === "TEMPORAIRE") {
             $dom->setNom($form1Data['nom']);
@@ -84,7 +84,7 @@ trait DomsTrait
     private function criteria($form1Data, $em)
     {
         $sousTypedocument = $form1Data['sousTypeDocument'];
-        $catg = $form1Data['categorie'];
+        $catg = $form1Data['categoryId'];
 
         $agenceServiceEmetteur =  $this->agenceServiceIpsObjet();
 
@@ -307,8 +307,8 @@ trait DomsTrait
         }
 
         $sousTypeDocument = $em->getRepository(SousTypeDocument::class)->find($form1Data['sousTypeDocument']->getId());
-        if (isset($form1Data['categorie'])) {
-            $categoryId = $em->getRepository(Catg::class)->find($form1Data['categorie']->getId());
+        if (isset($form1Data['categoryId'])) {
+            $categoryId = $em->getRepository(Catg::class)->find($form1Data['categoryId']->getId());
         } else {
             $categoryId = null;
         }
