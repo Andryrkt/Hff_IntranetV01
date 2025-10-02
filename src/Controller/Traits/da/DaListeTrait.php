63655f63a8674282ad0467f34571bfbfe01736d3
Menja <ranofimenjajam@gmail.com>
2025-10-02 11:24:23 +0300

    ajout des deux statuts

--- src/Controller/Traits/da/DaListeTrait.php
+++ src/Controller/Traits/da/DaListeTrait.php
@@ -60,6 +60,8 @@ trait DaListeTrait
             DemandeAppro::STATUT_SOUMIS_ATE          => 'bg-proposition-achat',
             DemandeAppro::STATUT_DW_A_VALIDE         => 'bg-soumis-validation',
             DemandeAppro::STATUT_SOUMIS_APPRO        => 'bg-demande-achat',
+            DemandeAppro::STATUT_DEMANDE_DEVIS       => 'bg-demande-devis',
+            DemandeAppro::STATUT_DEVIS_A_RELANCER    => 'bg-devis-a-relancer',
             DemandeAppro::STATUT_EN_COURS_CREATION   => 'bg-en-cours-creation',
             DemandeAppro::STATUT_AUTORISER_MODIF_ATE => 'bg-creation-demande-initiale',
         ];
