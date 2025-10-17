e24cb5402309e16f6190911c667145d8b2aaed4b
Menja <ranofimenjajam@gmail.com>
2025-10-17 14:13:13 +0300

    ajout de statut en cours de proposition

--- src/Repository/da/DaAfficherRepository.php
+++ src/Repository/da/DaAfficherRepository.php
@@ -528,6 +528,7 @@ class DaAfficherRepository extends EntityRepository
                 ->setParameter('authorizedStatuts', [
                     DemandeAppro::STATUT_SOUMIS_APPRO,
                     DemandeAppro::STATUT_SOUMIS_ATE,
+                    DemandeAppro::STATUT_EN_COURS_PROPOSITION,
                     DemandeAppro::STATUT_AUTORISER_MODIF_ATE,
                     DemandeAppro::STATUT_VALIDE,
                     DemandeAppro::STATUT_TERMINER
