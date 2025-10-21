d0014318785e79da1fda786557f513694f4f43e6
Menja <ranofimenjajam@gmail.com>
2025-10-21 09:11:46 +0300

    front pour les formulaire DW

--- Views/js/formsDw/bg-color.js
+++ Views/js/formsDw/bg-color.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const content = document.querySelector(".content");
-  const bandeau = document.querySelector("#bandeau-orange-cat");
-  if (content) content.classList.add("bg-orange-cat");
-  if (bandeau) bandeau.classList.add("bg-orange-cat");
+  const bandeau = document.querySelector("#bandeau");
+  if (content) content.classList.add(bandeau.dataset.bgColor);
+  if (bandeau) bandeau.classList.add(bandeau.dataset.bgColor);
 });
