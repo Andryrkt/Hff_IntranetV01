a7a10464ff8f5edd861cb2a8116ccf8acb1827b5
Menja <ranofimenjajam@gmail.com>
2025-10-21 15:45:24 +0300

    ajout des repositorys dans le controller

--- src/Controller/da/ListeCdeFrn/ActionSurNonDispoController.php
+++ src/Controller/da/ListeCdeFrn/ActionSurNonDispoController.php
@@ -2,8 +2,16 @@
 
 namespace App\Controller\da\ListeCdeFrn;
 
+use App\Entity\da\DaAfficher;
 use App\Controller\Controller;
+use App\Entity\da\DemandeAppro;
+use App\Entity\da\DemandeApproL;
+use App\Entity\da\DemandeApproLR;
+use App\Repository\da\DaAfficherRepository;
+use App\Repository\da\DemandeApproRepository;
 use Symfony\Component\HttpFoundation\Request;
+use App\Repository\da\DemandeApproLRepository;
+use App\Repository\da\DemandeApproLRRepository;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -12,6 +20,22 @@ use Symfony\Component\HttpFoundation\JsonResponse;
  */
 class ActionSurNonDispoController extends Controller
 {
+    private DaAfficherRepository $daAfficherRepository;
+    private DemandeApproRepository $demandeApproRepository;
+    private DemandeApproLRepository $demandeApproLRepository;
+    private DemandeApproLRRepository $demandeApproLRRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $em                             = $this->getEntityManager();
+        $this->daAfficherRepository     = $em->getRepository(DaAfficher::class);
+        $this->demandeApproRepository   = $em->getRepository(DemandeAppro::class);
+        $this->demandeApproLRepository  = $em->getRepository(DemandeApproL::class);
+        $this->demandeApproLRRepository = $em->getRepository(DemandeApproLR::class);
+    }
+
     /**
      * @Route("/da-list-cde-frn/delete-articles", name="api_list_cde_frn_delete_articles", methods={"POST"})
      */
