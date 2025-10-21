baf83789ebad0cf3b96f5dc67353cc810d4bd211
Menja <ranofimenjajam@gmail.com>
2025-10-21 08:49:50 +0300

    bg orange CAT d'une formulaire DW

---
+++ Views/js/formsDw/orange-cat.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const content = document.querySelector(".content");
+  const bandeauNewConge = document.querySelector("#bandeau-orange-cat");
+  if (content) content.classList.add("bg-orange-cat");
+  if (bandeauNewConge) bandeauNewConge.classList.add("bg-orange-cat");
+});
