4eb5c29498e8c20ac90ad16a1bcce25babf5ee10
Menja <ranofimenjajam@gmail.com>
2025-10-21 09:12:47 +0300

    suppression des js inutiles

--- Views/js/contrat/contrat.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const content = document.querySelector(".content");
-  const bandeau = document.querySelector("#bandeau-bleu-hff");
-  if (content) content.classList.add("bg-bleu-hff");
-  if (bandeau) bandeau.classList.add("bg-bleu-hff");
-});
--- Views/js/formsDw/orange-cat.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const content = document.querySelector(".content");
-  const bandeau = document.querySelector("#bandeau-orange-cat");
-  if (content) content.classList.add("bg-orange-cat");
-  if (bandeau) bandeau.classList.add("bg-orange-cat");
-});
