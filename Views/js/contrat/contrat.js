84297ed7ad54e70f48720d69e3fec78df230cebe
Menja <ranofimenjajam@gmail.com>
2025-10-21 08:54:13 +0300

    bg bleu HFF

--- Views/js/contrat/contrat.js
+++ Views/js/contrat/contrat.js
@@ -1,10 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const content = document.querySelector(".content");
-  const bandeauNewConge = document.querySelector("#bandeau-new-contrat");
-  if (content) {
-    content.classList.add("bg-bleu-hff");
-  }
-  if (bandeauNewConge) {
-    bandeauNewConge.classList.add("bg-bleu-hff");
-  }
+  const bandeau = document.querySelector("#bandeau-bleu-hff");
+  if (content) content.classList.add("bg-bleu-hff");
+  if (bandeau) bandeau.classList.add("bg-bleu-hff");
 });
--- Views/js/formsDw/orange-cat.js
+++ Views/js/formsDw/orange-cat.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
   const content = document.querySelector(".content");
-  const bandeauNewConge = document.querySelector("#bandeau-orange-cat");
+  const bandeau = document.querySelector("#bandeau-orange-cat");
   if (content) content.classList.add("bg-orange-cat");
-  if (bandeauNewConge) bandeauNewConge.classList.add("bg-orange-cat");
+  if (bandeau) bandeau.classList.add("bg-orange-cat");
 });
