69c63413519a846e29491a1164068aa6eb29d4ed
Menja <ranofimenjajam@gmail.com>
2025-10-22 10:18:39 +0300

    ajout de trait de préparation de données

---
+++ src/Traits/PrepareData.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Traits;
+
+trait PrepareData
+{
+    private function prepareDataForMailPropositionDa(): array
+    {
+        $datasPrepared = [];
+        return $datasPrepared;
+    }
+}
