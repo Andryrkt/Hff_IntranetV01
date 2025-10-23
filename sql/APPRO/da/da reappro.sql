5e1545e299cd5ac38d28e06d6355c20db66a1413
Menja <ranofimenjajam@gmail.com>
2025-10-23 16:32:29 +0300

    ajout de da_article_reappro, objet + bdd + repository

---
+++ sql/APPRO/da/da reappro.sql
@@ -0,0 +1,14 @@
+CREATE TABLE da_article_reappro 
+(
+    id int IDENTITY(1,1) NOT NULL,
+    art_constp varchar(3) NOT NULL,
+    art_refp varchar(50) NOT NULL,
+    art_desi varchar(100) NOT NULL,
+    qte_validee_appro VARCHAR(100) NOT NULL,
+    art_pu VARCHAR(100) NOT NULL,
+    code_agence varchar(2) NULL,
+    code_service varchar(3) NULL,
+    date_creation DATETIME2(0) NULL,
+    date_modification DATETIME2(0) NULL,
+    CONSTRAINT PK_da_article_reappro PRIMARY KEY (id)
+);
