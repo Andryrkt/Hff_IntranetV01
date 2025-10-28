04b39e0e4d36770fc91426c9da66103747570b68
Menja <ranofimenjajam@gmail.com>
2025-10-28 11:07:50 +0300

    modif du form type de la daobservationtype

--- src/Form/da/DaObservationType.php
+++ src/Form/da/DaObservationType.php
@@ -15,14 +15,18 @@ class DaObservationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $datypeId = $options['daTypeId'];
-        if ($datypeId == DemandeAppro::TYPE_DA_DIRECT) $observationLabel = 'Autoriser le service à modifier';
-        if ($datypeId == DemandeAppro::TYPE_DA_AVEC_DIT) $observationLabel = 'Autoriser l’ATELIER à modifier';
+
+        if ($datypeId != DemandeAppro::TYPE_DA_REAPPRO) {
+            if ($datypeId == DemandeAppro::TYPE_DA_DIRECT) $observationLabel = 'Autoriser le service à modifier';
+            if ($datypeId == DemandeAppro::TYPE_DA_AVEC_DIT) $observationLabel = 'Autoriser l’ATELIER à modifier';
+            $builder
+                ->add('statutChange', CheckboxType::class, [
+                    'label'    => $observationLabel,
+                    'required' => false
+                ]);
+        }
 
         $builder
-            ->add('statutChange', CheckboxType::class, [
-                'label'    => $observationLabel,
-                'required' => false
-            ])
             ->add('observation', TextareaType::class, [
                 'label' => 'Observation',
                 'attr'  => [
