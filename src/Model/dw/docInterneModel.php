7121d11a22d4a2cb25a2a7f7bd332ad6a4c5925c
Menja <ranofimenjajam@gmail.com>
2025-11-13 10:07:50 +0300

    résolution de bug sur la documentation interne + amélioration

--- src/Model/dw/docInterneModel.php
+++ src/Model/dw/docInterneModel.php
@@ -7,12 +7,15 @@ use App\Model\Model;
 
 class docInterneModel extends Model
 {
+    use ConversionTrait;
+
     public function getDistinctColumn($column)
     {
-        $statement = "SELECT DISTINCT $column FROM DW_Processus_procedure";
+        $statement = "SELECT DISTINCT $column FROM DW_Processus_procedure where $column is not null";
         $result = $this->connexion->query($statement);
         $data = [];
-        while ($tabType = odbc_fetch_array($result)) {
+        while ($resultStmt = odbc_fetch_array($result)) {
+            $tabType = $this->convertirEnUtf8($resultStmt);
             $data[$tabType[$column]] = $tabType[$column];
         }
         return $data;
