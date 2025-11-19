3a09ad2b016450e56a83614f61f67cdb5ca6bf50
Menja <ranofimenjajam@gmail.com>
2025-11-19 10:00:28 +0300

    index from lines dans DA direct

--- Views/js/da/newDirect/new.js
+++ Views/js/da/newDirect/new.js
@@ -3,7 +3,7 @@ import { ajouterUneLigne } from "./dal";
 import { handleAgenceChange } from "../../dit/fonctionUtils/fonctionListDit.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-  localStorage.setItem("daDirectLineCounter", 0); // initialiser le nombre de ligne à 0
+  buildIndexFromLines(); // initialiser le compteur de ligne pour la création d'une DA directe
 
   document
     .getElementById("add-child")
@@ -52,6 +52,46 @@ document.addEventListener("DOMContentLoaded", function () {
 window.addEventListener("load", () => {
   displayOverlay(false);
 });
+
+function getMaxIndexFromIds() {
+  const elements = document.querySelectorAll(
+    "div[id^='demande_appro_direct_form_DAL_'].DAL-container"
+  );
+  return Array.from(elements).reduce((max, el) => {
+    const match = el.id.match(/^demande_appro_direct_form_DAL_(\d+)$/);
+    if (match) {
+      const value = parseInt(match[1], 10);
+      return !isNaN(value) && value > max ? value : max;
+    }
+    return max;
+  }, 0);
+}
+
+function getMaxLineFromValues() {
+  const elements = document.querySelectorAll(
+    "[id^='demande_appro_direct_form_DAL_'][id$='_numeroLigne']"
+  );
+  return Array.from(elements).reduce((max, el) => {
+    const value = parseInt(el.value, 10);
+    if (isNaN(value)) {
+      console.warn("Valeur non numérique trouvée pour numeroLigne:", el.value);
+      return max; // ignore les valeurs invalides
+    }
+    return value > max ? value : max;
+  }, 0);
+}
+
+function buildIndexFromLines() {
+  const maxIndex = getMaxIndexFromIds();
+  const maxLine = getMaxLineFromValues();
+
+  // Log et stockage des résultats dans localStorage
+  console.log("Numéro de ligne Max:", maxLine);
+  localStorage.setItem("daDirectNumLigneMax", maxLine);
+
+  console.log("Max index:", maxIndex);
+  localStorage.setItem("daDirectLineCounter", maxIndex);
+}
 
 /**===========================================================================
  * Configuration des agences et services
