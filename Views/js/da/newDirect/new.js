63bc7a1c3568c318968faeb3b722ba1bad14dbf2
Menja <ranofimenjajam@gmail.com>
2025-11-19 10:09:11 +0300

    message de confirmation

--- Views/js/da/newDirect/new.js
+++ Views/js/da/newDirect/new.js
@@ -5,35 +5,65 @@ import { handleAgenceChange } from "../../dit/fonctionUtils/fonctionListDit.js";
 document.addEventListener("DOMContentLoaded", function () {
   buildIndexFromLines(); // initialiser le compteur de ligne pour la création d'une DA directe
 
+  const actionsConfig = {
+    enregistrerBrouillon: {
+      title: "Confirmer l’enregistrement",
+      html: `Souhaitez-vous enregistrer <strong class="text-primary">provisoirement</strong> cette demande ?<br><small class="text-primary"><strong><u>NB</u>: </strong>Elle ne sera pas transmise au service APPRO.</small>`,
+      icon: "question",
+      confirmButtonText: "Oui, Enregistrer",
+      canceledText: "L’enregistrement provisoire a été annulé.",
+    },
+    soumissionAppro: {
+      title: "Confirmer la soumission",
+      html: `Êtes-vous sûr de vouloir <strong style="color: #f8bb86;">soumettre</strong> cette demande ?<br><small style="color: #f8bb86;"><strong><u>NB</u>: </strong>Elle sera transmise au service APPRO pour traitement.</small>`,
+      icon: "warning",
+      confirmButtonText: "Oui, Soumettre",
+      canceledText: "La soumission de la demande a été annulée.",
+    },
+  };
+
   document
     .getElementById("add-child")
     .addEventListener("click", ajouterUneLigne);
 
   document.getElementById("myForm").addEventListener("submit", function (e) {
-    e.preventDefault();
+    e.preventDefault(); // empêcher l'envoi immédiat
+    const action = e.submitter.name; // 👉 nom (attribut "name") du bouton qui a déclenché le submit
+    // Définition des paramètres selon l'action
+
+    const config = actionsConfig[action];
+    if (!config) return;
 
     if (document.getElementById("children-container").childElementCount > 0) {
       Swal.fire({
-        title: "Êtes-vous sûr(e) ?",
-        html: `Voulez-vous vraiment envoyer la demande?`,
-        icon: "warning",
+        title: config.title,
+        html: config.html,
+        icon: config.icon,
         showCancelButton: true,
         reverseButtons: true,
         confirmButtonColor: "#198754",
         cancelButtonColor: "#6c757d",
-        confirmButtonText: "Oui, Envoyer",
-        cancelButtonText: "Non, annuler",
+        confirmButtonText: config.confirmButtonText,
+        cancelButtonText: "Non, Annuler",
       }).then((result) => {
         if (result.isConfirmed) {
           displayOverlay(true);
           document.getElementById("child-prototype").remove();
-          document.getElementById("myForm").submit();
+
+          // ajouter un champ caché avec l’action choisie
+          const hidden = document.createElement("input");
+          hidden.type = "hidden";
+          hidden.name = action;
+          hidden.value = "1";
+          document.getElementById("myForm").appendChild(hidden);
+
+          document.getElementById("myForm").submit(); // n’émule pas le clic sur le bouton d’envoi → donc le name et value du bouton cliqué ne sont pas envoyés.
         } else if (result.dismiss === Swal.DismissReason.cancel) {
           // ❌ Si l'utilisateur annule
           Swal.fire({
             icon: "info",
             title: "Annulé",
-            text: "Votre demande n'a pas été envoyée.",
+            text: config.canceledText,
             timer: 2000,
             showConfirmButton: false,
           });
