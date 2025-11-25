eab43c909f13b312989905f6ebe17c309b8c544e
Menja <ranofimenjajam@gmail.com>
2025-11-25 10:31:45 +0300

    création de PDF pour le bon à payer

---
+++ src/Service/genererPdf/bap/GenererPdfBonAPayer.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Service\genererPdf\bap;
+
+use App\Service\genererPdf\GeneratePdf;
+
+class GenererPdfBonAPayer extends GeneratePdf
+{
+    /**
+     * Fonction pour générer le PDF du bon à payer
+     */
+    public function genererPDF($data) {}
+}
