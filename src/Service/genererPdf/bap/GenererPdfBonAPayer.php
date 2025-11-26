27dfcde2ae8a581ed2498fcf4c0eff4b3fb6f384
Menja <ranofimenjajam@gmail.com>
2025-11-26 16:04:18 +0300

    génération PDF: info BC + validation BC - étape 4

--- src/Service/genererPdf/bap/GenererPdfBonAPayer.php
+++ src/Service/genererPdf/bap/GenererPdfBonAPayer.php
@@ -2,12 +2,15 @@
 
 namespace App\Service\genererPdf\bap;
 
+use App\Controller\Traits\FormatageTrait;
 use TCPDF;
 use App\Entity\da\DemandeAppro;
 use App\Service\genererPdf\GeneratePdf;
 
 class GenererPdfBonAPayer extends GeneratePdf
 {
+    use FormatageTrait;
+
     /**
      * Fonction pour générer le PDF du bon à payer
      */
@@ -48,82 +51,37 @@ class GenererPdfBonAPayer extends GeneratePdf
         $w100 = $this->getUsableWidth($pdf);
         $w50  = $w100 / 2;
         $pdf->setFont('helvetica', 'B', 9);
-        $pdf->Cell($w50, 5, 'INFORMATION DU BC', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->Cell($w50, 5, 'INFORMATION VALIDATION BC', 0, 1, '', false, '', 0, false, 'T', 'M');
+        $this->cell($pdf, $w50, 5, 'INFORMATION DU BC', 0);
+        $this->cell($pdf, $w50, 5, 'INFORMATION VALIDATION BC', 1);
         $pdf->Ln(3);
 
         $pdf->setFont('helvetica', '', 9);
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Fournisseur', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . $infoBC["nom_fournisseur"], 0, 0, '', false, '', 0, false, 'T', 'M');
 
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Nom Validateur', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . $infoValidationBC["validateur"], 0, 1, '', false, '', 0, false, 'T', 'M');
+        // Infos principales
+        $this->printInfo($pdf, 'Fournisseur', $infoBC["nom_fournisseur"], $w50, 0);
+        $this->printInfo($pdf, 'Nom Validateur', $infoValidationBC["validateur"] ?? "-", $w50);
 
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'N° FRN', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . $infoBC["num_fournisseur"], 0, 0, '', false, '', 0, false, 'T', 'M');
-
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Date Validation', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . $infoValidationBC["dateValidation"]->format("d/m/Y"), 0, 1, '', false, '', 0, false, 'T', 'M');
+        $this->printInfo($pdf, 'N° FRN', $infoBC["num_fournisseur"], $w50, 0);
+        $this->printInfo($pdf, 'Date Validation', $infoValidationBC["dateValidation"] ? $infoValidationBC["dateValidation"]->format("d/m/Y") : "-", $w50);
         $pdf->Ln(3);
 
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Téléphone', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . $infoBC["tel_fournisseur"], 0, 1, '', false, '', 0, false, 'T', 'M');
+        $this->printInfo($pdf, 'Téléphone', $infoBC["tel_fournisseur"], $w50);
 
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Adresse FRN', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": ", 0, 1, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(15, 5, '', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w100 - 15, 5, $infoBC["adr1_fournisseur"], 0, 1, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(15, 5, '', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w100 - 15, 5, $infoBC["adr2_fournisseur"], 0, 1, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(15, 5, '', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w100 - 15, 5, $infoBC["ptt_fournisseur"] . " " . $infoBC["adr4_fournisseur"], 0, 1, '', false, '', 0, false, 'T', 'M');
+        // Adresse
+        $this->printAdresse($pdf, $infoBC, $w100);
 
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'N°', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . $infoBC["num_cde"], 0, 1, '', false, '', 0, false, 'T', 'M');
-
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Date', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . date("d/m/Y", strtotime($infoBC["date_cde"])), 0, 1, '', false, '', 0, false, 'T', 'M');
+        $this->printInfo($pdf, 'N°', $infoBC["num_cde"] ?? "-", $w50);
+        $this->printInfo($pdf, 'Date', $infoBC["date_cde"] ? date("d/m/Y", strtotime($infoBC["date_cde"])) : "-", $w50);
         $pdf->Ln(3);
 
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Succursale', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . $infoBC["succ_cde"], 0, 1, '', false, '', 0, false, 'T', 'M');
-
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Service', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . $infoBC["serv_cde"], 0, 1, '', false, '', 0, false, 'T', 'M');
-
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Opérateur', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . $infoBC["nom_ope"], 0, 1, '', false, '', 0, false, 'T', 'M');
-
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'N° cmd externe', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . $infoBC["num_cde_ext"], 0, 1, '', false, '', 0, false, 'T', 'M');
-
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Référence', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . $infoBC["libelle_cde"], 0, 1, '', false, '', 0, false, 'T', 'M');
-
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Montant HT', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . number_format((float) $infoBC["mtn_cde"], 2, ',', ' '), 0, 1, '', false, '', 0, false, 'T', 'M');
-
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Montant TTC', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . number_format((float) $infoBC["ttc_cde"], 2, ',', ' '), 0, 1, '', false, '', 0, false, 'T', 'M');
-
-        $pdf->cell(6, 5, ' -', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell(30, 5, 'Nature de l’achat', 0, 0, '', false, '', 0, false, 'T', 'M');
-        $pdf->cell($w50 - 36, 5, ": " . $infoBC["type_cde"], 0, 1, '', false, '', 0, false, 'T', 'M');
+        $this->printInfo($pdf, 'Succursale', $infoBC["succ_cde"] ?? "-", $w50);
+        $this->printInfo($pdf, 'Service', $infoBC["serv_cde"] ?? "-", $w50);
+        $this->printInfo($pdf, 'Opérateur', $infoBC["nom_ope"] ?? "-", $w50);
+        $this->printInfo($pdf, 'N° cmd externe', $infoBC["num_cde_ext"] ?? "-", $w50);
+        $this->printInfo($pdf, 'Référence', $infoBC["libelle_cde"] ?? "-", $w50);
+        $this->printInfo($pdf, 'Montant HT', $this->formaterPrix($infoBC["mtn_cde"]), $w50);
+        $this->printInfo($pdf, 'Montant TTC', $this->formaterPrix($infoBC["ttc_cde"]), $w50);
+        $this->printInfo($pdf, 'Nature de l’achat', $infoBC["type_cde"] ?? "-", $w50);
     }
 
     private function savePDF(TCPDF $pdf, string $numDa, string $dest = "F"): string
@@ -148,5 +106,34 @@ class GenererPdfBonAPayer extends GeneratePdf
         $w_total = $pdf->GetPageWidth();  // Largeur totale du PDF
         $margins = $pdf->GetMargins();    // Tableau des marges (left, top, right)
         return $w_total - $margins['left'] - $margins['right'];
+    }
+
+    private function cell(TCPDF $pdf, $w, $h, $txt, $ln)
+    {
+        $pdf->Cell($w, $h, $txt, 0, $ln, 'L', false, '', 0, false, 'T', 'M');
+    }
+
+    private function printInfo(TCPDF $pdf, $label, $value, $w50, $endLine = 1)
+    {
+        $this->cell($pdf, 6, 5, ' -', 0);
+        $this->cell($pdf, 30, 5, $label, 0);
+        $this->cell($pdf, $w50 - 36, 5, ": " . $value, $endLine);
+    }
+
+    private function printAdresse(TCPDF $pdf, $infoBC, $w100)
+    {
+        $this->cell($pdf, 6, 5, ' -', 0);
+        $this->cell($pdf, 30, 5, 'Adresse FRN', 0);
+        $this->cell($pdf, $w100 - 36, 5, ": ", 1);
+
+        // Adresses multi-lignes
+        $this->cell($pdf, 15, 5, '', 0);
+        $this->cell($pdf, $w100 - 15, 5, $infoBC["adr1_fournisseur"], 1);
+
+        $this->cell($pdf, 15, 5, '', 0);
+        $this->cell($pdf, $w100 - 15, 5, $infoBC["adr2_fournisseur"], 1);
+
+        $this->cell($pdf, 15, 5, '', 0);
+        $this->cell($pdf, $w100 - 15, 5, $infoBC["ptt_fournisseur"] . " " . $infoBC["adr4_fournisseur"], 1);
     }
 }
