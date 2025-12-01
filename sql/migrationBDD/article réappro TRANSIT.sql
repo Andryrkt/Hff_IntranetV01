c630c9fcf4a62122803f9c69b165e627c60f463d
Menja <ranofimenjajam@gmail.com>
2025-12-01 10:30:28 +0300

    article REAPPRO

---
+++ sql/migrationBDD/article réappro TRANSIT.sql
@@ -0,0 +1,4 @@
+INSERT INTO HFF_INTRANET.dbo.da_article_reappro
+(art_constp, art_refp, art_desi, qte_validee_appro, art_pu, code_agence, code_service, date_creation, date_modification)
+VALUES
+('BOI','3A0007',TRIM('BONBONNE D''EAU'),'2','37000', '80', 'FIN', '2025-12-01 00:00:00.000','2025-12-01 00:00:00.000'),
