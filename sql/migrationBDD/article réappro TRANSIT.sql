76948ed6c043a0614f2de49e8be71a12715c9453
Menja <ranofimenjajam@gmail.com>
2025-12-01 16:13:14 +0300

    supprimer méthode inutile

--- sql/migrationBDD/article réappro TRANSIT.sql
+++ sql/migrationBDD/article réappro TRANSIT.sql
@@ -20,3 +20,12 @@ VALUES
 ('FBU','1F0041',TRIM('SCOTCH GM TRANSPARENT'),'2','20900', '80', 'TRA', '2025-12-01 00:00:00.000','2025-12-01 00:00:00.000'),
 ('FBU','1F0045',TRIM('TROMBONE MM'),'2','3990', '80', 'TRA', '2025-12-01 00:00:00.000','2025-12-01 00:00:00.000'),
 ('FBU','1F0046',TRIM('TROMBONE PM'),'1','950', '80', 'TRA', '2025-12-01 00:00:00.000','2025-12-01 00:00:00.000');
+
+
+INSERT INTO HFF_INTRANET.dbo.da_article_reappro
+(art_constp, art_refp, art_desi, qte_validee_appro, art_pu, code_agence, code_service, date_creation, date_modification)
+VALUES
+('FBU','1F0016',TRIM('COLLE BATON'),'1','1900', '80', 'TRA', '2025-12-01 00:00:00.000','2025-12-01 00:00:00.000'),
+('CEN','4A0027',TRIM('GEL HYDROALCOOLIQUE'),'1','11883', '80', 'TRA', '2025-12-01 00:00:00.000','2025-12-01 00:00:00.000'),
+('CEN','4C0001',TRIM('CHAMOISINE'),'1','2470', '80', 'TRA', '2025-12-01 00:00:00.000','2025-12-01 00:00:00.000')
+;
