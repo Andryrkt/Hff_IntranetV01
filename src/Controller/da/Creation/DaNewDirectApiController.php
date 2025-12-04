70da08d9a739ffd3034da2f50222512488c2a2e5
Menja <ranofimenjajam@gmail.com>
2025-12-04 16:32:35 +0300

    création du controller pour la DA directe

--- src/Controller/da/Creation/DaNewDirectApiController.php
+++ src/Controller/da/Creation/DaNewDirectApiController.php
@@ -3,7 +3,8 @@
 namespace App\Controller\da\Creation;
 
 use App\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use App\Entity\da\DaArticleReappro;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -11,8 +12,42 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class DaNewDirectApiController extends Controller
 {
+    private const ERROR_MESSAGES = [
+        'codeAgenceServiceManquant' => 'codeAgence ou codeService manquant',
+        'codeAgenceIncorrect'       => 'codeAgence doit contenir exactement 2 caractères',
+        'codeServiceIncorrect'      => 'codeService doit contenir exactement 3 caractères',
+    ];
+
     /**
-     * @Route("/new-direct", name="api_da_new_direct", methods={"POST"})
+     * @Route("/da-new-direct/{codeAgence}/{codeService}", name="api_da_new_direct", methods={"GET"})
      */
-    public function newDirect(Request $request) {}
+    public function listeArticle(string $codeAgence, string $codeService)
+    {
+        if (!$codeAgence || !$codeService) return $this->errorMessage(self::ERROR_MESSAGES['codeAgenceServiceManquant']);
+        if (strlen($codeAgence) !== 2) return $this->errorMessage(self::ERROR_MESSAGES['codeAgenceIncorrect']);
+        if (strlen($codeService) !== 3) return $this->errorMessage(self::ERROR_MESSAGES['codeServiceIncorrect']);
+
+        try {
+            $daArticleReapproRepository = $this->getEntityManager()->getRepository(DaArticleReappro::class);
+            $articlesReappro = $daArticleReapproRepository->getArticlesList($codeAgence, $codeService);
+
+            return new JsonResponse([
+                'status'  => 'success',
+                'title'   => 'Succès de l\'opération',
+                'message' => "La récupération des articles est un succès.",
+                'data'    => array_combine($articlesReappro, $articlesReappro),
+            ]);
+        } catch (\Exception $e) {
+            return $this->errorMessage($e->getMessage());
+        }
+    }
+
+    private function errorMessage(string $errorMessage): JsonResponse
+    {
+        return new JsonResponse([
+            'status'  => 'error',
+            'title'   => 'Erreur lors de la récupération des articles',
+            'message' => "Impossible de récupérer les articles: $errorMessage. Merci de vérifier les informations et de réessayer.",
+        ], 400);
+    }
 }
