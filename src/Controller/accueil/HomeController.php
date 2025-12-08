46796daf423cf0f8164e79e39d0f77d3072f6792
Menja <ranofimenjajam@gmail.com>
2025-12-08 10:56:39 +0300

    suppression de perfLogger

--- src/Controller/accueil/HomeController.php
+++ src/Controller/accueil/HomeController.php
@@ -29,38 +29,27 @@ class HomeController extends Controller
         $appsByCode = [];
         $user = null;
 
-        $perfLogger = PerfLogger::getInstance();
-        $perfLogger->log('Debut de la page d\'accueil', 'HomeController');
 
         // Vérifier si l'utilisateur est connecté
         if ($this->isUserConnected()) {
-            $perfLogger->log("if (\$this->isUserConnected()) {", 'HomeController');
             try {
                 // Utiliser le MenuService pour récupérer le menu
                 $menuService = $this->getMenuService();
-                $perfLogger->log("\$menuService = \$this->getMenuService();", 'HomeController');
                 $menu = $menuService->getMenuStructure();
-                $perfLogger->log("\$menu = \$menuService->getMenuStructure();", 'HomeController');
                 $user = $this->getUser();
-                $perfLogger->log("\$user = \$this->getUser();", 'HomeController');
             } catch (Exception $e) {
-                $perfLogger->log("catch (Exception \$e)", 'HomeController');
                 // En cas d'erreur, on continue sans menu
                 error_log("Erreur MenuService: " . $e->getMessage());
             }
-            $perfLogger->log("fin du try", 'HomeController');
         } else {
-            $perfLogger->log("else", 'HomeController');
             // Si l'utilisateur n'est pas connecté, on utilise le menu par défaut
             $this->redirectToRoute('security_signin');
         }
 
         foreach ($user->getApplications() as $application) {
-            $perfLogger->log("foreach (\$user->getApplications() as \$application)", 'HomeController');
             $appsByCode[$application->getCodeApp()] = true;
         }
 
-        $perfLogger->log("fin de boucle", 'HomeController');
 
         return $this->render('main/accueil.html.twig', [
             'menuItems'  => $menu,
