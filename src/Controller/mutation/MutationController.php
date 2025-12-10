30f4d6a3316adb7fac77829b63f53a96e12e2026
Menja <ranofimenjajam@gmail.com>
2025-12-10 17:02:38 +0300

    refactor sur mutation

--- src/Controller/mutation/MutationController.php
+++ src/Controller/mutation/MutationController.php
@@ -48,10 +48,6 @@ class MutationController extends Controller
         $this->autorisationAcces($this->getUser(), Application::ID_MUT);
         /** FIN AUtorisation acées */
 
-        //recuperation de l'utilisateur connecter
-        $userId = $this->getSessionService()->get('user_id');
-        $user = $this->getEntityManager()->getRepository(User::class)->find($userId);
-
         $mutation = new Mutation;
         $this->initialisationMutation($mutation, $this->getEntityManager());
 
@@ -73,9 +69,9 @@ class MutationController extends Controller
             } else if ((int) $mutationModel->getNombreDM($dateDebut, $dateFin, $matricule) > 0) {
                 $this->historiqueOperation->sendNotificationCreation("La demande de mutation a échoué car le matricule '$matricule' est déjà rattaché à une demande de mutation entre les plages de dates.", '-', 'mutation_liste', false);
             } else {
-                $mutation = $this->enregistrementValeurDansMutation($form, $this->getEntityManager(), $user);
+                $mutation = $this->enregistrementValeurDansMutation($form, $this->getEntityManager());
                 $generatePdf = new GeneratePdfMutation;
-                $generatePdf->genererPDF($this->donneePourPdf($form, $user));
+                $generatePdf->genererPDF($this->donneePourPdf($form));
                 $this->envoyerPieceJointes($form, $this->fusionPdf);
                 $generatePdf->copyInterneToDOCUWARE($mutation->getNumeroMutation(), $mutation->getAgenceEmetteur()->getCodeAgence() . $mutation->getServiceEmetteur()->getCodeService());
                 $this->historiqueOperation->sendNotificationCreation('La demande de mutation a été enregistrée avec succès', $mutation->getNumeroMutation(), 'mutation_liste', true);
