20622dec2feafe2dedec41584d74e95918234581
Menja <ranofimenjajam@gmail.com>
2025-12-11 10:14:46 +0300

    modif de la méthode sur la page d'accueil

--- src/Controller/accueil/HomeController.php
+++ src/Controller/accueil/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\accueil;
 
 use App\Controller\Controller;
+use App\Entity\admin\Application;
 use App\Service\navigation\MenuService;
 use Symfony\Component\Routing\Annotation\Route;
 use Exception;
@@ -25,9 +26,6 @@ class HomeController extends Controller
     public function showPageAcceuil()
     {
         $menu = [];
-        $appsByCode = [];
-        $user = null;
-
 
         // Vérifier si l'utilisateur est connecté
         if ($this->isUserConnected()) {
@@ -35,7 +33,6 @@ class HomeController extends Controller
                 // Utiliser le MenuService pour récupérer le menu
                 $menuService = $this->getMenuService();
                 $menu = $menuService->getMenuStructure();
-                $user = $this->getUser();
             } catch (Exception $e) {
                 // En cas d'erreur, on continue sans menu
                 error_log("Erreur MenuService: " . $e->getMessage());
@@ -45,14 +42,11 @@ class HomeController extends Controller
             $this->redirectToRoute('security_signin');
         }
 
-        foreach ($user->getApplications() as $application) {
-            $appsByCode[$application->getCodeApp()] = true;
-        }
-
+        $userInfo = $this->getSessionService()->get('user_info');
 
         return $this->render('main/accueil.html.twig', [
-            'menuItems'  => $menu,
-            'appsByCode' => $appsByCode,
+            'menuItems' => $menu,
+            'hasDAP'    => in_array(Application::ID_DAP, $userInfo["applications"] ?? []),
         ]);
     }
 }
