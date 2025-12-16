bea36f39ab7801c4046b512fc2f96e2ee6f86d90
lanto hasina <andryrkt08@gmail.com>
2025-12-16 08:02:39 +0300

    correction rapide

--- src/Model/magasin/devis/ListeDevisMagasinModel.php
+++ src/Model/magasin/devis/ListeDevisMagasinModel.php
@@ -29,7 +29,7 @@ class ListeDevisMagasinModel extends Model
             AND nent_servcrt <> 'ASS'
             AND (CAST(nent_numcli AS VARCHAR(20)) NOT LIKE '199%' and nent_numcli not in ('1101222', '1990000'))
             AND nent_numcde not in ($numDeviAExclureString)
-            AND nent_datecde >= '01-09-2025'
+            AND nent_datecde >= MDY(9, 1, 2025)
             AND year(Nent_datecde) = year(TODAY)
         ";
 
