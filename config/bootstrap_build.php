710579a4cd53b0469078a7227ee33dfe04e1e997
Menja <ranofimenjajam@gmail.com>
2025-12-17 15:20:45 +0300

    ajout de bootstrap_build et bootstrap_runtime

--- config/bootstrap_build.php
+++ config/bootstrap_build.php
@@ -1,194 +1,53 @@
 <?php
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use App\Doctrine\EntityManagerFactory;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\Config\ConfigCache;
-use Symfony\Component\Config\Resource\FileResource;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\RouteCollection;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\Loader\AnnotationDirectoryLoader;
-use App\Loader\CustomAnnotationClassLoader;
-use Doctrine\Common\Annotations\AnnotationReader;
-use Twig\Environment;
-use Twig\Loader\FilesystemLoader;
-use Twig\Extension\DebugExtension;
-use Symfony\Bridge\Twig\Extension\TranslationExtension;
-use Symfony\Bridge\Twig\Extension\RoutingExtension;
-use Symfony\Bridge\Twig\Extension\FormExtension;
-use Symfony\Bridge\Twig\Extension\AssetExtension;
-use Symfony\Component\Asset\Packages;
-use Symfony\Component\Asset\PathPackage;
-use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
-use Symfony\Component\Form\FormRenderer;
-use Symfony\Bridge\Twig\Form\TwigRendererEngine;
-use Twig\RuntimeLoader\FactoryRuntimeLoader;
-use Illuminate\Pagination\Paginator;
-use Symfony\Component\HttpKernel\Controller\ContainerControllerResolver;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Component\HttpFoundation\RequestStack;
-use Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage;
+use Symfony\Component\DependencyInjection\Definition;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-// Autoload et variables d'environnement
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__) . '/.env')) {
-    $dotenv = \Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-    $dotenv->load();
-}
-
-require_once __DIR__ . '/listeConstructeur.php';
-
-// Variables pour CLI
-$_ENV['BASE_PATH_COURT'] ??= '/Hffintranet';
-$_SERVER['HTTP_HOST'] ??= 'localhost';
-$_SERVER['REQUEST_URI'] ??= '/';
-
-// Répertoire cache
+// --- Cache ---
 $cacheDir = dirname(__DIR__) . '/var/cache';
 @mkdir($cacheDir, 0777, true);
 
-// --- Chargement du container depuis cache ou build ---
-$containerFile = $cacheDir . '/container.bin';
-if (!file_exists($containerFile)) {
+// --- Container ---
+$container = new ContainerBuilder();
+$container->setParameter('kernel.project_dir', dirname(__DIR__));
+$container->setParameter('kernel.cache_dir', $cacheDir);
+$container->setParameter('kernel.debug', false);
 
-    $container = new ContainerBuilder();
-    $container->setParameter('kernel.project_dir', dirname(__DIR__));
-    $container->setParameter('kernel.cache_dir', $cacheDir);
-    $container->setParameter('kernel.debug', true);
+// EntityManager
+$entityManagerDef = new Definition(\Doctrine\ORM\EntityManager::class);
+$entityManagerDef->setFactory([EntityManagerFactory::class, 'createEntityManager']);
+$entityManagerDef->setPublic(true);
+$container->setDefinition('doctrine.orm.default_entity_manager', $entityManagerDef);
 
-    // Doctrine
-    $entityManager = require dirname(__DIR__) . '/doctrineBootstrap.php';
-    $container->set('doctrine.orm.default_entity_manager', $entityManager);
+// ManagerRegistry (si tu utilises ton SimpleManagerRegistry)
+$registryDef = new Definition(\core\SimpleManagerRegistry::class, [
+    $container->getDefinition('doctrine.orm.default_entity_manager')
+]);
+$registryDef->setPublic(true);
+$container->setDefinition('doctrine', $registryDef);
 
-    // Services de base
-    $container->set('request_stack', new RequestStack());
-    $container->set('session', new Session(new NativeSessionStorage()));
+// --- Services simples compilables ---
+$requestStackDef = new Definition(RequestStack::class);
+$requestStackDef->setPublic(true);
+$container->setDefinition('request_stack', $requestStackDef);
 
-    // Charger services.yaml et parameters.yaml
-    $loader = new YamlFileLoader($container, new FileLocator(__DIR__));
-    $loader->load('services.yaml');
-    $loader->load('parameters.yaml');
+$loader = new YamlFileLoader($container, new FileLocator(__DIR__));
+$loader->load('services.yaml');
+$loader->load('parameters.yaml');
 
-    // Twig
-    $twig = new Environment(
-        new FilesystemLoader([
-            dirname(__DIR__) . '/Views/templates',
-            dirname(__DIR__) . '/vendor/symfony/twig-bridge/Resources/views/Form',
-        ]),
-        ['debug' => true, 'cache' => $cacheDir . '/twig']
-    );
-    $container->set('twig', $twig);
+// Pagination
+\Illuminate\Pagination\Paginator::useBootstrap();
 
-    // --- Services Twig et Navigation ---
-    $session = $container->get('session');
-    $requestStack = $container->get('request_stack');
-
-    $menuService = new \App\Service\navigation\MenuService($session);
-    $breadcrumbService = new \App\Service\navigation\BreadcrumbMenuService($menuService);
-
-    $twig->addExtension(new DebugExtension());
-    $twig->addExtension(new TranslationExtension(new \Symfony\Component\Translation\Translator('fr_FR')));
-    $twig->addExtension(new RoutingExtension(new \Symfony\Component\Routing\Generator\UrlGenerator(new RouteCollection(), new RequestContext())));
-    $twig->addExtension(new FormExtension());
-    $twig->addExtension(new \App\Twig\AppExtension($session, $requestStack));
-    $twig->addExtension(new \App\Twig\BreadcrumbExtension($breadcrumbService));
-    $twig->addExtension(new \App\Twig\CarbonExtension());
-    $twig->addExtension(new \App\Twig\DeleteWordExtension());
-
-    // Asset
-    $publicPath = $_ENV['BASE_PATH_COURT'] . '/public';
-    $packages = new Packages(new PathPackage($publicPath, new EmptyVersionStrategy()));
-    $twig->addExtension(new AssetExtension($packages));
-
-    // FormRendererEngine
-    $defaultFormTheme = 'bootstrap_5_layout.html.twig';
-    $formEngine = new TwigRendererEngine([$defaultFormTheme], $twig);
-    $twig->addRuntimeLoader(new FactoryRuntimeLoader([
-        FormRenderer::class => fn() => new FormRenderer($formEngine),
-    ]));
-
-    // FormFactory
-    $formFactory = \Symfony\Component\Form\Forms::createFormFactoryBuilder()
-        ->addExtension(new \Symfony\Component\Form\Extension\Core\CoreExtension())
-        ->addExtension(new \Symfony\Component\Form\Extension\Validator\ValidatorExtension(\Symfony\Component\Validator\Validation::createValidator()))
-        ->addExtension(new \Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension())
-        ->addExtension(new \Symfony\Bridge\Doctrine\Form\DoctrineOrmExtension($container->get('doctrine')))
-        ->getFormFactory();
-    $container->set('form.factory', $formFactory);
-
-    // Pagination
-    Paginator::useBootstrap();
-
-    // Routes via Annotation + cache
-    $routeCacheFile = $cacheDir . '/routes.bin';
-    $routeCache = new ConfigCache($routeCacheFile, false);
-
-    if (!$routeCache->isFresh()) {
-        $collection = new RouteCollection();
-        $reader = new AnnotationReader();
-        foreach ([dirname(__DIR__) . '/src/Controller', dirname(__DIR__) . '/src/Api'] as $dir) {
-            if (!is_dir($dir)) continue;
-
-            $loader = new AnnotationDirectoryLoader(
-                new FileLocator($dir),
-                new CustomAnnotationClassLoader($reader)
-            );
-            $sub = $loader->load($dir);
-            $collection->addCollection($sub);
-
-            foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir)) as $file) {
-                if ($file->isFile() && $file->getExtension() === 'php') {
-                    $collection->addResource(new FileResource($file->getPathname()));
-                }
-            }
-        }
-
-        $routeCache->write(serialize($collection), $collection->getResources());
-    } else {
-        $collection = unserialize(file_get_contents($routeCacheFile));
-    }
-
-    foreach ($collection as $route) {
-        $route->setOption('case_sensitive', false);
-    }
-    $container->set('route_collection', $collection);
-
-    // Compiler container
-    $container->compile();
-
-    // Sauvegarder container
-    file_put_contents($containerFile, serialize($container));
-} else {
-    $container = unserialize(file_get_contents($containerFile));
-}
-
-// --- Correction préfixe /Hffintranet/
-$request = Request::createFromGlobals();
-$pathInfo = $request->getPathInfo();
-if (stripos($pathInfo, '/hffintranet') === 0 && strpos($pathInfo, '/Hffintranet') !== 0) {
-    $correctUrl = preg_replace('#^/hffintranet#i', '/Hffintranet', $pathInfo);
-    (new \Symfony\Component\HttpFoundation\RedirectResponse($correctUrl, 301))->send();
-    exit;
-}
-$container->get('request_stack')->push($request);
-
-// Context + matcher + resolvers
-$context = new RequestContext();
-$context->fromRequest($request);
-$matcher = new UrlMatcher($container->get('route_collection'), $context);
-
-$controllerResolver = new ContainerControllerResolver($container);
-$argumentResolver = new ArgumentResolver();
-
-global $container;
-
-return [
-    'twig'               => $container->get('twig'),
-    'matcher'            => $matcher,
-    'argumentResolver'   => $argumentResolver,
-    'controllerResolver' => $controllerResolver,
-];
+// --- Compiler et dump PHP natif ---
+$container->compile();
+$dumper = new PhpDumper($container);
+file_put_contents($cacheDir . '/Container.php', $dumper->dump([
+    'class' => 'AppContainer'
+]));
---
+++ config/bootstrap_runtime.php
@@ -0,0 +1,146 @@
+<?php
+
+use Symfony\Component\Config\ConfigCache;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\HttpKernel\Controller\ContainerControllerResolver;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+require dirname(__DIR__) . '/var/cache/Container.php';
+
+/** @var \Symfony\Component\DependencyInjection\ContainerInterface $container */
+$container = new AppContainer();
+
+// Session runtime
+$session = new \Symfony\Component\HttpFoundation\Session\Session(
+    new \Symfony\Component\HttpFoundation\Session\Storage\NativeSessionStorage()
+);
+$container->set('session', $session);
+
+// Twig runtime
+$twig = new \Twig\Environment(
+    new \Twig\Loader\FilesystemLoader([
+        dirname(__DIR__) . '/Views/templates',
+        dirname(__DIR__) . '/vendor/symfony/twig-bridge/Resources/views/Form',
+    ]),
+    ['debug' => false, 'cache' => dirname(__DIR__) . '/var/cache/twig']
+);
+$container->set('twig', $twig);
+
+$formFactory = \Symfony\Component\Form\Forms::createFormFactoryBuilder()
+    ->addExtension(new \Symfony\Component\Form\Extension\Core\CoreExtension())
+    ->addExtension(new \Symfony\Component\Form\Extension\Validator\ValidatorExtension(\Symfony\Component\Validator\Validation::createValidator()))
+    ->addExtension(new \Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension())
+    ->addExtension(new \Symfony\Bridge\Doctrine\Form\DoctrineOrmExtension($container->get('doctrine')))
+    ->addExtension(new \Symfony\Component\Form\Extension\DependencyInjection\DependencyInjectionExtension($container, [], []))
+    ->getFormFactory();
+
+$container->set('form.factory', $formFactory);
+
+// --- Variables d'environnement & CLI ---
+if (file_exists(dirname(__DIR__) . '/.env')) {
+    \Dotenv\Dotenv::createImmutable(dirname(__DIR__))->load();
+}
+require_once __DIR__ . '/listeConstructeur.php';
+$_ENV['BASE_PATH_COURT'] ??= '/Hffintranet';
+$_SERVER['HTTP_HOST'] ??= 'localhost';
+$_SERVER['REQUEST_URI'] ??= '/';
+
+// --- Request / Session ---
+$request = Request::createFromGlobals();
+$container->get('request_stack')->push($request);
+$session = $container->get('session');
+
+// --- Routes cacheées ---
+$routeCacheFile = dirname(__DIR__) . '/var/cache/routes.php';
+$cacheRoutes = new ConfigCache($routeCacheFile, false); // TODO Mode debug : true => vérifie si les fichiers ont changé
+
+if (!$cacheRoutes->isFresh()) {
+    $collection = new \Symfony\Component\Routing\RouteCollection();
+    $reader = new \Doctrine\Common\Annotations\AnnotationReader();
+    foreach ([dirname(__DIR__) . '/src/Controller', dirname(__DIR__) . '/src/Api'] as $dir) {
+        if (!is_dir($dir)) continue;
+        $loaderAnnotation = new \Symfony\Component\Routing\Loader\AnnotationDirectoryLoader(
+            new \Symfony\Component\Config\FileLocator($dir),
+            new \App\Loader\CustomAnnotationClassLoader($reader)
+        );
+        $sub = $loaderAnnotation->load($dir);
+        $collection->addCollection($sub);
+
+        foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir)) as $file) {
+            if ($file->isFile() && $file->getExtension() === 'php') {
+                $collection->addResource(new \Symfony\Component\Config\Resource\FileResource($file->getPathname()));
+            }
+        }
+    }
+
+    foreach ($collection as $route) {
+        $route->setOption('case_sensitive', false);
+    }
+
+    // Écriture du cache avec toutes les ressources
+    $cacheRoutes->write(serialize($collection), $collection->getResources());
+} else {
+    // Charger la collection depuis le cache
+    $collection = unserialize(file_get_contents($routeCacheFile));
+}
+
+// --- UrlGenerator / Matcher ---
+$context = new RequestContext();
+$context->fromRequest($request);
+$matcher = new UrlMatcher($collection, $context);
+$urlGenerator = new \Symfony\Component\Routing\Generator\UrlGenerator($collection, $context);
+$container->set('router', $urlGenerator);
+
+// --- Twig extensions runtime (Menuservice) ---
+$menuService = new \App\Service\navigation\MenuService($session);
+$container->set('menuService', $menuService);
+
+// --- Twig extensions runtime ---
+$twig = $container->get('twig');
+$twig->addExtension(new \Twig\Extension\DebugExtension());
+$twig->addExtension(new \Symfony\Bridge\Twig\Extension\TranslationExtension(new \Symfony\Component\Translation\Translator('fr_FR')));
+$twig->addExtension(new \Symfony\Bridge\Twig\Extension\RoutingExtension($urlGenerator));
+$twig->addExtension(new \Symfony\Bridge\Twig\Extension\FormExtension());
+$twig->addExtension(new \App\Twig\AppExtension($session, $container->get('request_stack')));
+$twig->addExtension(new \App\Twig\BreadcrumbExtension(new \App\Service\navigation\BreadcrumbMenuService($menuService)));
+$twig->addExtension(new \App\Twig\CarbonExtension());
+$twig->addExtension(new \App\Twig\DeleteWordExtension());
+
+// --- Asset ---
+$publicPath = $_ENV['BASE_PATH_COURT'] . '/public';
+$packages = new \Symfony\Component\Asset\Packages(
+    new \Symfony\Component\Asset\PathPackage($publicPath, new \Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy())
+);
+$twig->addExtension(new \Symfony\Bridge\Twig\Extension\AssetExtension($packages));
+
+// --- FormRendererEngine ---
+$defaultFormTheme = 'bootstrap_5_layout.html.twig';
+$formEngine = new \Symfony\Bridge\Twig\Form\TwigRendererEngine([$defaultFormTheme], $twig);
+$twig->addRuntimeLoader(new \Twig\RuntimeLoader\FactoryRuntimeLoader([
+    \Symfony\Component\Form\FormRenderer::class => fn() => new \Symfony\Component\Form\FormRenderer($formEngine),
+]));
+
+// --- Correction /Hffintranet/
+$pathInfo = $request->getPathInfo();
+if (stripos($pathInfo, '/hffintranet') === 0 && strpos($pathInfo, '/Hffintranet') !== 0) {
+    $correctUrl = preg_replace('#^/hffintranet#i', '/Hffintranet', $pathInfo);
+    (new \Symfony\Component\HttpFoundation\RedirectResponse($correctUrl, 301))->send();
+    exit;
+}
+
+// --- Controllers / Resolvers ---
+$controllerResolver = new ContainerControllerResolver($container);
+$argumentResolver = new ArgumentResolver();
+
+global $container;
+
+return [
+    'twig'               => $twig,
+    'matcher'            => $matcher,
+    'controllerResolver' => $controllerResolver,
+    'argumentResolver'   => $argumentResolver,
+];
