b8f4ab32c4a69769cb7be5d461e07004778a6000
Menja <ranofimenjajam@gmail.com>
2025-12-29 14:01:13 +0300

    interface application OK

---
+++ Views/js/admin/application/application.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("#application_pages").select2({
+    placeholder: "-- Choisir page(s) associée(s) --",
+    allowClear: true,
+    theme: "bootstrap",
+  });
+});
--- src/Form/admin/ApplicationType.php
+++ src/Form/admin/ApplicationType.php
@@ -3,6 +3,8 @@
 namespace App\Form\admin;
 
 use App\Entity\admin\Application;
+use App\Entity\admin\historisation\pageConsultation\PageHff;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -18,16 +20,24 @@ class ApplicationType extends AbstractType
                 'nom',
                 TextType::class,
                 [
-                    'label' => 'nom',
+                    'label' => 'Nom de l\'application',
                 ]
             )
             ->add(
                 'codeApp',
                 TextType::class,
                 [
-                    'label' => 'Code Application'
+                    'label' => 'Code de l\'application'
                 ]
             )
+            ->add('pages', EntityType::class, [
+                'label'    => 'Pages associées',
+                'class'    => PageHff::class,
+                'choice_label' => 'nom', // ou un autre champ de PageHff que tu veux afficher
+                'multiple' => true,       // permet de choisir plusieurs pages
+                'expanded' => false,      // false = select multiple, true = checkboxes
+                'required' => false,
+            ])
         ;
     }
 
