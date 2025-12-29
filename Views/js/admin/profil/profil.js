6db235f7fb93a7f5a8d30c7f6c51736754ea2d09
Menja <ranofimenjajam@gmail.com>
2025-12-29 16:26:42 +0300

    ajout de interface pour la création et modification de profil

---
+++ Views/js/admin/profil/profil.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("#profil_applications").select2({
+    placeholder: "-- Choisir application(s) autorisée(s) --",
+    allowClear: true,
+    theme: "bootstrap",
+  });
+});
