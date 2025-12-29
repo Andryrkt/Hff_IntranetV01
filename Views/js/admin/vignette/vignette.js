2c42348c7198effb7fc79deb39ad420002bd3216
Menja <ranofimenjajam@gmail.com>
2025-12-29 15:08:49 +0300

    ajout de vignetteType et vignette js

---
+++ Views/js/admin/vignette/vignette.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+  $("#vignette_applications").select2({
+    placeholder: "-- Choisir application(s) associée(s) --",
+    allowClear: true,
+    theme: "bootstrap",
+  });
+});
