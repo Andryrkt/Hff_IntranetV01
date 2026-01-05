f8ae1f01abec2d229d22e64b64a28d199190972d
Menja <ranofimenjajam@gmail.com>
2026-01-05 13:31:14 +0300

    formulaire de recherche sur la liste de Utilisateurs

--- Views/js/admin/utilisateur/listUtilisateur.js
+++ Views/js/admin/utilisateur/listUtilisateur.js
@@ -1,11 +1,19 @@
 /** RECHERCHE */
+document.addEventListener("DOMContentLoaded", function () {
+  const searchInputs = document.querySelectorAll(".user-search-input");
 
-document.getElementById("searchInput").addEventListener("keyup", function () {
-  let filter = this.value.toLowerCase();
-  let rows = document.querySelectorAll("#tableBody tr");
+  searchInputs.forEach((input) => {
+    let label = input.dataset.label;
+    input.addEventListener("keyup", () => {
+      let filter = input.value.toLowerCase();
+      let rows = document.querySelectorAll("#tableBody tr:not(.d-none)");
 
-  rows.forEach(function (row) {
-    let text = row.textContent.toLowerCase();
-    row.style.display = text.includes(filter) ? "" : "none";
+      rows.forEach((row) => {
+        let text = row
+          .querySelector(`td[data-label="${label}"]`)
+          .textContent.toLowerCase();
+        row.classList.toggle("d-none", !text.includes(filter));
+      });
+    });
   });
 });
