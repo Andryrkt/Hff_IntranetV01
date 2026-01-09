ec6593abf1d1f1d6fbef820154ce0e5c5851cece
Menja <ranofimenjajam@gmail.com>
2026-01-09 15:33:46 +0300

    déplacement choix de DA

--- src/Controller/da/Creation/DaNewAvecDitController.php
+++ src/Controller/da/Creation/DaNewAvecDitController.php
@@ -35,26 +35,6 @@ class DaNewAvecDitController extends Controller
     }
 
     /**
-     * @Route("/da-first-form", name="da_first_form")
-     */
-    public function firstForm()
-    {
-        //verification si user connecter
-        $this->verifierSessionUtilisateur();
-
-        return $this->render('da/first-form.html.twig', [
-            'estAte'                 => $this->estUserDansServiceAtelier(),
-            'estCreateurDeDADirecte' => $this->estCreateurDeDADirecte(),
-            'urls'                   => [
-                'avecDit' => $this->getUrlGenerator()->generate('da_list_dit'),
-                'direct'  => $this->getUrlGenerator()->generate('da_new_direct', ['id' => 0]),
-                'reappro' => $this->getUrlGenerator()->generate('da_new_reappro', ['id' => 0]),
-            ],
-            'estAdmin'               => $this->estAdmin(),
-        ]);
-    }
-
-    /**
      * @Route("/new-avec-dit/{daId<\d+>}/{ditId}", name="da_new_avec_dit")
      */
     public function new(int $daId, int $ditId, Request $request)
---
+++ src/Controller/da/Creation/DaNewController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\da\Creation;
+
+use App\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+/** @Route("/demande-appro") */
+class DaNewController extends Controller
+{
+    /**
+     * @Route("/da-first-form", name="da_first_form")
+     */
+    public function firstForm()
+    {
+        //verification si user connecter
+        $this->verifierSessionUtilisateur();
+
+        return $this->render('da/first-form.html.twig', [
+            'estAte'                 => $this->estUserDansServiceAtelier(),
+            'estCreateurDeDADirecte' => $this->estCreateurDeDADirecte(),
+            'urls'                   => [
+                'avecDit' => $this->getUrlGenerator()->generate('da_list_dit'),
+                'direct'  => $this->getUrlGenerator()->generate('da_new_direct', ['id' => 0]),
+                'reappro' => $this->getUrlGenerator()->generate('da_new_reappro', ['id' => 0]),
+            ],
+            'estAdmin'               => $this->estAdmin(),
+        ]);
+    }
+}
