42d2cd37738a80f2346764276995604de4d91f2e
Menja <ranofimenjajam@gmail.com>
2026-01-09 16:36:02 +0300

    nouveau forme

--- src/Controller/da/Creation/DaNewController.php
+++ src/Controller/da/Creation/DaNewController.php
@@ -28,7 +28,7 @@ class DaNewController extends Controller
 
         if ($estAdmin || $estAtelier) {
             $options['avecDit'] = [
-                'label' => 'Demande d\'achat avec DIT',
+                'label' => 'Demande d’achat avec DIT',
                 'url' => $this->getUrlGenerator()->generate('da_list_dit'),
                 'icon' => $this->getIconDaAvecDIT(),
                 'type' => 'simple'
@@ -37,7 +37,7 @@ class DaNewController extends Controller
 
         if ($estAdmin || $estCreateurDeDADirecte) {
             $options['direct'] = [
-                'label' => 'Demande d\'achat direct',
+                'label' => 'Demande d’achat direct',
                 'url' => $this->getUrlGenerator()->generate('da_new_direct', ['id' => 0]),
                 'icon' => $this->getIconDaDirect(),
                 'type' => 'simple'
@@ -49,12 +49,12 @@ class DaNewController extends Controller
                 'type' => 'groupe',
                 'sousOptions' => [
                     'mensuel' => [
-                        'label' => 'Réappro mensuel',
+                        'label' => 'Réapprovisionnement mensuel',
                         'url' => $this->getUrlGenerator()->generate('da_new_reappro_mensuel', ['id' => 0]),
                         'icon' => $this->getIconDaReapproMensuel()
                     ],
                     'ponctuel' => [
-                        'label' => 'Réappro ponctuel',
+                        'label' => 'Réapprovisionnement ponctuel',
                         'url' => $this->getUrlGenerator()->generate('da_new_reappro_ponctuel', ['id' => 0]),
                         'icon' => $this->getIconDaReapproPonctuel()
                     ]
