9614dea28327a4ccfb9262062ac8edbb392e17eb
Menja <ranofimenjajam@gmail.com>
2026-01-09 16:22:26 +0300

    changement de la logique dans le controleur pour la sélection de choix

--- src/Controller/da/Creation/DaNewController.php
+++ src/Controller/da/Creation/DaNewController.php
@@ -3,28 +3,67 @@
 namespace App\Controller\da\Creation;
 
 use App\Controller\Controller;
+use App\Controller\Traits\da\MarkupIconTrait;
 use Symfony\Component\Routing\Annotation\Route;
 
 /** @Route("/demande-appro") */
 class DaNewController extends Controller
 {
+    use MarkupIconTrait;
+
     /**
      * @Route("/da-first-form", name="da_first_form")
      */
     public function firstForm()
     {
-        //verification si user connecter
+        // Vérification si user connecté
         $this->verifierSessionUtilisateur();
 
+        $estAtelier = $this->estUserDansServiceAtelier();
+        $estCreateurDeDADirecte = $this->estCreateurDeDADirecte();
+        $estAdmin = $this->estAdmin();
+
+        // Préparer les options disponibles
+        $options = [];
+
+        if ($estAdmin || $estAtelier) {
+            $options['avecDit'] = [
+                'label' => 'Demande d\'achat avec DIT',
+                'url' => $this->getUrlGenerator()->generate('da_list_dit'),
+                'icon' => $this->getIconDaAvecDIT(),
+                'type' => 'simple'
+            ];
+        }
+
+        if ($estAdmin || $estCreateurDeDADirecte) {
+            $options['direct'] = [
+                'label' => 'Demande d\'achat direct',
+                'url' => $this->getUrlGenerator()->generate('da_new_direct', ['id' => 0]),
+                'icon' => $this->getIconDaDirect(),
+                'type' => 'simple'
+            ];
+
+            $options['reappro'] = [
+                'label' => 'Demande de réapprovisionnement',
+                'icon' => $this->faIconLayer('fa-boxes-stacked', null, '#0d6efd', null, '#cfe2ff'),
+                'type' => 'groupe',
+                'sousOptions' => [
+                    'mensuel' => [
+                        'label' => 'Réappro mensuel',
+                        'url' => $this->getUrlGenerator()->generate('da_new_reappro_mensuel', ['id' => 0]),
+                        'icon' => $this->getIconDaReapproMensuel()
+                    ],
+                    'ponctuel' => [
+                        'label' => 'Réappro ponctuel',
+                        'url' => $this->getUrlGenerator()->generate('da_new_reappro_ponctuel', ['id' => 0]),
+                        'icon' => $this->getIconDaReapproPonctuel()
+                    ]
+                ]
+            ];
+        }
+
         return $this->render('da/first-form.html.twig', [
-            'estAte'                 => $this->estUserDansServiceAtelier(),
-            'estCreateurDeDADirecte' => $this->estCreateurDeDADirecte(),
-            'urls'                   => [
-                'avecDit' => $this->getUrlGenerator()->generate('da_list_dit'),
-                'direct'  => $this->getUrlGenerator()->generate('da_new_direct', ['id' => 0]),
-                'reappro' => $this->getUrlGenerator()->generate('da_new_reappro_mensuel', ['id' => 0]),
-            ],
-            'estAdmin'               => $this->estAdmin(),
+            'options' => $options
         ]);
     }
 }
