f8ebdda5eaec660fc6e9238e9873ae1649d98e83
Menja <ranofimenjajam@gmail.com>
2026-01-13 15:29:19 +0300

    ajout de fonction pour rechercher des fileNames

--- src/Repository/da/DaObservationRepository.php
+++ src/Repository/da/DaObservationRepository.php
@@ -4,4 +4,18 @@ namespace App\Repository\da;
 
 use Doctrine\ORM\EntityRepository;
 
-class DaObservationRepository extends EntityRepository {}
+class DaObservationRepository extends EntityRepository
+{
+    /**
+     * @return array<int, array{numeroDemandeAppro: string, fileNames: array}>
+     */
+    public function findAttachmentsByNumeroDA(string $numDa): array
+    {
+        return $this->createQueryBuilder('do')
+            ->select('do.numDa, do.fileNames')
+            ->where('do.numDa = :numDa')
+            ->setParameter('numDa', $numDa)
+            ->getQuery()
+            ->getArrayResult();
+    }
+}
