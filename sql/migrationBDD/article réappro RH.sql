82399c206a264f55ae49e6f1030d24acec7c1949
Menja <ranofimenjajam@gmail.com>
2026-01-16 14:20:19 +0300

    ajout d'article pour RH

--- sql/migrationBDD/article réappro RH.sql
+++ sql/migrationBDD/article réappro RH.sql
@@ -6,43 +6,44 @@ VALUES
 ('FBU','1F0002',TRIM('AGRAFE 24/6'),'2','1235', '80', 'INF', '2025-12-01 00:00:00.000','2025-12-01 00:00:00.000'),
 ('FBU','1F0034',TRIM('POST IT MM'),'1','375', '80', 'INF', '2025-12-01 00:00:00.000','2025-12-01 00:00:00.000'),
 ('FBU','1F0041',TRIM('SCOTCH GM TRANSPARENT'),'1','900', '80', 'INF', '2025-12-01 00:00:00.000','2025-12-01 00:00:00.000')
-CEN	4A0004	4A0004	ENCAUSTIQUE                        	 3 
-CEN	4A0009	4A0009	LAVE VITRE                         	 6 
-CEN	4A0011	4A0011	SAVON LIQUIDE NICKEL CITRON 1L     	 11 
-CEN	4A0012	4A0012	SAVON NOSY MORCEAUX 120GR          	 4 
-CEN	4A0013	4A0013	SAVON PATE                         	 2 
-CEN	4A0014	4A0014	SAVON POUDRE 250GR MADIO           	 6 
-CEN	4A0016	4A0016	SERPILLÈRE                         	 6 
-CEN	4A0018	4A0018	VIM                                	 4 
-CEN	4A0019	4A0019	LAVE SOL MASTER BIDON 20L	 7 
-CEN	4A0021	4A0021	EAU DE JAVEL 1L                    	 8 
-CEN	4A0023	4A0023	LIQUIDE LAVE MAIN SANIGEL 5L	 3 
-CEN	4A0026	4A0026	DESINFECTANT MULTISURFACE HYDROALCO	 1 
-CEN	4A0029	4A0029	PAILLE DE FER PARQUET              	 3 
-CEN	4A0031	4A0031	GEL DESINFECTANT 5L                	 1 
-CEN	4B0026	4B0026	SMART ONE PH 20 1150	 46 
-CEN	4C0001	4C0001	CHAMOISINE                         	 7 
-CEN	4D0001	4D0001	BALAI BROSSE                       	 4 
-CEN	4D0004	4D0004	BALAI RASTA                        	 3 
-CEN	4D0009	4D0009	CANARD WC                          	 6 
-CEN	4D0010	4D0010	KIFAFA GASY                        	 2 
-FBU	1A0003	1A0003	CHEMISE CARTONNÉE                  	 1 
-FBU	1A0005	1A0005	RAME PAPIER A3                     	 1 
-FBU	1A0006	1A0006	RAME PAPIER A4                     	 17 
-FBU	1B0001	1B0001	BLOC NOTE GM                       	 2 
-FBU	1B0003	1B0003	BLOC PAPILLON AVEC SUPPORT FLUO    	 1 
-FBU	1B0007	1B0007	CAHIER DE TRANSMISSION             	 1 
-FBU	1B0008	1B0008	CAHIER GM 200P                     	 1 
-FBU	1B0015	1B0015	CARNET BON DE CAISSE               	 2 
-FBU	1B0030	1B0030	SOUS-CHEMISE                       	 1 
-FBU	1C0012	1C0012	STYLO BLEU                         	 2 
-FBU	1C0013	1C0013	STYLO GEL BLEU                     	 2 
-FBU	1D0001	1D0001	ENVELOPPE À FENÊTRE pqt de 100                	 19 
-FBU	1D0006	1D0006	ENVELOPPE GM HFF                   	 50 
-FBU	1D0007	1D0007	ENVELOPPE PM HFF                   	 67 
-FBU	1E0002	1E0002	CARTON ARCHIVE DOS 15              	 1 
-FBU	1F0002	1F0002	AGRAFE 24/6                        	 4 
-FBU	1F0010	1F0010	BLANCO                             	 1 
-FBU	1F0027	1F0027	PILE LR3                           	 1 
-FBU	1F0028	1F0028	PILE LR6                           	 1 
-FBU	1F0046	1F0046	TROMBONE PM                        	 1 
+CEN	4A0004	ENCAUSTIQUE                        	 3 	 23 750 
+CEN	4A0009	LAVE VITRE                         	 6 	 5 035 
+CEN	4A0011	SAVON LIQUIDE NICKEL CITRON 1L     	 11 	 3 515 
+CEN	4A0012	SAVON NOSY MORCEAUX 120GR          	 4 	 2 280 
+CEN	4A0013	SAVON PATE                         	 2 	 5 415 
+CEN	4A0014	SAVON POUDRE 250GR MADIO           	 6 	 5 225 
+CEN	4A0016	SERPILLÈRE                         	 6 	 7 600 
+CEN	4A0018	VIM                                	 4 	 2 375 
+CEN	4A0019	LAVE SOL MASTER BIDON 20L	 7 	 3 865 
+CEN	4A0021	EAU DE JAVEL 1L                    	 8 	 2 090 
+CEN	4A0023	LIQUIDE LAVE MAIN SANIGEL 5L      	 3 	 22 325 
+CEN	4A0026	DESINFECTANT MULTISURFACE HYDROALCO	 1 	 16 625 
+CEN	4A0029	PAILLE DE FER PARQUET              	 3 	 950 
+CEN	4A0031	GEL DESINFECTANT 5L                	 1 	 90 250 
+CEN	4B0026	SMART ONE PH 20 1150	 46 	 23 700 
+CEN	4C0001	CHAMOISINE                         	 7 	 2 470 
+CEN	4D0001	BALAI BROSSE                       	 4 	 6 175 
+CEN	4D0004	BALAI RASTA                        	 3 	 7 125 
+CEN	4D0009	CANARD WC                          	 6 	 10 450 
+CEN	4D0010	KIFAFA GASY                        	 2 	 2 850 
+FBU	1A0003	CHEMISE CARTONNÉE                  	 1 	 28 500 
+FBU	1A0005	RAME PAPIER A3                     	 1 	 52 250 
+FBU	1A0006	RAME PAPIER A4                     	 17 	 23 275 
+FBU	1B0001	BLOC NOTE GM                       	 2 	 4 465 
+FBU	1B0003	BLOC PAPILLON AVEC SUPPORT FLUO    	 1 	 17 100 
+FBU	1B0007	CAHIER DE TRANSMISSION             	 1 	 14 250 
+FBU	1B0008	CAHIER GM 200P                     	 1 	 5 700 
+FBU	1B0015	CARNET BON DE CAISSE               	 2 	 7 600 
+FBU	1B0030	SOUS-CHEMISE                       	 1 	 33 250 
+FBU	1C0012	STYLO BLEU                         	 2 	 570 
+FBU	1C0013	STYLO GEL BLEU                     	 2 	 10 450 
+FBU	1D0001	ENVELOPPE À FENÊTRE pqt de 100                	 19 	 15 300 
+FBU	1D0006	ENVELOPPE GM HFF                   	 50 	 417 
+FBU	1D0007	ENVELOPPE PM HFF                   	 67 	 362 
+FBU	1E0002	CARTON ARCHIVE DOS 15              	 1 	 4 465 
+FBU	1F0002	AGRAFE 24/6                        	 4 	 1 235 
+FBU	1F0010	BLANCO                             	 1 	 6 175 
+FBU	1F0027	PILE LR3                           	 1 	 9 500 
+FBU	1F0028	PILE LR6                           	 1 	 9 500 
+FBU	1F0046	TROMBONE PM                        	 1 	 950 
+ 
