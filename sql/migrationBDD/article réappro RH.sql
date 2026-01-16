9066b52cb15dd385501eb3f8c4fe874e0e11c594
Menja <ranofimenjajam@gmail.com>
2026-01-16 14:26:14 +0300

    ajout d'article pour RH

--- sql/migrationBDD/article réappro RH.sql
+++ sql/migrationBDD/article réappro RH.sql
@@ -1,44 +1,45 @@
 INSERT INTO HFF_INTRANET.dbo.da_article_reappro
 (art_constp, art_refp, art_desi, qte_validee_appro, art_pu, code_agence, code_service, date_creation, date_modification)
 VALUES
-CEN	4A0004	ENCAUSTIQUE                        	 3 	 23 750 
-CEN	4A0009	LAVE VITRE                         	 6 	 5 035 
-CEN	4A0011	SAVON LIQUIDE NICKEL CITRON 1L     	 11 	 3 515 
-CEN	4A0012	SAVON NOSY MORCEAUX 120GR          	 4 	 2 280 
-CEN	4A0013	SAVON PATE                         	 2 	 5 415 
-CEN	4A0014	SAVON POUDRE 250GR MADIO           	 6 	 5 225 
-CEN	4A0016	SERPILLÈRE                         	 6 	 7 600 
-CEN	4A0018	VIM                                	 4 	 2 375 
-CEN	4A0019	LAVE SOL MASTER BIDON 20L	 7 	 3 865 
-CEN	4A0021	EAU DE JAVEL 1L                    	 8 	 2 090 
-CEN	4A0023	LIQUIDE LAVE MAIN SANIGEL 5L      	 3 	 22 325 
-CEN	4A0026	DESINFECTANT MULTISURFACE HYDROALCO	 1 	 16 625 
-CEN	4A0029	PAILLE DE FER PARQUET              	 3 	 950 
-CEN	4A0031	GEL DESINFECTANT 5L                	 1 	 90 250 
-CEN	4B0026	SMART ONE PH 20 1150	 46 	 23 700 
-CEN	4C0001	CHAMOISINE                         	 7 	 2 470 
-CEN	4D0001	BALAI BROSSE                       	 4 	 6 175 
-CEN	4D0004	BALAI RASTA                        	 3 	 7 125 
-CEN	4D0009	CANARD WC                          	 6 	 10 450 
-CEN	4D0010	KIFAFA GASY                        	 2 	 2 850 
-FBU	1A0003	CHEMISE CARTONNÉE                  	 1 	 28 500 
-FBU	1A0005	RAME PAPIER A3                     	 1 	 52 250 
-FBU	1A0006	RAME PAPIER A4                     	 17 	 23 275 
-FBU	1B0001	BLOC NOTE GM                       	 2 	 4 465 
-FBU	1B0003	BLOC PAPILLON AVEC SUPPORT FLUO    	 1 	 17 100 
-FBU	1B0007	CAHIER DE TRANSMISSION             	 1 	 14 250 
-FBU	1B0008	CAHIER GM 200P                     	 1 	 5 700 
-FBU	1B0015	CARNET BON DE CAISSE               	 2 	 7 600 
-FBU	1B0030	SOUS-CHEMISE                       	 1 	 33 250 
-FBU	1C0012	STYLO BLEU                         	 2 	 570 
-FBU	1C0013	STYLO GEL BLEU                     	 2 	 10 450 
-FBU	1D0001	ENVELOPPE À FENÊTRE pqt de 100                	 19 	 15 300 
-FBU	1D0006	ENVELOPPE GM HFF                   	 50 	 417 
-FBU	1D0007	ENVELOPPE PM HFF                   	 67 	 362 
-FBU	1E0002	CARTON ARCHIVE DOS 15              	 1 	 4 465 
-FBU	1F0002	AGRAFE 24/6                        	 4 	 1 235 
-FBU	1F0010	BLANCO                             	 1 	 6 175 
-FBU	1F0027	PILE LR3                           	 1 	 9 500 
-FBU	1F0028	PILE LR6                           	 1 	 9 500 
-FBU	1F0046	TROMBONE PM                        	 1 	 950 
- 
+('CEN','4A0004',TRIM('ENCAUSTIQUE')                        ,'3' ,'23750' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0009',TRIM('LAVE VITRE')                         ,'6' ,'5035'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0011',TRIM('SAVON LIQUIDE NICKEL CITRON 1L')     ,'11','3515'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0012',TRIM('SAVON NOSY MORCEAUX 120GR')          ,'4' ,'2280'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0013',TRIM('SAVON PATE')                         ,'2' ,'5415'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0014',TRIM('SAVON POUDRE 250GR MADIO')           ,'6' ,'5225'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0016',TRIM('SERPILLÈRE')                         ,'6' ,'7600'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0018',TRIM('VIM')                                ,'4' ,'2375'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0019',TRIM('LAVE SOL MASTER BIDON 20L')          ,'7' ,'3865'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0021',TRIM('EAU DE JAVEL 1L')                    ,'8' ,'2090'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0023',TRIM('LIQUIDE LAVE MAIN SANIGEL 5L')       ,'3' ,'22325' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0026',TRIM('DESINFECTANT MULTISURFACE HYDROALCO'),'1' ,'16625' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0029',TRIM('PAILLE DE FER PARQUET')              ,'3' ,'950'   , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4A0031',TRIM('GEL DESINFECTANT 5L')                ,'1' ,'90250' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4B0026',TRIM('SMART ONE PH 20 1150')               ,'46','23700' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4C0001',TRIM('CHAMOISINE')                         ,'7' ,'2470'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4D0001',TRIM('BALAI BROSSE')                       ,'4' ,'6175'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4D0004',TRIM('BALAI RASTA')                        ,'3' ,'7125'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4D0009',TRIM('CANARD WC')                          ,'6' ,'10450' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('CEN','4D0010',TRIM('KIFAFA GASY')                        ,'2' ,'2850'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1A0003',TRIM('CHEMISE CARTONNÉE')                  ,'1' ,'28500' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1A0005',TRIM('RAME PAPIER A3')                     ,'1' ,'52250' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1A0006',TRIM('RAME PAPIER A4')                     ,'17','23275' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1B0001',TRIM('BLOC NOTE GM')                       ,'2' ,'4465'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1B0003',TRIM('BLOC PAPILLON AVEC SUPPORT FLUO')    ,'1' ,'17100' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1B0007',TRIM('CAHIER DE TRANSMISSION')             ,'1' ,'14250' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1B0008',TRIM('CAHIER GM 200P')                     ,'1' ,'5700'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1B0015',TRIM('CARNET BON DE CAISSE')               ,'2' ,'7600'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1B0030',TRIM('SOUS-CHEMISE')                       ,'1' ,'33250' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1C0012',TRIM('STYLO BLEU')                         ,'2' ,'570'   , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1C0013',TRIM('STYLO GEL BLEU')                     ,'2' ,'10450' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1D0001',TRIM('ENVELOPPE À FENÊTRE pqt de 100')     ,'19','15300' , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1D0006',TRIM('ENVELOPPE GM HFF')                   ,'50','417'   , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1D0007',TRIM('ENVELOPPE PM HFF')                   ,'67','362'   , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1E0002',TRIM('CARTON ARCHIVE DOS 15')              ,'1' ,'4465'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1F0002',TRIM('AGRAFE 24/6')                        ,'4' ,'1235'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1F0010',TRIM('BLANCO')                             ,'1' ,'6175'  , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1F0027',TRIM('PILE LR3')                           ,'1' ,'950'   , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1F0028',TRIM('PILE LR6')                           ,'1' ,'950'   , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1F0046',TRIM('TROMBONE PM')                        ,'1' ,'950'   , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1F0028',TRIM('PILE LR6')                           ,'1' ,'950'   , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
+('FBU','1F0046',TRIM('TROMBONE PM')                        ,'1' ,'950'   , '80', 'PER', '2026-01-16 00:00:00.000','2026-01-16 00:00:00.000'),
