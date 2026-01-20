4857ce1befdf19d898985065f234523006349517
Menja <ranofimenjajam@gmail.com>
2026-01-20 10:44:33 +0300

    résoudre le premier form de création

--- src/Controller/da/Creation/DaNewController.php
+++ src/Controller/da/Creation/DaNewController.php
@@ -28,37 +28,26 @@ class DaNewController extends Controller
 
         if ($estAdmin || $estAtelier) {
             $options['avecDit'] = [
-                'label' => 'Demande d’achat avec DIT',
-                'url' => $this->getUrlGenerator()->generate('da_list_dit'),
-                'icon' => $this->getIconDaAvecDIT(),
-                'type' => 'simple'
+                'label' => 'Demande d’approvisionnement avec DIT',
+                'url'   => $this->getUrlGenerator()->generate('da_list_dit'),
+                'icon'  => $this->getIconDaAvecDIT(),
+                'type'  => 'simple'
             ];
         }
 
         if ($estAdmin || $estCreateurDeDADirecte) {
             $options['direct'] = [
-                'label' => 'Demande d’achat direct',
-                'url' => $this->getUrlGenerator()->generate('da_new_direct', ['id' => 0]),
-                'icon' => $this->getIconDaDirect(),
-                'type' => 'simple'
+                'label' => 'Demande d’achat',
+                'url'   => $this->getUrlGenerator()->generate('da_new_achat', ['id' => 0]),
+                'icon'  => $this->getIconDaDirect(),
+                'type'  => 'simple'
             ];
 
             $options['reappro'] = [
                 'label' => 'Demande de réapprovisionnement',
-                'icon' => $this->faIconLayer('fa-boxes-stacked', null, '#0d6efd', null, '#cfe2ff'),
-                'type' => 'groupe',
-                'sousOptions' => [
-                    'mensuel' => [
-                        'label' => 'Réapprovisionnement mensuel',
-                        'url' => $this->getUrlGenerator()->generate('da_new_reappro_mensuel', ['id' => 0]),
-                        'icon' => $this->getIconDaReapproMensuel()
-                    ],
-                    'ponctuel' => [
-                        'label' => 'Réapprovisionnement ponctuel',
-                        'url' => $this->getUrlGenerator()->generate('da_new_reappro_ponctuel', ['id' => 0]),
-                        'icon' => $this->getIconDaReapproPonctuel()
-                    ]
-                ]
+                'url'   => $this->getUrlGenerator()->generate('da_new_reappro_mensuel', ['id' => 0]),
+                'icon'  => $this->getIconDaReapproMensuel(),
+                'type'  => 'simple'
             ];
         }
 
