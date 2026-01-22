d20791fbbd4f51ff94880ea9ce4ae6debca076f6
Menja <ranofimenjajam@gmail.com>
2026-01-22 11:39:18 +0300

    vue + controlleur pour l'affectation d'articles

---
+++ src/Controller/da/Affectation/DaAffectationAchatController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Controller\da\Affectation;
+
+use App\Controller\Controller;
+use App\Controller\Traits\AutorisationTrait;
+use App\Entity\admin\Application;
+use App\Entity\da\DemandeApproParent;
+use App\Repository\da\DemandeApproParentRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/** @Route("/demande-appro") */
+class DaAffectationAchatController extends Controller
+{
+    use AutorisationTrait;
+    private DemandeApproParentRepository $demandeApproParentRepository;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $em = $this->getEntityManager();
+        $this->demandeApproParentRepository = $em->getRepository(DemandeApproParent::class);
+    }
+
+    /**
+     * @Route("/affectation-achat/{id}", name="da_affectation_achat")
+     */
+    public function affectationDaAchat($id, Request $request)
+    {
+        //verification si user connecter
+        $this->verifierSessionUtilisateur();
+
+        /** Autorisation accès */
+        $this->autorisationAcces($this->getUser(), Application::ID_DAP);
+        /** FIN AUtorisation accès */
+
+        /** @var DemandeApproParent $daParent */
+        $daParent = $this->demandeApproParentRepository->find($id);
+
+        $form = $this->getFormFactory()->createBuilder(DemandeApproParentType::class, $daParent)->getForm();
+
+        dump($daParent);
+        foreach ($daParent->getDemandeApproParentLines() as $demandeApproParentLine) {
+            dump($demandeApproParentLine);
+        }
+        die;
+
+        $daObservation = new DaObservation();
+
+        $formReappro = $this->getFormFactory()->createBuilder(DaObservationValidationType::class, $daObservation)->getForm();
+        $formObservation = $this->getFormFactory()->createBuilder(DaObservationType::class, $daObservation, ['daTypeId' => $da->getDaTypeId()])->getForm();
+
+        $dateRange = $this->getLast12MonthsRange();
+        $monthsList = $this->getMonthsList($dateRange['start'], $dateRange['end']);
+        $dataHistoriqueConsommation = $this->getHistoriqueConsommation($da, $dateRange, $monthsList);
+        $observations = $this->daObservationRepository->findBy(['numDa' => $da->getNumeroDemandeAppro()]);
+
+        //========================================== Traitement du formulaire en général ===================================================//
+        $this->traitementFormulaire($formReappro, $formObservation, $request, $da, $observations, $monthsList, $dataHistoriqueConsommation);
+        //==================================================================================================================================//
+
+        $fichiers = $this->getAllDAFile([
+            'baiPath'   => $this->getBaIntranetPath($da),
+            'badPath'   => $this->getBaDocuWarePath($da),
+        ]);
+
+        return $this->render("da/affectation-da.html.twig", [
+            'demandeApproParent' => $daParent,
+        ]);
+    }
+}
