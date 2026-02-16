a9783688416ca4d8f595d16d0db30dd7af9d4219
Menja <ranofimenjajam@gmail.com>
2026-02-16 15:46:46 +0300

    select2SelectAll dans vignette

--- Views/js/admin/vignette/vignette.js
+++ Views/js/admin/vignette/vignette.js
@@ -1,10 +1,4 @@
-$(document).ready(function () {
-  $("#vignette_applications").select2({
-    placeholder: "-- Choisir application(s) associée(s) --",
-    allowClear: true,
-    theme: "bootstrap",
-  });
-});
+import { initSelect2WithSelectAll } from "../../utils/select2SelectAll";
 
 document.addEventListener("DOMContentLoaded", function () {
   const nom = document.querySelector("#vignette_nom");
@@ -16,5 +10,9 @@ document.addEventListener("DOMContentLoaded", function () {
 
   reference.addEventListener("input", function () {
     this.value = this.value.toUpperCase().slice(0, 10);
+  });
+
+  initSelect2WithSelectAll("#vignette_applications", {
+    placeholder: "-- Choisir application(s) associée(s) --",
   });
 });
