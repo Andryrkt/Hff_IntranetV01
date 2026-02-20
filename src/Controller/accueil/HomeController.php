18652ffc7aa07fd7afed795a91eedcecca0bdc4f
Menja <ranofimenjajam@gmail.com>
2026-02-20 10:52:20 +0300

    modification de home controller

--- src/Controller/accueil/HomeController.php
+++ src/Controller/accueil/HomeController.php
@@ -3,26 +3,17 @@
 namespace App\Controller\accueil;
 
 use App\Controller\Controller;
-use App\Service\navigation\MenuService;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends Controller
 {
-    public function __construct()
-    {
-        parent::__construct();
-    }
-
     /**
      * @Route("/", name="profil_acceuil")
      */
     public function showPageAcceuil()
     {
-        /** @var MenuService $menuService */
-        $menuService = $this->getContainer()->get('menuService');
-
         return $this->render('main/accueil.html.twig', [
-            'menuItems' => $menuService->getMenuStructure(),
+            'menuItems' => $this->getMenuService()->getMenuStructure(),
         ]);
     }
 }
