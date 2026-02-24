3c5c2f8fd70bb1481d1c9b8f50d36b8b14a8e70d
Menja <ranofimenjajam@gmail.com>
2026-02-24 15:03:01 +0300

    ajout de méthode de batch pour la vérification si trop perçu

--- src/Model/dom/DomModel.php
+++ src/Model/dom/DomModel.php
@@ -3,7 +3,7 @@
 namespace App\Model\dom;
 
 use App\Model\Model;
-
+use App\Service\TableauEnStringService;
 
 class DomModel extends Model
 {
@@ -27,6 +27,36 @@ class DomModel extends Model
         }
 
         return $DateM;
+    }
+
+    public function verifierSiTropPercuBatch(array $numeroDoms): array
+    {
+        if (empty($numeroDoms)) {
+            return [];
+        }
+
+        $placeholders = TableauEnStringService::orEnString($numeroDoms);
+
+        $sql = "SELECT
+                    dom.Numero_Ordre_Mission,
+                    CASE
+                        WHEN (dom.Nombre_Jour - COALESCE(SUM(domtp.Nombre_Jour_Tp), 0)) > 0 THEN 1
+                        ELSE 0
+                    END AS is_trop_percu
+                FROM Demande_ordre_mission dom
+                LEFT JOIN Demande_ordre_mission_tp domtp
+                    ON dom.Numero_Ordre_Mission = domtp.Numero_Ordre_Mission
+                WHERE dom.Numero_Ordre_Mission IN ($placeholders)
+                AND dom.Sous_Type_Document IN (2, 3)
+                GROUP BY dom.Numero_Ordre_Mission, dom.Nombre_Jour";
+
+        $result = [];
+        $rs = $this->connexion->query($sql);
+        while ($row = odbc_fetch_array($rs)) {
+            $result[$row['Numero_Ordre_Mission']] = (bool) $row['is_trop_percu'];
+        }
+
+        return $result;
     }
 
     public function verifierSiTropPercu(string $numeroDom)
@@ -57,133 +87,4 @@ class DomModel extends Model
 
         return $result['reponse'];
     }
-
-    /**
-     * recuperer le nom et prenoms du matricule 
-     */
-    // public function getName($Matricule)
-    // {
-    //     $Queryname  = "SELECT Nom, Prenoms
-    //                     FROM Personnel
-    //                     WHERE Matricule = '" . $Matricule . "'";
-    //     $execCname = $this->connexion->query($Queryname);
-    //     $Infopers = array();
-    //     while ($tabInfo = odbc_fetch_array($execCname)) {
-    //         $Infopers[] = $tabInfo;
-    //     }
-    //     return $Infopers;
-    // }
-    // categorie
-    /**
-     * recuperation des catégories selon le type de mission et code agence 
-     */
-    // public function CategPers($TypeMiss, $codeAg)
-    // {
-    //     $SqlTypeMiss = "SELECT DISTINCT
-    //                     Catg 
-    //                     FROM Idemnity 
-    //                     WHERE Type = '" . $TypeMiss . "' 
-    //                     AND Rmq  in('STD','" . $codeAg . "')";
-    //     $execSqlTypeMiss = $this->connexion->query($SqlTypeMiss);
-    //     $ListCatg = array();
-    //     while ($TabTYpeMiss = odbc_fetch_array($execSqlTypeMiss)) {
-    //         $ListCatg[] = $TabTYpeMiss;
-    //     }
-    //     return $ListCatg;
-    // }
-    /**
-     * recuperation catégorie Rental 
-     * @param default CodeR : 50 
-     */
-    // public function catgeRental($CodeR)
-    // {
-    //     $SqlRentacatg = "SELECT DISTINCT Catg FROM Idemnity WHERE Type = 'MUTATION' AND Rmq = '" . $CodeR . "' ";
-    //     $exSql = $this->connexion->query($SqlRentacatg);
-    //     $ListCatge = array();
-    //     while ($tab_list = odbc_fetch_array($exSql)) {
-    //         $ListCatge[] = $tab_list;
-    //     }
-    //     return $ListCatge;
-    // }
-
-    /**
-     * selection site (region ) 
-     * @param  TypeM: type de mission 
-     * @param catgPERs:  Catégorie du personnel selectionner 
-     */
-    // public function SelectSite($TypeM, $catgPERs)
-    // {
-    //     $Site = "SELECT DISTINCT Destination FROM Idemnity WHERE Type = '" . $TypeM . "' AND Catg='" . $catgPERs . "'  ";
-    //     $exSite = $this->connexion->query($Site);
-    //     $list = array();
-    //     while ($tab = odbc_fetch_array($exSite)) {
-    //         $list[] = $tab;
-    //     }
-    //     return $list;
-    // }
-
-
-    /**
-     * recuperation Prix des idemnité
-     * @param TypeM: Type de mission
-     * @param CategPers: Catgégorie du personnel selectionner 
-     * @param Dest : site (region) selectionner
-     * @param AgCode: Code agence 
-     */
-    // public function SelectMUTPrixRental($TypeM, $CategPers, $Dest, $AgCode)
-    // {
-    //     $PrixRental = "SELECT DISTINCT Montant_idemnite FROM Idemnity WHERE Type = '" . $TypeM . "' 
-    //                 AND Catg = '" . $CategPers . "' AND Destination = '" . $Dest . "' AND Rmq = '" . $AgCode . "' ";
-    //     $exPrixRental = $this->connexion->query($PrixRental);
-    //     $Prix = array();
-    //     while ($tab_prix = odbc_fetch_array($exPrixRental)) {
-    //         $Prix[] = $tab_prix;
-    //     }
-    //     return $Prix;
-    // }
-
-
-    //count si 50 catg 
-    /**
-     * test si le catgérie appartion à l'agence 50
-     */
-    // public function SiRentalCatg($catg)
-    // {
-    //     $sqlcount = "SELECT count(*) as nbCount FROM Idemnity WHERE Catg ='" . $catg . "' and Rmq = '50' ";
-    //     $exsqlcount = $this->connexion->query($sqlcount);
-    //     return $exsqlcount ? odbc_fetch_array($exsqlcount)['nbCount'] : false;
-    // }
-
-    /*
-    public function agenceDebiteur()
-    {
-        $statement = " SELECT 
-        a.code_agence + ' ' + a.libelle_agence as agenceDebiteur 
-        FROM agences a";
-
-        $sql= $this->connexion->query($statement);
-        $agences = array();
-        while ($tab = odbc_fetch_array($sql)) {
-            $agences[] = $tab;
-        }
-        return $agences;
-    }
-    
-    public function serviceDebiteur($agenceId)
-    {
-        $statement = "SELECT DISTINCT 
-        s.code_service + ' ' + s.libelle_service as serviceDebiteur
-        FROM services s
-        INNER JOIN agence_service ags ON s.id = ags.service_id
-        INNER JOIN agences a ON a.id = ags.agence_id
-        WHERE a.code_agence + ' ' + a.libelle_agence = '". $agenceId ."'
-        ";
-        $sql= $this->connexion->query($statement);
-        $services = array();
-        while ($tab = odbc_fetch_array($sql)) {
-            $services[] = $tab;
-        }
-        return $services;
-    }
-        */
 }
