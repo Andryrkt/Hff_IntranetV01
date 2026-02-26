65e04fae2215e0a775e280efc83d8c839ce72e1f
Menja <ranofimenjajam@gmail.com>
2026-02-26 10:46:37 +0300

    liste des OR à livrer

--- src/Controller/magasin/ors/Livrer/OrLivrerController.php
+++ src/Controller/magasin/ors/Livrer/OrLivrerController.php
@@ -6,16 +6,13 @@ namespace App\Controller\magasin\ors\Livrer;
 ini_set('max_execution_time', 10000);
 ini_set('memory_limit', '1000M');
 
+use App\Constants\admin\ApplicationConstant;
 use App\Controller\Controller;
-use App\Entity\admin\Application;
 use App\Service\TableauEnStringService;
 use App\Controller\Traits\Transformation;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Form\magasin\MagasinListeOrALivrerSearchType;
 use App\Controller\Traits\magasin\ors\MagasinOrALivrerTrait;
-use App\Entity\admin\utilisateur\Role;
-
 /**
  * @Route("/magasin/or")
  */
@@ -30,20 +27,13 @@ class OrLivrerController extends Controller
      */
     public function listOrLivrer(Request $request)
     {
-        $codeAgence = $this->getUser()->getAgenceAutoriserCode();
-        $serviceAgence = $this->getUser()->getServiceAutoriserCode();
+        $agenceServiceAutorises = $this->getSecurityService()->getAgenceServices(ApplicationConstant::CODE_MAGASIN);
 
-        /** CREATION D'AUTORISATION */
-        $autoriser = $this->hasRoles(Role::ROLE_ADMINISTRATEUR, Role::ROLE_MULTI_SUCURSALES);
-        //FIN AUTORISATION
+        $codeAgence = array_column($agenceServiceAutorises, 'agence_code');
 
-        if ($autoriser) {
-            $agenceUser = "''";
-        } else {
-            $agenceUser = TableauEnStringService::TableauEnString(',', $codeAgence);
-        }
+        $agenceUser = TableauEnStringService::TableauEnString(',', $codeAgence);
 
-        $form = $this->getFormFactory()->createBuilder(MagasinListeOrALivrerSearchType::class, ['agenceUser' => $agenceUser, 'autoriser' => $autoriser], [
+        $form = $this->getFormFactory()->createBuilder(MagasinListeOrALivrerSearchType::class, ['agenceUser' => $agenceUser], [
             'method' => 'GET'
         ])->getForm();
 
