e74f23e373bf57b316d85c2dc79ddce18d4defcf
Menja <ranofimenjajam@gmail.com>
2026-03-10 09:31:07 +0300

    ajout de code Societe dans l'entité de facture

--- sql/profilUser/profilUser.sql
+++ sql/profilUser/profilUser.sql
@@ -122,3 +122,7 @@ alter table demande_intervention drop column societe;
 alter table devis_soumis_a_validation add code_societe varchar(2) null;
 
 update devis_soumis_a_validation set code_societe='HF';
+
+alter table facture_soumis_a_validation add code_societe varchar(2) null;
+
+update facture_soumis_a_validation set code_societe='HF';
