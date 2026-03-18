b9eb4bb9c4e4eb0f88ef5c9031833a112af3cd05
Andryrkt <andryrkt08@gmail.com>
2026-03-18 10:07:54 +0300

    rectification de la filtre matricule sur la liste de congé

--- src/Controller/ddc/CongeController.php
+++ src/Controller/ddc/CongeController.php
@@ -103,7 +103,7 @@ class CongeController extends Controller
 
             // Gestion spéciale pour les matricules multiples
             // On divise la chaîne de matricules multiples pour éviter les problèmes de longueur de champ
-            $originalMatricule = $congeSearch->getMatricule();
+            $originalMatricule = $form->get('matricule')->getData();
             if ($originalMatricule && strpos($originalMatricule, ',') !== false) {
                 // Si plusieurs matricules sont fournis, on ne les stocke pas dans l'entité
                 // pour éviter les problèmes avec la longueur du champ (length=4)
