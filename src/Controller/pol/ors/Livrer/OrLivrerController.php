03d6558d410f5b835e4107ee5a59c2d5387b4940
Menja <ranofimenjajam@gmail.com>
2026-03-23 16:55:07 +0300

    résolution de multisuccursale sur POL ORS à traiter / livrer

--- src/Controller/pol/ors/Livrer/OrLivrerController.php
+++ src/Controller/pol/ors/Livrer/OrLivrerController.php
@@ -31,14 +31,19 @@ class OrLivrerController extends Controller
      */
     public function listOrLivrer(Request $request)
     {
+        $agenceUser = "''";
+
         // Vérifier la permission de voir tous les données
         $multisuccursale = $this->getSecurityService()->verifierPermission(SecurityService::PERMISSION_MULTI_SUCCURSALE);
 
-        $agenceServiceAutorises = $this->getSecurityService()->getAgenceServices(ApplicationConstant::CODE_POL);
+        if (!$multisuccursale) {
+            $agenceServiceAutorises = $this->getSecurityService()->getAgenceServices(ApplicationConstant::CODE_POL);
 
-        $codeAgence = array_column($agenceServiceAutorises, 'agence_code');
+            // Si l'utilisateur n'a pas d'agence et service autorisé, on prend son agence par défaut
+            $codeAgence = empty($agenceServiceAutorises) ? [$this->getSecurityService()->getCodeAgenceUser()] : array_column($agenceServiceAutorises, 'agence_code');
 
-        $agenceUser = $multisuccursale ? "''" : TableauEnStringService::TableauEnString(',', $codeAgence);
+            $agenceUser = TableauEnStringService::TableauEnString(',', $codeAgence);
+        }
 
         $form = $this->getFormFactory()->createBuilder(MagasinListeOrALivrerSearchType::class, ['agenceUser' => $agenceUser], [
             'method' => 'GET',
