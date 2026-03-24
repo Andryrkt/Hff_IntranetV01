f079482932f64cb069381a38972eeae30b92be11
Andryrkt <andryrkt08@gmail.com>
2026-03-24 11:30:49 +0300

    reorganisation des options pour le filtre de statu DA et BC dans la liste cde frn et list DA

--- src/Form/da/DaSearchType.php
+++ src/Form/da/DaSearchType.php
@@ -29,15 +29,16 @@ class DaSearchType extends  AbstractType
     use MarkupIconTrait;
 
     private const STATUT_DA = [
-        DemandeAppro::STATUT_VALIDE               => DemandeAppro::STATUT_VALIDE,
-        DemandeAppro::STATUT_CLOTUREE             => DemandeAppro::STATUT_CLOTUREE,
-        DemandeAppro::STATUT_SOUMIS_ATE           => DemandeAppro::STATUT_SOUMIS_ATE,
-        DemandeAppro::STATUT_SOUMIS_APPRO         => DemandeAppro::STATUT_SOUMIS_APPRO,
+        DemandeAppro::STATUT_EN_COURS_CREATION    => DemandeAppro::STATUT_EN_COURS_CREATION,
+        DemandeAppro::STATUT_SOUMIS_APPRO         => DemandeAppro::STATUT_SOUMIS_APPRO, // demande d'achat
         DemandeAppro::STATUT_DEMANDE_DEVIS        => DemandeAppro::STATUT_DEMANDE_DEVIS,
         DemandeAppro::STATUT_DEVIS_A_RELANCER     => DemandeAppro::STATUT_DEVIS_A_RELANCER,
-        DemandeAppro::STATUT_EN_COURS_CREATION    => DemandeAppro::STATUT_EN_COURS_CREATION,
-        DemandeAppro::STATUT_AUTORISER_EMETTEUR   => DemandeAppro::STATUT_AUTORISER_EMETTEUR,
+        DemandeAppro::STATUT_AUTORISER_EMETTEUR   => DemandeAppro::STATUT_AUTORISER_EMETTEUR, // Création demande initiale
         DemandeAppro::STATUT_EN_COURS_PROPOSITION => DemandeAppro::STATUT_EN_COURS_PROPOSITION,
+        DemandeAppro::STATUT_SOUMIS_ATE           => DemandeAppro::STATUT_SOUMIS_ATE, // proposition d'achat
+        DemandeAppro::STATUT_VALIDE               => DemandeAppro::STATUT_VALIDE, // Bon d'achats validé
+        DemandeAppro::STATUT_CLOTUREE             => DemandeAppro::STATUT_CLOTUREE,
+        DemandeAppro::STATUT_CLOTUREE_HORS_DELAI  => DemandeAppro::STATUT_CLOTUREE_HORS_DELAI,
     ];
 
     private const STATUT_BC = [
@@ -45,17 +46,18 @@ class DaSearchType extends  AbstractType
         DaSoumissionBc::STATUT_A_EDITER                 => DaSoumissionBc::STATUT_A_EDITER,
         DaSoumissionBc::STATUT_A_SOUMETTRE_A_VALIDATION => DaSoumissionBc::STATUT_A_SOUMETTRE_A_VALIDATION,
         DaSoumissionBc::STATUT_A_VALIDER_DA             => DaSoumissionBc::STATUT_A_VALIDER_DA,
+        DaSoumissionBc::STATUT_REFUSE                   => DaSoumissionBc::STATUT_REFUSE,
         DaSoumissionBc::STATUT_A_ENVOYER_AU_FOURNISSEUR => DaSoumissionBc::STATUT_A_ENVOYER_AU_FOURNISSEUR,
         DaSoumissionBc::STATUT_BC_ENVOYE_AU_FOURNISSEUR => DaSoumissionBc::STATUT_BC_ENVOYE_AU_FOURNISSEUR,
         DaSoumissionBc::STATUT_NON_DISPO                => DaSoumissionBc::STATUT_NON_DISPO,
-        DaSoumissionBc::STATUT_SOUMISSION               => DaSoumissionBc::STATUT_SOUMISSION,
-        DaSoumissionBc::STATUT_VALIDE                   => DaSoumissionBc::STATUT_VALIDE,
-        DaSoumissionBc::STATUT_CLOTURE                  => DaSoumissionBc::STATUT_CLOTURE,
-        DaSoumissionBc::STATUT_REFUSE                   => DaSoumissionBc::STATUT_REFUSE,
-        DaSoumissionBc::STATUT_TOUS_LIVRES              => DaSoumissionBc::STATUT_TOUS_LIVRES,
-        DaSoumissionBc::STATUT_PARTIELLEMENT_LIVRE      => DaSoumissionBc::STATUT_PARTIELLEMENT_LIVRE,
+        DaSoumissionBc::STATUT_EN_COURS_DE_PREPARATION  => DaSoumissionBc::STATUT_EN_COURS_DE_PREPARATION,
         DaSoumissionBc::STATUT_PARTIELLEMENT_DISPO      => DaSoumissionBc::STATUT_PARTIELLEMENT_DISPO,
         DaSoumissionBc::STATUT_COMPLET_NON_LIVRE        => DaSoumissionBc::STATUT_COMPLET_NON_LIVRE,
+        DaSoumissionBc::STATUT_PARTIELLEMENT_LIVRE      => DaSoumissionBc::STATUT_PARTIELLEMENT_LIVRE,
+        DaSoumissionBc::STATUT_TOUS_LIVRES              => DaSoumissionBc::STATUT_TOUS_LIVRES,
+        DaSoumissionBc::STATUT_PAS_DANS_OR              => DaSoumissionBc::STATUT_PAS_DANS_OR,
+        DaSoumissionBc::STATUT_PAS_DANS_OR_CESSION      => DaSoumissionBc::STATUT_PAS_DANS_OR_CESSION,
+        DaSoumissionBc::STATUT_PAS_DANS_BC              => DaSoumissionBc::STATUT_PAS_DANS_BC,
     ];
 
     private const STATUT = [
@@ -88,10 +90,10 @@ class DaSearchType extends  AbstractType
         ksort($statut);
 
         $statut_bc = self::STATUT_BC;
-        ksort($statut_bc);
+        // ksort($statut_bc);
 
         $statut_da = self::STATUT_DA;
-        ksort($statut_da);
+        // ksort($statut_da);
 
         $type_achat = [
             'Demande d’approvisionnement via OR'      => DemandeAppro::TYPE_DA_AVEC_DIT,
