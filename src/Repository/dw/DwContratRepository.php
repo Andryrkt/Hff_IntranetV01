49b73290a0ffb4de805a787f35ce53ab42f87847
Menja <ranofimenjajam@gmail.com>
2026-03-31 16:57:25 +0300

    ajout de repository

---
+++ src/Repository/dw/DwContratRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Repository\dw;
+
+use Doctrine\ORM\EntityRepository;
+
+class DwContratRepository extends EntityRepository
+{
+    /** 
+     * Récupère le chemin du document associé à une référence de contrat.
+     * @param string $refContrat La référence de contrat lequel on souhaite récupérer le chemin.
+     */
+    public function getPathByRefContrat(string $refContrat)
+    {
+        return  $this->createQueryBuilder('d')
+            ->select('d.path')
+            ->where('d.refContrat = :refContrat')
+            ->setParameter('refContrat', $refContrat)
+            ->orderBy('d.numeroVersion', 'ASC')
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+}
