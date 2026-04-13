719e0e35f54127a3532e5f398126d058fb288d35
Menja <ranofimenjajam@gmail.com>
2026-04-13 15:06:02 +0300

    validation de DIT par un admin

--- src/Controller/dit/DitValidationController.php
+++ src/Controller/dit/DitValidationController.php
@@ -5,7 +5,6 @@ namespace App\Controller\dit;
 use App\Model\dit\DitModel;
 use App\Controller\Controller;
 use App\Form\dit\DitValidationType;
-use App\Entity\admin\utilisateur\User;
 use App\Entity\dit\DemandeIntervention;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -20,16 +19,7 @@ class DitValidationController extends Controller
      */
     public function validationDit($numDit, $id, Request $request)
     {
-
-        /** CREATION D'AUTORISATION */
-        $userConnecter = $this->getUser();
-        $roleNames = [];
-        foreach ($userConnecter->getRoles() as $role) {
-            $roleNames[] = $role->getRoleName();
-        }
-        $autoriser = in_array('ADMINISTRATEUR', $roleNames) || in_array('VALIDATEUR', $roleNames);
-        //FIN AUTORISATION
-
+        $autoriser = $this->estAdmin();
 
         $dit = $this->getEntityManager()->getRepository(DemandeIntervention::class)->find($id);
         $ditModel = new DitModel();
@@ -62,63 +52,6 @@ class DitValidationController extends Controller
 
         $form = $this->getFormFactory()->createBuilder(DitValidationType::class, $dit)->getForm();
 
-        // $form->handleRequest($request);
-
-        // // Vérifier si le formulaire est soumis et valide
-        // if ($form->isSubmitted() && $form->isValid()) {
-
-        //     $email = new EmailService();
-        //     $dit = $form->getData();
-
-        //     $userDemandeur = $this->getEntityManager()->getRepository(User::class)->findOneBy(['nom_utilisateur' => $dit->getUtilisateurDemandeur()]);
-        //     dump($userDemandeur);
-        //         $userDemandeur = $this->arrayToObjet($userDemandeur);
-        //         dump($userDemandeur);
-        //         $emailSuperieurs = $this->recupMailSuperieur($userDemandeur);
-        //         dump($emailSuperieurs);
-        //         $id = $this->getSessionService()->get('user_info')['id'];
-        //         dump($id);
-        //         $userConnecter = $this->getEntityManager()->getRepository(User::class)->find($id);
-        //         dump($userDemandeur);
-        //     if ($request->request->has('refuser')) {
-
-        //         $variableEmail = $this->donnerRefu($userDemandeur, $emailSuperieurs, $userConnecter, $dit);
-
-        //         $content = $this->emailRefu($variableEmail);
-
-        //         $this->confirmationEmail($email, $content);
-
-        //     } elseif ($request->request->has('valider')) {
-
-
-        //         $statutDemande = $this->getEntityManager()->getRepository(StatutDemande::class)->find(51);
-        //         $dit
-        //         ->setIdStatutDemande($statutDemande)
-        //         ->setDateValidation(new \DateTime($this->getDatesystem()))
-        //         ->setHeureValidation($this->getTime())
-        //         ;
-        //         $this->getEntityManager()->flush();
-
-        //         dd($dit);
-
-        //         if($dit->getDemandeDevis() === "OUI") {
-
-        //         $variableEmail = $this->donnerValideAvecDevis($userDemandeur, $emailSuperieurs, $userConnecter, $dit);
-
-        //         $content = $this->emailValideAvecDevis($variableEmail);
-
-        //         $this->confirmationEmail($email, $content);
-        //         } else {
-        //         $content = $this->emailValideSansDevis();
-
-        //         $this->confirmationEmail($email, $content);
-        //         }
-        //     }
-
-        //     $this->redirectToRoute("dit_index");
-        // }
-
-        // dd($dit);
         //RECUPERATION DE LISTE COMMANDE 
         $commandes = $ditModel->RecupereCommandeOr($dit->getNumeroOR());
 
