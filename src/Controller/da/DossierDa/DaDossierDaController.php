a878e40e3dac3e9b5fb63fb91fdbb3f886ea4131
Menja <ranofimenjajam@gmail.com>
2026-04-16 14:57:47 +0300

    back-end pour l'affichage des fichiers de DA

--- src/Controller/da/DossierDa/DaDossierDaController.php
+++ src/Controller/da/DossierDa/DaDossierDaController.php
@@ -3,6 +3,10 @@
 namespace App\Controller\da\DossierDa;
 
 use App\Controller\Controller;
+use App\Controller\Traits\AutorisationTrait;
+use App\Entity\admin\Application;
+use App\Entity\da\DemandeAppro;
+use App\Service\da\DocRattacheService;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -10,13 +14,33 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class DaDossierDaController extends Controller
 {
+    use AutorisationTrait;
+
+    private DocRattacheService $docRattacheService;
+
+    public function __construct(DocRattacheService $docRattacheService)
+    {
+        $this->docRattacheService = $docRattacheService;
+    }
+
     /**
      * @Route("/dossier-da/{numDa}", name="da_dossier_da")
      */
     public function dossierDa($numDa)
     {
+        //verification si user connecter
+        $this->verifierSessionUtilisateur();
+
+        /** Autorisation accées */
+        $this->autorisationAcces($this->getUser(), Application::ID_DAP);
+        /** FIN AUtorisation acées */
+
+        $demandeAppro = $this->getEntityManager()->getRepository(DemandeAppro::class)->findOneBy(['numDa' => $numDa]);
+
+        $fichiers = $this->docRattacheService->getAllAttachedFiles($demandeAppro);
+
         return $this->render("da/dossier-da.html.twig", [
-            'numDa' => $numDa,
+            'fichiers' => $fichiers,
         ]);
     }
 }
