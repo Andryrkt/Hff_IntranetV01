43856f5e66aed9a2a61a8a00028eab3d0a522dd9
Menja <ranofimenjajam@gmail.com>
2026-04-20 13:54:01 +0300

    création de PDF réappro et validation

---
+++ src/Controller/admin/generationPDF/GenerationPDFReapproController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller\admin\generationPDF;
+
+use App\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\Traits\da\validation\DaValidationReapproTrait;
+use App\Service\genererPdf\da\GenererPdfDaReappro;
+
+/** @Route(path="/admin/generation-PDF") */
+class GenerationPDFReapproController extends Controller
+{
+    use DaValidationReapproTrait;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->initDaValidationReapproTrait();
+    }
+
+    /**
+     * @Route(path="/valider/da-reappro/{numeroDemandeAppro}", name="valider_da_reappro")
+     */
+    public function validerDaReappro(string $numeroDemandeAppro)
+    {
+        if (!$this->estAdmin()) {
+            $this->redirectToRoute('security_signin');
+        }
+        $demandeAppro = $this->demandeApproRepository->findAvecDernieresDALetLRParNumero($numeroDemandeAppro);
+        // création de PDF
+        $genererPdfReappro = new GenererPdfDaReappro();
+        $dateRange = $this->getLast12MonthsRange();
+        $monthsList = $this->getMonthsList($dateRange['start'], $dateRange['end']);
+        $dataHistoriqueConsommation = $this->getHistoriqueConsommation($demandeAppro, $dateRange, $monthsList);
+        $observations = $this->daObservationRepository->findBy(
+            ['numDa' => $numeroDemandeAppro],
+            ['dateCreation' => 'ASC']
+        );
+        $genererPdfReappro->genererPdfBonAchatValide($demandeAppro, $observations, $monthsList, $dataHistoriqueConsommation);
+
+        // Dépôt du document dans DocuWare
+        $genererPdfReappro->copyToDWDaAValiderReapproPonctuel($numeroDemandeAppro, "");
+
+        // Enregistrement dans la table de Soumission
+        $this->ajouterDansDaSoumisAValidation($demandeAppro);
+    }
+}
