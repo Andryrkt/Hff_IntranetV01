54b606498409ffec94303bd738e2f006668bd95c
Menja <ranofimenjajam@gmail.com>
2026-06-02 16:24:23 +0300

    modif de filecontrolleur

--- src/Controller/FileController.php
+++ src/Controller/FileController.php
@@ -12,26 +12,17 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 class FileController extends Controller
 {
     /**
-     * @Route("/secure-file/bap/{numeroDdp}/{urlPdf}", name="bap_pdf_viewer")
+     * @Route("/secure-file/bap/{numeroDdp}/{fullPath}", name="bap_pdf_viewer")
      */
-    public function showBapPdf(string $numeroDdp, string $urlPdf): Response
+    public function showBapPdf(string $numeroDdp, string $fullPath): Response
     {
-        // Get projectDir from the container via the kernel service
-        $projectDir = $_ENV['BASE_PATH_FICHIER'];
-
-        $relativePath = "/ddp/$numeroDdp/$urlPdf";
-        $fullPath = $projectDir . $relativePath;
-
         if (!file_exists($fullPath)) {
             throw new NotFoundHttpException('Le fichier DDP/BAP est introuvable.');
         }
 
         $response = new BinaryFileResponse($fullPath);
         $response->headers->set('Content-Type', 'application/pdf');
-        $response->setContentDisposition(
-            ResponseHeaderBag::DISPOSITION_INLINE,
-            $urlPdf
-        );
+        $response->setContentDisposition(ResponseHeaderBag::DISPOSITION_INLINE, "$numeroDdp.pdf");
         return $response;
     }
 }
