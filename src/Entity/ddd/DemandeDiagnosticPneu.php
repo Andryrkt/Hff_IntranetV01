2b7f429165220c72cd872b55a9d6607e07849ed9
Jaina20 <87335072+Jaina20@users.noreply.github.com>
2026-08-05 08:57:22 +0300

    feat:creation des entity pour ddd

---
+++ src/Entity/ddd/DemandeDiagnosticPneu.php
@@ -0,0 +1,321 @@
+<?php
+
+namespace App\Entity\ddd;
+
+use App\Entity\ddd\Chantier;
+use DateTime;
+
+/**
+ * @ORM\Entity(repositoryClass=DiagnosticPneu::class)
+ * @ORM\Table(name="demande_diagnostic_pneu")
+ * @ORM\HasLifecycleCallbacks
+ */
+class DemandeDiagnosticPneu
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="bigint")
+     */
+    private ?int $id = null;
+
+    /**
+     * @ORM\Column(type="string", length=12, unique=true, name="numero_demande")
+     */
+    private ?string $numeroDemande = null;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Chantier::class)
+     * @ORM\JoinColumn(name="id_chantier", referencedColumnName="id_chantier", nullable=false)
+     */
+    private ?Chantier $chantier = null;
+
+    /**
+     * @ORM\Column(type="integer", name="id_materiel")
+     */
+    private ?int $idMateriel = null;
+
+    /**
+     * @ORM\Column(type="string", length=20, name="numero_parc_materiel")
+     */
+    private ?string $numeroParcMateriel = null;
+
+    /**
+     * @ORM\Column(type="string", length=50, name="marque_materiel")
+     */
+    private ?string $marqueMateriel = null;
+
+    /**
+     * @ORM\Column(type="string", length=50, name="type_materiel")
+     */
+    private ?string $typeMateriel = null;
+
+    /**
+     * @ORM\Column(type="string", length=150, name="designation_materiel")
+     */
+    private ?string $designationMateriel = null;
+
+    /**
+     * @ORM\Column(type="date", name="date_depart_chantier")
+     */
+    private ?DateTime $dateDepartChantier = null;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    private ?string $livraison = null;
+
+    /**
+     * @ORM\Column(type="smallint", name="nb_pneu_sur_machine")
+     */
+    private ?int $nbPneuSurMachine = null;
+
+    /**
+     * @ORM\Column(type="smallint", name="nb_pneu_secours")
+     */
+    private ?int $nbPneuSecours = null;
+
+    /**
+     * @ORM\Column(type="smallint", name="nb_pneu_a_diagnostiquer")
+     */
+    private ?int $nbPneuADiagnostiquer = null;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private ?string $observation = null;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private ?string $demandeur = null;
+
+    /**
+     * @ORM\Column(type="datetime", name="date_creation")
+     */
+    private ?DateTime $dateCreation = null;
+
+    /**
+     * @ORM\Column(type="string", length=20)
+     */
+    private ?string $statut = 'a_traiter_atelier';
+
+    /**
+     * @ORM\Column(type="string", length=12, nullable=true, name="numero_dit")
+     */
+    private ?string $numeroDit = null;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=true, name="numero_or")
+     */
+    private ?string $numeroOr = null;
+
+    public function __construct()
+    {
+        $this->dateCreation = new DateTime();
+        $this->statut = 'a_traiter_atelier';
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNumeroDemande(): ?string
+    {
+        return $this->numeroDemande;
+    }
+
+    public function setNumeroDemande(string $numeroDemande): self
+    {
+        $this->numeroDemande = $numeroDemande;
+        return $this;
+    }
+
+    public function getChantier(): ?Chantier
+    {
+        return $this->chantier;
+    }
+
+    public function setChantier(?Chantier $chantier): self
+    {
+        $this->chantier = $chantier;
+        return $this;
+    }
+
+    public function getIdMateriel(): ?int
+    {
+        return $this->idMateriel;
+    }
+
+    public function setIdMateriel(int $idMateriel): self
+    {
+        $this->idMateriel = $idMateriel;
+        return $this;
+    }
+
+    public function getNumeroParcMateriel(): ?string
+    {
+        return $this->numeroParcMateriel;
+    }
+
+    public function setNumeroParcMateriel(string $numeroParcMateriel): self
+    {
+        $this->numeroParcMateriel = $numeroParcMateriel;
+        return $this;
+    }
+
+    public function getMarqueMateriel(): ?string
+    {
+        return $this->marqueMateriel;
+    }
+
+    public function setMarqueMateriel(string $marqueMateriel): self
+    {
+        $this->marqueMateriel = $marqueMateriel;
+        return $this;
+    }
+
+    public function getTypeMateriel(): ?string
+    {
+        return $this->typeMateriel;
+    }
+
+    public function setTypeMateriel(string $typeMateriel): self
+    {
+        $this->typeMateriel = $typeMateriel;
+        return $this;
+    }
+
+    public function getDesignationMateriel(): ?string
+    {
+        return $this->designationMateriel;
+    }
+
+    public function setDesignationMateriel(string $designationMateriel): self
+    {
+        $this->designationMateriel = $designationMateriel;
+        return $this;
+    }
+
+    public function getDateDepartChantier(): ?DateTime
+    {
+        return $this->dateDepartChantier;
+    }
+
+    public function setDateDepartChantier(DateTime $dateDepartChantier): self
+    {
+        $this->dateDepartChantier = $dateDepartChantier;
+        return $this;
+    }
+
+    public function getLivraison(): ?string
+    {
+        return $this->livraison;
+    }
+
+    public function setLivraison(?string $livraison): self
+    {
+        $this->livraison = $livraison;
+        return $this;
+    }
+
+    public function getNbPneuSurMachine(): ?int
+    {
+        return $this->nbPneuSurMachine;
+    }
+
+    public function setNbPneuSurMachine(int $nbPneuSurMachine): self
+    {
+        $this->nbPneuSurMachine = $nbPneuSurMachine;
+        return $this;
+    }
+
+    public function getNbPneuSecours(): ?int
+    {
+        return $this->nbPneuSecours;
+    }
+
+    public function setNbPneuSecours(int $nbPneuSecours): self
+    {
+        $this->nbPneuSecours = $nbPneuSecours;
+        return $this;
+    }
+
+    public function getNbPneuADiagnostiquer(): ?int
+    {
+        return $this->nbPneuADiagnostiquer;
+    }
+
+    public function setNbPneuADiagnostiquer(int $nbPneuADiagnostiquer): self
+    {
+        $this->nbPneuADiagnostiquer = $nbPneuADiagnostiquer;
+        return $this;
+    }
+
+    public function getObservation(): ?string
+    {
+        return $this->observation;
+    }
+
+    public function setObservation(?string $observation): self
+    {
+        $this->observation = $observation;
+        return $this;
+    }
+
+    public function getDemandeur(): ?string
+    {
+        return $this->demandeur;
+    }
+
+    public function setDemandeur(string $demandeur): self
+    {
+        $this->demandeur = $demandeur;
+        return $this;
+    }
+
+    public function getDateCreation(): ?DateTime
+    {
+        return $this->dateCreation;
+    }
+
+    public function setDateCreation(DateTime $dateCreation): self
+    {
+        $this->dateCreation = $dateCreation;
+        return $this;
+    }
+
+    public function getStatut(): ?string
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(string $statut): self
+    {
+        $this->statut = $statut;
+        return $this;
+    }
+
+    public function getNumeroDit(): ?string
+    {
+        return $this->numeroDit;
+    }
+
+    public function setNumeroDit(?string $numeroDit): self
+    {
+        $this->numeroDit = $numeroDit;
+        return $this;
+    }
+
+    public function getNumeroOr(): ?string
+    {
+        return $this->numeroOr;
+    }
+
+    public function setNumeroOr(?string $numeroOr): self
+    {
+        $this->numeroOr = $numeroOr;
+        return $this;
+    }
+}
