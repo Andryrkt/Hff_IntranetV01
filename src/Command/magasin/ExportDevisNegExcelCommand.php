db12326cd1c96014ddf66825c5a9990b0687bd3e
Andryrkt <andryrkt08@gmail.com>
2026-09-15 07:39:23 +0300

    correctiond e l'exportDevisExcelCommand

--- src/Command/magasin/ExportDevisNegExcelCommand.php
+++ src/Command/magasin/ExportDevisNegExcelCommand.php
@@ -42,144 +42,129 @@ class ExportDevisNegExcelCommand extends Command
             $io->info('Connexion à la base de données Informix...');
             $db->connect();
 
-            $sql = "WITH devis_extract AS (
-    SELECT
-        nent.*,
-        SUBSTR(
-            nent.nent_libcde,
-            7,
-            INSTR(SUBSTR(nent.nent_libcde, 7), ' ') - 1
-        ) AS numero_devis
-    FROM ips_hffprod:informix.neg_ent nent
-    WHERE nent.nent_libcde <> ''
-      AND nent.nent_soc = 'HF'
-      AND nent.nent_succ = '40'
-      AND nent.nent_natop = 'DIR'
-      AND YEAR(nent.nent_datecde) = 2026
-),
+            $sql = "SELECT 
+nent.nent_datecde                                           AS date_cde_brute
+                ,CASE 
+                    WHEN dneg.statut_dw = '' OR dneg.statut_dw IS NULL THEN 'A traiter'
+                    ELSE dneg.statut_dw
+                END                                                         AS statut_dw
+                ,dneg.statut_bc                                             AS statut_bc
+                ,nent.nent_numcde                                           AS numero_devis
+                ,(select TO_CHAR(date_creation, '%d/%m/%Y') from ir_prod108:Informix.devis_soumis_a_validation_neg d 
+                where d.numero_devis = nent.nent_numcde 
+                AND d.numero_version = (SELECT MIN(numero_version) 
+                							FROM ir_prod108:Informix.devis_soumis_a_validation_neg 
+                							WHERE numero_devis = nent.nent_numcde
+                						)
+                )															as date_premiere_soumission_devis
+                ,TO_CHAR(nent.nent_datecde, '%d/%m/%Y')                     AS date_creation
+                ,nent.nent_succ || ' - ' || nent.nent_servcrt                    AS emetteur
+                ,nent.nent_numcli || ' - ' || nent.nent_nomcli                   AS client
+                ,TRIM(nent.nent_refcde)                                     AS reference_client -- Libellé
+                ,nent.nent_cdeht                                            AS montant_devis
+                ,nent.nent_devise                                           AS devise
+                ,TO_CHAR(dneg.date_envoye_devis_client, '%d/%m/%Y')         AS date_envoye_devis_au_client
+                --,dneg.stop_progression_global                               AS stop_progression_global
+                --,dneg.motif_stop_global                                     AS motif_stop_global
 
--- Dernière version pour chaque devis
-last_version_devis AS (
-    SELECT
-        de.numero_devis,
-        MAX(numero_version) AS max_version,
-        MIN(numero_version) AS min_version
-    FROM ir_prod108:informix.devis_soumis_a_validation_neg dsvn
-    inner join devis_extract de
-    on de.numero_devis = dsvn.numero_devis
-    GROUP BY numero_devis
-),
+                -- Pour statut_relance_1
+                ,CASE
+                    WHEN rl.date_relance1 IS NOT NULL
+                        THEN TO_CHAR(rl.date_relance1, '%d/%m/%Y')
+                    WHEN dneg.statut_bc = 'En attente bc'
+                        AND NVL(rl.nb_relances, 0) = 0
+                        AND dneg.date_envoye_devis_client IS NOT NULL
+                        AND (TODAY - DATE(dneg.date_envoye_devis_client)) >= 7
+                        AND (dneg.stop_progression_global = 0 OR dneg.stop_progression_global IS NULL)
+                        THEN 'A relancer'
+                    ELSE NULL
+                END AS statut_relance_1
+                -- Pour statut_relance_2
+                ,CASE
+                    WHEN rl.date_relance2 IS NOT NULL
+                        THEN TO_CHAR(rl.date_relance2, '%d/%m/%Y')
+                    WHEN dneg.statut_bc = 'En attente bc'
+                        AND rl.nb_relances = 1
+                        AND rl.delai_jours >= 7
+                        AND (dneg.stop_progression_global = 0 OR dneg.stop_progression_global IS NULL)
+                        THEN 'A relancer'
+                    ELSE NULL
+                END AS statut_relance_2
 
-statut_devis AS (
-    SELECT
-        d.numero_devis,
-        d.statut_dw,
-        d.date_creation AS date_premiere_soumission,
-        d.date_envoye_devis_client
-    FROM ir_prod108:informix.devis_soumis_a_validation_neg d
-    INNER JOIN last_version_devis lv 
-        ON d.numero_devis = lv.numero_devis 
-        AND d.numero_version = lv.max_version
-),
-
--- Première soumission (version MIN)
-first_submission AS (
-    SELECT
-        d.numero_devis,
-        d.date_creation AS date_premiere_soumission
-    FROM ir_prod108:informix.devis_soumis_a_validation_neg d
-    INNER JOIN last_version_devis lv 
-        ON d.numero_devis = lv.numero_devis 
-        AND d.numero_version = lv.min_version
-),
-
-relances AS (
-    SELECT
-        numero_devis,
-        MAX(CASE WHEN numero_relance = 1 THEN date_de_relance END) AS date_relance1,
-        MAX(CASE WHEN numero_relance = 2 THEN date_de_relance END) AS date_relance2,
-        MAX(CASE WHEN numero_relance = 3 THEN date_de_relance END) AS date_relance3,
-        COUNT(*) AS nb_relances,
-        MAX(date_de_relance) AS derniere_relance,
-        (TODAY - DATE(MAX(date_de_relance))) AS delai_jours
-    FROM ir_prod108:informix.pointage_relance
-    GROUP BY numero_devis
-),
-
--- Dernier BC pour chaque devis
-last_bc AS (
-    SELECT
-        de.numero_devis,
-        MAX(numero_version) AS max_version
-    FROM ir_prod108:informix.bc_client_soumis_neg b
-    inner join devis_extract de
-    on de.numero_devis = b.numero_devis
-    GROUP BY numero_devis
-),
-
-bc_info AS (
-    SELECT
-        b.numero_devis,
-        b.date_creation AS date_soumission_po,
-        b.numero_bc AS numero_po
-    FROM ir_prod108:informix.bc_client_soumis_neg b
-    INNER JOIN last_bc lb 
-        ON b.numero_devis = lb.numero_devis 
-        AND b.numero_version = lb.max_version
-),
-
--- nombre de ligne
-nb_ligne AS (
-    SELECT 
-        de.nent_numcde, 
-        COUNT(*) AS nb_ligne 
-    FROM ips_hffprod:Informix.neg_lig nl
-    INNER JOIN devis_extract de
-        ON de.nent_numcde = nl.nlig_numcde 
-    WHERE nl.nlig_codg = 'ST'
+                -- Pour statut_relance_3
+                ,CASE
+                    WHEN rl.date_relance3 IS NOT NULL
+                        THEN TO_CHAR(rl.date_relance3, '%d/%m/%Y')
+                    WHEN dneg.statut_bc = 'En attente bc'
+                        AND rl.nb_relances = 2
+                        AND rl.delai_jours >= 7
+                        AND (dneg.stop_progression_global = 0 OR dneg.stop_progression_global IS NULL)
+                        THEN 'A relancer'
+                    ELSE NULL
+                END AS statut_relance_3
+                , case when dneg.stop_progression_global = 0 then 'non' else 'oui' end as stop_relance
+                ,nent.nent_posl                                             AS position_ips -- position devis IPS
+                ,b.numero_bc 												as numero_po --PO/BC client
+                ,b.date_creation 											as date_soumission_po --Date soumission PO
+                ,TRIM(ausr.ausr_nom)                                        AS utilisateur_createur_devis -- crée par
+                ,dneg.utilisateur                                           AS soumis_par
+                ,SUBSTR(nent.nent_libcde,7,INSTR(SUBSTR(nent.nent_libcde, 7), ' ') - 1) AS numero_bc_negoce --numéro BC négoce
+               ,TO_CHAR(nent.nent_delai, '%d/%m/%Y') 							AS delai -- délai 
+               ,nent.nent_posf 												as position_bc_negoce --Position bc négoce IPS
+                -- Nbre Pièce BC négoce
+               ,(select COUNT(*) AS nb_ligne 
+    FROM ips_hffprod:Informix.neg_lig nl WHERE nl.nlig_codg = 'ST'
       AND nl.nlig_soc = 'HF'
       and nl.nlig_succ = '40'
-    GROUP BY de.nent_numcde
-)
+      and nl.nlig_numcde = SUBSTR(nent.nent_libcde,7,INSTR(SUBSTR(nent.nent_libcde, 7), ' ') - 1)
+      ) 																	as nbre_piece_bc_negoce
+                ,'' AS constructeur
 
-SELECT
-    -- Devis
-    d.numero_devis,
-    d.nent_numcli || ' - ' || d.nent_nomcli AS client,
-    
-    -- Statut et dates de soumission
-    s.statut_dw,
-    TO_CHAR(fs.date_premiere_soumission, '%d/%m/%Y') as dare_premier_soumission,
-    TO_CHAR(s.date_envoye_devis_client, '%d/%m/%Y') as date_envoye_devis_client,
-    
-    -- Dates de relance (formatées)
-    TO_CHAR(r.date_relance1, '%d/%m/%Y') AS date_relance_1,
-    TO_CHAR(r.date_relance2, '%d/%m/%Y') AS date_relance_2,
-    TO_CHAR(r.date_relance3, '%d/%m/%Y') AS date_relance_3,
-    
-    -- Informations BC/PO
-    TO_CHAR(b.date_soumission_po, '%d/%m/%Y') as date_soumission_po,
-    b.numero_po,
-    
-    -- Champs calculés
-     d.nent_numcde as numero_commande_negoce,
-    '' AS constructeur,
-    n.nb_ligne AS nombre_piece_devis,
-    TO_CHAR(d.nent_delai, '%d/%m/%Y') AS delai,
-     d.nent_posf  AS facture,
-     d.nent_cdeht as montant
+            FROM ips_hffprod:informix.neg_ent nent
 
-FROM devis_extract d
-LEFT JOIN statut_devis s ON d.numero_devis = s.numero_devis
-LEFT JOIN first_submission fs ON d.numero_devis = fs.numero_devis
-LEFT JOIN relances r ON d.numero_devis = r.numero_devis
-LEFT JOIN bc_info b ON d.numero_devis = b.numero_devis
-LEFT join nb_ligne n on n.nent_numcde = d.nent_numcde
+            LEFT JOIN ips_hffprod:informix.agr_usr ausr
+                ON ausr.ausr_num = nent.nent_usr
+                AND ausr.ausr_soc = nent.nent_soc
 
-WHERE 1=1  -- Pour faciliter l'ajout de filtres supplémentaires
-  -- Ajoute ici d'autres conditions si nécessaire
+            LEFT JOIN ir_prod108:Informix.devis_soumis_a_validation_neg dneg
+                ON dneg.numero_devis = nent.nent_numcde
+                AND dneg.numero_version = (SELECT MAX(numero_version) FROM ir_prod108:Informix.devis_soumis_a_validation_neg WHERE numero_devis = nent.nent_numcde) 
 
-ORDER BY d.nent_numcde DESC";
+            left join ir_prod108:informix.bc_client_soumis_neg b
+            	on b.numero_devis = nent.nent_numcde
+            	and b.numero_version = (select MAX(bb.numero_version) from ir_prod108:informix.bc_client_soumis_neg bb where bb.numero_devis = nent.nent_numcde)
+            	
+            LEFT JOIN (
+                SELECT
+                    numero_devis as num_dev
+                    ,MAX(CASE WHEN numero_relance = 1 THEN date_de_relance ELSE NULL END) AS date_relance1
+                    ,MAX(CASE WHEN numero_relance = 2 THEN date_de_relance ELSE NULL END) AS date_relance2
+                    ,MAX(CASE WHEN numero_relance = 3 THEN date_de_relance ELSE NULL END) AS date_relance3
+                    ,COUNT(*) AS nb_relances
+                    ,MAX(date_de_relance) AS derniere_relance
+                    ,(TODAY - DATE(MAX(date_de_relance))) AS delai_jours
+                FROM ir_prod108:Informix.pointage_relance
+                GROUP BY 1
+            ) rl ON rl.num_dev = nent.nent_numcde
+
+            WHERE nent.nent_natop    = 'DEV'
+                AND nent.nent_servcrt  <> 'ASS'
+                AND nent.nent_numcli   NOT BETWEEN 1990000 AND 1999999
+                AND nent.nent_numcli   <> 1990000
+                AND nent.nent_numcde   NOT IN (19407989,19407991,19408971,19410383,19409906,19409996)
+                AND (nent_datecde >= MDY(9, 1, 2025) OR nent_numcde IN ('54207000', '54206997'))
+                AND nent.nent_succ <> '60'
+                AND nent.nent_soc = 'HF'
+                AND EXISTS (
+                                SELECT 1 FROM ips_hffprod:informix.neg_lig nl
+                                WHERE nl.nlig_numcde = nent.nent_numcde
+                                AND nl.nlig_constp IN ('AGR','ATC','AUS','CAT','CGM','CMX','DNL','DYN','GRO','HYS','JDR','KIT','MAN','MNT','OLY','OOM','PAR','PDV','PER','PUB','REM','SHM','TBI','THO') 
+                            )
+                AND NOT (dneg.statut_dw = 'A traiter'
+                AND nent.nent_posl = 'TR')
+       			AND  nent.nent_posl in ('--','AC','DE', 'TR') 
+       			AND  nent.nent_succ = '40'  
+       		ORDER BY date_cde_brute DESC";
 
             $io->info('Exécution de la requête SQL...');
             $result = $db->executeQuery($sql);
@@ -194,7 +179,7 @@ ORDER BY d.nent_numcde DESC";
                 $cdeToDevisMap = [];
                 $allNums = [];
                 foreach ($rows as $row) {
-                    $cde = trim((string)($row['numero_commande_negoce'] ?? ''));
+                    $cde = trim((string)($row['numero_bc_negoce'] ?? ''));
                     $dev = trim((string)($row['numero_devis'] ?? ''));
                     if ($cde !== '') {
                         $allNums[] = $cde;
@@ -259,21 +244,31 @@ ORDER BY d.nent_numcde DESC";
             // Préparation des données pour Excel
             $excelData = [];
             $excelData[] = [
+                'Statut DW',
+                'Statut BC',
                 'Numéro devis',
+                'Date 1ère soumission devis',
+                'Date création',
+                'Emetteur',
                 'Client',
-                'Date 1ère soumission',
+                'Référence client',
+                'Montant devis',
+                'Devise',
                 'Date envoyé devis au client',
-                'Relance 1',
-                'Relance 2',
-                'Relance 3',
-                'Date soumission PO',
+                'Statut relance 1',
+                'Statut relance 2',
+                'Statut relance 3',
+                'Stop relance',
+                'Position IPS',
                 'Numéro PO',
-                'Numéro commande négoce',
-                'Constructeur',
-                'Nombre pièce devis',
+                'Date soumission PO',
+                'Utilisateur créateur devis',
+                'Soumis par',
+                'Numéro BC négoce',
                 'Délai',
-                'Facturé',
-                'Montant HT'
+                'Position BC négoce IPS',
+                'Nbre pièce BC négoce',
+                'Constructeur',
             ];
 
             $formatDate = function ($dateVal) {
@@ -286,34 +281,44 @@ ORDER BY d.nent_numcde DESC";
 
             foreach ($rows as $row) {
                 $numDev = trim((string)($row['numero_devis'] ?? ''));
-                $numCde = trim((string)($row['numero_commande_negoce'] ?? ''));
+                $numBcNegoce = trim((string)($row['numero_bc_negoce'] ?? ''));
 
                 $constList = [];
                 if ($numDev !== '' && isset($constructeursMap[$numDev])) {
                     $constList = array_merge($constList, $constructeursMap[$numDev]);
                 }
-                if ($numCde !== '' && isset($constructeursMap[$numCde])) {
-                    $constList = array_merge($constList, $constructeursMap[$numCde]);
+                if ($numBcNegoce !== '' && isset($constructeursMap[$numBcNegoce])) {
+                    $constList = array_merge($constList, $constructeursMap[$numBcNegoce]);
                 }
                 $constList = array_unique(array_filter($constList));
                 $constructeurs = implode(', ', $constList);
 
                 $excelData[] = [
+                    $row['statut_dw'] ?? '',
+                    $row['statut_bc'] ?? '',
                     $numDev,
+                    $formatDate($row['date_premiere_soumission_devis'] ?? ''),
+                    $formatDate($row['date_creation'] ?? ''),
+                    $row['emetteur'] ?? '',
                     $row['client'] ?? '',
-                    $formatDate($row['date_premiere_soumission'] ?? ''),
-                    $row['date_envoye_devis_au_client'] ?? '',
-                    $row['date_relance_1'] ?? '',
-                    $row['date_relance_2'] ?? '',
-                    $row['date_relance_3'] ?? '',
-                    $formatDate($row['date_soumission_po'] ?? ''),
+                    $row['reference_client'] ?? '',
+                    $row['montant_devis'] ?? '',
+                    $row['devise'] ?? '',
+                    $formatDate($row['date_envoye_devis_au_client'] ?? ''),
+                    $row['statut_relance_1'] ?? '',
+                    $row['statut_relance_2'] ?? '',
+                    $row['statut_relance_3'] ?? '',
+                    $row['stop_relance'] ?? '',
+                    $row['position_ips'] ?? '',
                     $row['numero_po'] ?? '',
-                    $numCde,
-                    $constructeurs,
-                    $row['nombre_piece_devis'] ?? '',
+                    $formatDate($row['date_soumission_po'] ?? ''),
+                    $row['utilisateur_createur_devis'] ?? '',
+                    $row['soumis_par'] ?? '',
+                    $numBcNegoce,
                     $row['delai'] ?? '',
-                    $row['facture'] ?? '',
-                    $row['montant'] ?? '',
+                    $row['position_bc_negoce'] ?? '',
+                    $row['nbre_piece_bc_negoce'] ?? '',
+                    $constructeurs,
                 ];
             }
 
