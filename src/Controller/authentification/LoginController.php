8cd8b6fa78ab2e4a86e3b25be7e3faefa9a14902
Menja <ranofimenjajam@gmail.com>
2026-09-16 14:48:53 +0300

    feat: ne plus stocker mot de passe de l'utilisateur

--- src/Controller/authentification/LoginController.php
+++ src/Controller/authentification/LoginController.php
@@ -65,10 +65,6 @@ class LoginController extends Controller
 
                     $this->getSessionService()->set('user_info', $userInfo);
 
-                    $filename = $_ENV['BASE_PATH_LONG'] . "\src\Controller\authentification.csv";
-                    $newData = [$userId, $username, $password];
-                    $this->synchronizeCSV($filename, $newData);
-
                     if ($profils->count() > 1) $this->redirectToRoute('choix_societe');
 
                     /** @var Profil $profil */
@@ -98,56 +94,6 @@ class LoginController extends Controller
         return $this->render('signIn.html.twig', [
             'error_msg' => $error_msg,
         ]);
-    }
-
-    private function synchronizeCSV(string $filename, array $newData)
-    {
-        $rows = [];
-        $found = false;
-
-        // Vérifier si le fichier existe avant de tenter de le lire
-        if (file_exists($filename)) {
-            $handle = fopen($filename, "r");
-
-            if (!$handle) {
-                die("Erreur : Impossible d'ouvrir le fichier $filename en lecture.");
-            }
-
-            while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
-                if ($data[0] == $newData[0]) { // Vérifie si l'ID existe déjà
-                    if ($data[2] !== $newData[2]) { // Vérifie si l'email est différent
-                        $data[2] = $newData[2]; // Met à jour l'email
-                    }
-                    $found = true;
-                }
-                $rows[] = $data; // Stocke la ligne (modifiée ou non)
-            }
-
-            fclose($handle);
-        }
-
-        // Si l'ID n'existe pas, ajoute une nouvelle ligne
-        if (!$found) {
-            $rows[] = $newData;
-        }
-
-        // Vérifier si le fichier est accessible en écriture
-        if (!is_writable($filename) && file_exists($filename)) {
-            die("Erreur : Impossible d'écrire dans le fichier $filename");
-        }
-
-        // Réécriture complète du fichier CSV
-        $handle = fopen($filename, "w");
-
-        if (!$handle) {
-            die("Erreur : Impossible d'ouvrir le fichier $filename en écriture.");
-        }
-
-        foreach ($rows as $row) {
-            fputcsv($handle, $row, ";");
-        }
-
-        fclose($handle);
     }
 
     /**
