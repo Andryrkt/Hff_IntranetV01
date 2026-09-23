ef396b0cefc7241bdad6f8c739509f0ebc09b579
Menja <ranofimenjajam@gmail.com>
2026-09-23 11:44:54 +0300

    feat: ajout de login dans iframeConstant

--- src/Constants/iframe/IframeConstant.php
+++ src/Constants/iframe/IframeConstant.php
@@ -8,4 +8,11 @@ class IframeConstant
         "rental-rollout" => "/RentalRollout/dashboard_rental.html",
         "ge-rollout"     => "/RentalRollout/dashboard_energie.html"
     ];
+
+    public static function getLinkIframe(string $slug, string $username): string
+    {
+        $link = self::LINK[$slug] ?? "";
+
+        return $link ? "{$link}?login={$username}" : "";
+    }
 }
